73d7ea1003e1c6f76d2c19a97e4216d769f091fa
sp-miguel-janer <miguel.janer@socialpoint.es>
2015-08-26 18:07:31 +0200

    added deviceInfo service to allow fake AppData

---
+++ Project/Assets/Scripts/Services/DeviceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SocialPoint.Hardware;
+using Zenject;
+
+class DeviceInfo : SocialPointDeviceInfo
+{
+    [InjectOptional("hardware_fake_app_info")]
+    public EmptyAppInfo InjectAppInfo
+    {
+        set
+        {
+            AppInfo = value;
+        }
+    }
+
+    public DeviceInfo()
+    {
+    }
+}
