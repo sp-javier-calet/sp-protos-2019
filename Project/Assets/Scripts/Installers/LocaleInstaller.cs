4996e246305d0f59b51ef635d7f4be18002922a0
sp-miguel-janer <miguel.janer@socialpoint.es>
2015-08-28 16:51:48 +0200

    Add environment selector to LocaleInstaller

--- Project/Assets/Scripts/Installers/LocaleInstaller.cs
+++ Project/Assets/Scripts/Installers/LocaleInstaller.cs
@@ -6,12 +6,16 @@ using Zenject;
 
 public class LocaleInstaller : MonoInstaller
 {
+    public enum EnvironmentID {dev,loc,prod};
+
     [Serializable]
     public class SettingsData
     {
         public string ProjectId = LocalizationManager.LocationData.DefaultProjectId;
-        public string EnvironmentId = LocalizationManager.LocationData.DefaultEnvironmentId;
-        public string SecretKey = LocalizationManager.LocationData.DefaultSecretKey;
+        public EnvironmentID EnvironmentId = EnvironmentID.prod;
+        public string SecretKeyDev = LocalizationManager.LocationData.DefaultSecretKey;
+        public string SecretKeyLoc = LocalizationManager.LocationData.DefaultSecretKey;
+        public string SecretKeyProd = LocalizationManager.LocationData.DefaultSecretKey;
         public string BundleDir = LocalizationManager.DefaultBundleDir;
         public string[] SupportedLanguages = LocalizationManager.DefaultSupportedLanguages;
         public float Timeout = LocalizationManager.DefaultTimeout;
@@ -23,8 +27,15 @@ public class LocaleInstaller : MonoInstaller
 	{	
         Container.Bind<Localization>().ToSingle();
         Container.BindInstance("locale_project_id", Settings.ProjectId);
-        Container.BindInstance("locale_env_id", Settings.EnvironmentId);
-        Container.BindInstance("locale_secret_key", Settings.SecretKey);
+        Container.BindInstance("locale_env_id", Settings.EnvironmentId.ToString());
+        string secretKey;
+        if(Settings.EnvironmentId == EnvironmentID.dev)
+            secretKey = Settings.SecretKeyDev;
+        else if(Settings.EnvironmentId == EnvironmentID.loc)
+            secretKey = Settings.SecretKeyLoc;
+        else
+            secretKey = Settings.SecretKeyProd;
+        Container.BindInstance("locale_secret_key", secretKey);
         Container.BindInstance("locale_supported_langs", Settings.SupportedLanguages);
         Container.BindInstance("locale_timeout", Settings.Timeout);
         Container.BindInstance("locale_bundle_dir", Settings.BundleDir);
