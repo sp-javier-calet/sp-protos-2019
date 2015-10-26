a3ab9fcb3a90baeb4555e1a6047ad464d4e2cda1
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2015-10-26 11:23:02 +0100

    Updated Google Play Services lib to v7.5.

---
+++ Project/Assets/Plugins/Android/google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/manuelalvarez/repositories/sp-unity-BaseGame/Project/Assets/Plugins/Android/google-play-services_lib/bin/AndroidManifest.xml : \
+/Users/manuelalvarez/repositories/sp-unity-BaseGame/Project/Assets/Plugins/Android/google-play-services_lib/AndroidManifest.xml \
