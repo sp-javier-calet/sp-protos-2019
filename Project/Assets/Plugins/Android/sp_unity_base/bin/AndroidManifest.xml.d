19af9315f350fa38c194cdeb496903f09d1e07d2
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2015-11-09 13:38:19 +0100

    Updated sp_unity_base project. Added Metadata reader.

--- Project/Assets/Plugins/Android/sp_unity_base/bin/AndroidManifest.xml.d
+++ Project/Assets/Plugins/Android/sp_unity_base/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/sp_unity_base/bin/AndroidManifest.xml : \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/sp_unity_base/AndroidManifest.xml \
+/Users/manuelalvarez/repositories/sp-unity-BaseGame/Project/Assets/Plugins/Android/sp_unity_base/bin/AndroidManifest.xml : \
+/Users/manuelalvarez/repositories/sp-unity-BaseGame/Project/Assets/Plugins/Android/sp_unity_base/AndroidManifest.xml \
