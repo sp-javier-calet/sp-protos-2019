72be57c8cee5e45369e7dd15a6c4839cc657a534
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2015-11-12 13:46:25 +0100

    Improved copy libs behaviour.

--- Project/Assets/Plugins/Android/build_plugin_copy_dependencies.sh
+++ Project/Assets/Plugins/Android/build_plugin_copy_dependencies.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
-# Retrieve Unity classes
-UNITY_DIR=`find /Applications -type d -iname "Unity*" -maxdepth 1 | sort | tail -1`
-UNITY_ANDROID_DIR="${UNITY_DIR}/Unity.app/Contents/PlaybackEngines/AndroidPlayer/"
-UNITY_ANDROID_JAR=`find "${UNITY_ANDROID_DIR}" -iname classes.jar | grep il2cpp | head -1`
-echo "Copying Unity jar from ${UNITY_ANDROID_JAR}..."
-mkdir -p ./libs
-cp "${UNITY_ANDROID_JAR}" libs/unity-classes.jar
+# Copy unity and common dependencies as a project lib during compilation.
+# Only works from Plugins/Android root, called from /sp_unity_project/build.xml, 
+# using -pre-build and -post-build targets:
+#   <target name="-pre-build">
+#       <exec executable="../build_plugin_copy_dependencies.sh" failonerror="true">
+#           <arg value="copy"/>
+#       </exec>
+#   </target>
+#   <target name="-post-build">
+#       <exec executable="../build_plugin_copy_dependencies.sh" failonerror="true">
+#           <arg value="clean"/>
+#       </exec>
+#   </target>
 
-# Copy game libraries in Plugins/Android
-cp ../*.jar libs/
+if [ -z "$1" ]; then
+    echo "Required [copy|clean] option"
+    exit 1
+fi
+
+if [ $1 = "copy" ]; then
+     # Retrieve Unity classes
+    UNITY_DIR=`find /Applications -type d -iname "Unity*" -maxdepth 1 | sort | tail -1`
+    UNITY_ANDROID_DIR="${UNITY_DIR}/Unity.app/Contents/PlaybackEngines/AndroidPlayer/"
+    UNITY_ANDROID_JAR=`find "${UNITY_ANDROID_DIR}" -iname classes.jar | grep il2cpp | head -1`
+    echo "Copying Unity jar from ${UNITY_ANDROID_JAR}..."
+    mkdir -p ./libs
+    cp "${UNITY_ANDROID_JAR}" libs/unity-classes.jar
+
+    # Copy game libraries in Plugins/Android
+    cp ../*.jar libs/
+fi 
+
+if [ $1 = "clean" ]; then
+    # Remove unity classes .jar
+    rm libs/unity-classes.jar
+
+    # Remove other .jar files moved to libs
+    l=`ls ../*.jar`; for p in $l ; do file=`echo $p | rev | cut -d "/" -f 1 | rev`; rm "libs/$file"; done;
+fi 
--- Project/Assets/Plugins/Android/build_plugin_project_base.sh
+++ Project/Assets/Plugins/Android/build_plugin_project_base.sh
@@ -9,12 +9,13 @@ PLUGINS_PATH="$2/.."
 TMP_DIR="$PLUGINS_PATH/.build_tmp"
 
 # Create Android build required folders, if needed
+echo "Creating required folders for project - $2"
 mkdir -p $2/res
 
 # Move .meta files temporary to work properly with unity and referenced projects
+echo "Removing .meta files"
 mkdir -p $TMP_DIR
 l=`find $PLUGINS_PATH -name "[!.]*.meta" -type f`; for p in $l ; do file=`echo $p | rev | cut -d "/" -f 1 | rev`; path=${p%$file}; mkdir -p "$TMP_DIR/$path"; dest="$TMP_DIR/$path.$file"; mv $p $dest; done;
-
 
 # Update and build
 android update lib-project -p $2
@@ -22,5 +23,8 @@ ant clean -buildfile $2/build.xml
 ant release -buildfile $2/build.xml
 
 # Restore .meta files
+echo "Restoring .meta files"
 l=`find $PLUGINS_PATH -name ".?*.meta" -type f`; for p in $l ; do file=`echo $p | rev | cut -d "/" -f 1 | rev`; path=${p%$file}; path=${path#$TMP_DIR}; mkdir -p "$TMP_DIR/$path"; file=${file#"."};  dest="$path$file"; mv $p $dest; done;
 rm -r $TMP_DIR
+
+echo "Completed successfully"
