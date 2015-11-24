364ac9249179c035ed72aef285b1b9230ddc917d
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2015-11-24 17:19:32 +0100

    Removed google-play-services_lib. Yes, again. And yes, I know, I know... it will be a problem and plugins won't compile but...

--- Project/Assets/Plugins/Android/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/manuelalvarez/repositories/sp-unity-BaseGame/Project/Assets/Plugins/Android/google-play-services_lib/bin/AndroidManifest.xml : \
-/Users/manuelalvarez/repositories/sp-unity-BaseGame/Project/Assets/Plugins/Android/google-play-services_lib/AndroidManifest.xml \
