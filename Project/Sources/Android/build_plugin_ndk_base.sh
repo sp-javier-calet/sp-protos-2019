ecdec75000b705de734e540e1cc22ab3091b039f
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2015-12-11 12:26:08 +0100

    Moved legacy compilation scripts to Sources.

---
+++ Project/Sources/Android/build_plugin_ndk_base.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Cleaning up"
+rm -rf libs
+rm -rf obj
+
+cd $2/jni
+echo "Compiling $1 Android Native plugin - $2"
+ndk-build NDK_APPLICATION_MK=Application.mk
+
+cd ..
+
+echo "Cleaning up"
+rm -rf obj
+
+echo ""
+echo "Done!"
