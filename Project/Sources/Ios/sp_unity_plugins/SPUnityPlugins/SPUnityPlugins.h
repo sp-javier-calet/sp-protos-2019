7db6f192ceb33a59937b11243b99bde6c25b6235
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2015-12-16 13:34:56 +0100

    Added plugins Xcode project

---
+++ Project/Sources/Ios/sp_unity_plugins/SPUnityPlugins/SPUnityPlugins.h
@@ -0,0 +1,19 @@
+//
+//  SPUnityPlugins.h
+//  SPUnityPlugins
+//
+//  Created by Manuel Álvarez on 14/12/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPUnityPlugins.
+FOUNDATION_EXPORT double SPUnityPluginsVersionNumber;
+
+//! Project version string for SPUnityPlugins.
+FOUNDATION_EXPORT const unsigned char SPUnityPluginsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPUnityPlugins/PublicHeader.h>
+
+
