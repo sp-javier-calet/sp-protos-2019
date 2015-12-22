38fd00d4bb3aaedc33759a8e158d3d40d7fe1554
Miguel Ibero <miguel.ibero@socialpoint.es>
2015-12-22 11:26:42 +0100

    added alert and wait script events

--- Project/Assets/Libraries/SocialPoint/ScriptEvents/ScriptBridge.cs
+++ Project/Assets/Libraries/SocialPoint/ScriptEvents/ScriptBridge.cs
@@ -1,6 +1,8 @@
 using SocialPoint.AppEvents;
 using SocialPoint.Attributes;
 using System;
+using System.Collections;
+using UnityEngine;
 
 namespace SocialPoint.ScriptEvents
 {
@@ -9,12 +11,21 @@ namespace SocialPoint.ScriptEvents
         public ScriptModel Script;
     }
 
+    public struct LogAction
+    {
+        public LogType Type;
+        public string Message;
+        public UnityEngine.Object Object;
+    }
+
+    public struct WaitAction
+    {
+        public float Seconds;
+        public object Action;
+    }
+
     public class RunScriptActionParser : BaseScriptEventParser<RunScriptAction>
     {
-        const string AttrKeyUri = "uri";
-        const string AttrKeyScheme = "scheme";
-        const string AttrKeyParameters = "params";
-
         IParser<ScriptModel> _parser;
 
         public RunScriptActionParser(IParser<ScriptModel> parser): base("action.base.run_script")
@@ -30,6 +41,70 @@ namespace SocialPoint.ScriptEvents
         }
     }
 
+    public class LogActionParser : BaseScriptEventParser<LogAction>
+    {
+        const string AttrKeyMessage = "msg";
+        const string AttrKeyLogType = "type";
+        const string AttrKeyLogTypeInfo = "info";
+        const string AttrKeyLogTypeWarn = "warn";
+        const string AttrKeyLogTypeError = "error";
+        
+        public LogActionParser(): base("action.log")
+        {
+        }
+        
+        override protected LogAction ParseEvent(Attr data)
+        {
+            if(data.AttrType == AttrType.VALUE)
+            {
+                return new LogAction {
+                    Message = data.AsValue.ToString(),
+                    Type = LogType.Log
+                };
+            }
+            else
+            {
+                var typeStr = data.AsDic[AttrKeyLogType].AsValue.ToString();
+                LogType type = LogType.Log;
+                if(typeStr == AttrKeyLogTypeError)
+                {
+                    type = LogType.Error;
+                }
+                else if(typeStr == AttrKeyLogTypeWarn)
+                {
+                    type = LogType.Warning;
+                }
+                return new LogAction {
+                    Message = data.AsDic[AttrKeyMessage].AsValue.ToString(),
+                    Type = type
+                };
+            }
+        }
+    }
+
+    public class WaitActionParser : BaseScriptEventParser<WaitAction>
+    {
+        const string AttrKeySeconds = "secs";
+        const string AttrKeyAction = "action";
+
+        IScriptEventDispatcher _dispatcher;
+
+        public WaitActionParser(IScriptEventDispatcher dispatcher): base("action.wait")
+        {
+            _dispatcher = dispatcher;
+        }
+        
+        override protected WaitAction ParseEvent(Attr data)
+        {
+            var action = data.AsDic[AttrKeyAction].AsDic;
+
+            return new WaitAction {
+                Seconds = data.AsDic[AttrKeySeconds].AsValue.ToFloat(),
+                Action = _dispatcher.Parse(action)
+            };
+        }
+    }
+
     public class ScriptBridge :
         IEventsBridge,
         IScriptEventsBridge
@@ -37,22 +112,28 @@ namespace SocialPoint.ScriptEvents
         IEventDispatcher _dispatcher;
         IScriptEventDispatcher _scriptDispatcher;
         IParser<ScriptModel> _scriptParser;
+        MonoBehaviour _behaviour;
 
-        public ScriptBridge(IParser<ScriptModel> scriptParser)
+        public ScriptBridge(IParser<ScriptModel> scriptParser, MonoBehaviour behaviour)
         {
             _scriptParser = scriptParser;
+            _behaviour = behaviour;
         }
 
         public void Load(IScriptEventDispatcher dispatcher)
         {
             _scriptDispatcher = dispatcher;
             _scriptDispatcher.AddParser(new RunScriptActionParser(_scriptParser));
+            _scriptDispatcher.AddParser(new LogActionParser());
+            _scriptDispatcher.AddParser(new WaitActionParser(dispatcher));
         }
 
         public void Load(IEventDispatcher dispatcher)
         {
             _dispatcher = dispatcher;
             _dispatcher.AddListener<RunScriptAction>(OnRunScriptAction);
+            _dispatcher.AddListener<LogAction>(OnLogAction);
+            _dispatcher.AddListener<WaitAction>(OnWaitAction);
         }
 
         public void Dispose()
@@ -67,6 +148,33 @@ namespace SocialPoint.ScriptEvents
         {
             var script = new Script(_scriptDispatcher, action.Script);
             script.Run();
+        }
+
+        void OnLogAction(LogAction action)
+        {
+            if(action.Type == LogType.Error)
+            {
+                UnityEngine.Debug.LogError(action.Message, action.Object);
+            }
+            else if(action.Type == LogType.Warning)
+            {
+                UnityEngine.Debug.LogWarning(action.Message, action.Object);
+            }
+            else
+            {
+                UnityEngine.Debug.Log(action.Message, action.Object);
+            }
+        }
+
+        void OnWaitAction(WaitAction action)
+        {
+            _behaviour.StartCoroutine(WaitCoroutine(action));
+        }
+
+        IEnumerator WaitCoroutine(WaitAction action)
+        {
+            yield return new WaitForSeconds(action.Seconds);
+            _dispatcher.Raise(action.Action);
         }
 
     }
