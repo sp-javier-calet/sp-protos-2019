a240645c4e778e63022ed07e9d447b7d9fa2dd4c
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-01-11 18:13:24 +0100

    updated curl background updater for android and ios

--- Project/Sources/Android/sp_unity_native_plugins/sp_unity_curl/src/SPUnityCurlFacadeAndroid.cpp
+++ Project/Sources/Android/sp_unity_native_plugins/sp_unity_curl/src/SPUnityCurlFacadeAndroid.cpp
@@ -1,14 +1,44 @@
 #include "SPUnityCurlFacade.h"
 #include "SPUnityCurlManager.h"
+#include <mutex>
+#include <thread>
 
-EXPORT_API void SPUnityCurlOnApplicationPause(bool paused)
+std::mutex m;
+bool AppPaused = false;
+
+void curlUpdater_thread()
 {
-    if(paused)
+    while(SPUnityCurlRunning() > 0)
     {
-        while(SPUnityCurlRunning() > 0)
+        std::lock_guard<std::mutex> lk(m);
+        if(AppPaused)
         {
             SPUnityCurlUpdate(0);
+        }
+        else
+        {
+            break;
         }
     }
 }
 
+EXPORT_API void SPUnityCurlOnApplicationPause(bool paused)
+{
+    if(SPUnityCurlRunning() == 0)
+    {
+        return;
+    }
+
+    if(paused)
+    {
+        std::lock_guard<std::mutex> lk(m);
+        AppPaused = true;
+        std::thread updater(curlUpdater_thread);
+        updater.detach();
+    }
+    else
+    {
+        std::lock_guard<std::mutex> lk(m);
+        AppPaused = false;
+    }
+}
