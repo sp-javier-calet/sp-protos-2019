0bbc2a94ce54fb3a8ac8f0004846b0937de91962
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-02-01 15:17:19 +0100

    fixed test format

--- Project/Assets/Libraries/SocialPoint/Utils/Editor/Tests/RandomUtilsTests.cs
+++ Project/Assets/Libraries/SocialPoint/Utils/Editor/Tests/RandomUtilsTests.cs
@@ -18,25 +18,25 @@ namespace SocialPoint.Utils
         }
 
         [Test]
-		[Repeat(1000)]
+        [Repeat(1000)]
         public void UserIdTest()
         {	
-			var uid1 = RandomUtils.GenerateUserId();
-			var uid2 = RandomUtils.GenerateUserId();
-			Assert.That(uid1 != uid2);
+            var uid1 = RandomUtils.GenerateUserId();
+            var uid2 = RandomUtils.GenerateUserId();
+            Assert.That(uid1 != uid2);
 
-			var rnd1 = uid1 & 0x7FFFFFFF;
-			var rnd2 = uid2 & 0x7FFFFFFF;
+            var rnd1 = uid1 & 0x7FFFFFFF;
+            var rnd2 = uid2 & 0x7FFFFFFF;
 
-			Assert.That(rnd1 != rnd2);
+            Assert.That(rnd1 != rnd2);
 
-			var ts1 = uid1 >> 31;
-			var ts2 = uid2 >> 31;
+            var ts1 = uid1 >> 31;
+            var ts2 = uid2 >> 31;
 
-			var ts = (ulong)TimeUtils.Timestamp;
+            var ts = (ulong)TimeUtils.Timestamp;
 
-			Assert.That(ts1 == ts2);
-			Assert.That(ts == ts1);
+            Assert.That(ts1 == ts2);
+            Assert.That(ts == ts1);
         }
 
     }
