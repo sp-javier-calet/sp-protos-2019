bd35581fde454c993375ea8de6df0d009322cab8
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-02-03 16:00:14 +0100

    Fixed style

--- Project/Assets/Libraries/SocialPoint/Notifications/BaseNotificationServices.cs
+++ Project/Assets/Libraries/SocialPoint/Notifications/BaseNotificationServices.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using SocialPoint.Base;
 using SocialPoint.ServerSync;
 
 namespace SocialPoint.Notifications
 {
-    public abstract class BaseNotificationServices : INotificationServices {
-
+    public abstract class BaseNotificationServices : INotificationServices
+    {
         protected delegate string PollPushNotificationToken();
 
-        private const string kPushTokenKey = "notifications_push_token";
-        private MonoBehaviour _behaviour;
-        private ICommandQueue _commandQueue;
-        private string _pushToken = null;
+        const string kPushTokenKey = "notifications_push_token";
+        MonoBehaviour _behaviour;
+        ICommandQueue _commandQueue;
+        string _pushToken = null;
 
         public BaseNotificationServices(MonoBehaviour behaviour, ICommandQueue commandqueue = null)
         {
@@ -30,7 +31,7 @@ namespace SocialPoint.Notifications
             _behaviour.StartCoroutine(CheckPushNotificationToken(pollDelegate));
         }
 
-        private IEnumerator CheckPushNotificationToken(PollPushNotificationToken pollDelegate)
+        IEnumerator CheckPushNotificationToken(PollPushNotificationToken pollDelegate)
         {
             while(_pushToken == null)
             {
@@ -39,8 +40,8 @@ namespace SocialPoint.Notifications
             }
             SendPushToken(_pushToken);
         }
-        
-        private void SendPushToken(string pushToken)
+
+        void SendPushToken(string pushToken)
         {
             string currentPushToken = PlayerPrefs.GetString(kPushTokenKey);
             if(_commandQueue != null && !string.IsNullOrEmpty(pushToken) && pushToken != currentPushToken)
@@ -58,8 +59,11 @@ namespace SocialPoint.Notifications
          * Interface methods
          */
         public abstract void Schedule(Notification notif);
+
         public abstract void ClearReceived();
+
         public abstract void CancelPending();
+
         public abstract void RegisterForRemote();
     }
 }
