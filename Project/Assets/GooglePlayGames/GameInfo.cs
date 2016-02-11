8c1bd6a63c886618b6798cb7da5db8de1f2dbc0f
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-02-11 15:16:40 +0100

    fixed android google play

--- Project/Assets/GooglePlayGames/GameInfo.cs
+++ Project/Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
         private const string UnescapedRequireGooglePlus = "REQUIRE_GOOGLE_PLUS";
 
-        public const string ApplicationId = "769118799129"; // Filled in automatically
+        public const string ApplicationId = "157200987425"; // Filled in automatically
         public const string IosClientId = ""; // Filled in automatically
-        public const string WebClientId = "769118799129-gt5m3p401onn4hpnlh5lc5mtdotts6kq.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = ""; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool RequireGooglePlus()
