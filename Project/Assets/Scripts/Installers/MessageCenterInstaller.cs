ea90ce207c7e19f17e4a22c300996bf07d92c19a
sp-miguel-janer <miguel.janer@socialpoint.es>
2016-02-23 17:08:56 +0100

    Installed on BaseGame

---
+++ Project/Assets/Scripts/Installers/MessageCenterInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+using SocialPoint.AdminPanel;
+using SocialPoint.ServerMessaging;
+
+public class MessageCenterInstaller : Installer
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<IMessageCenter>().ToSingle<MessageCenter>();
+        Container.Bind<IAdminPanelConfigurer>().ToSingle<AdminPanelMessageCenter>();
+    }
+}
---
+++ Project/Assets/Sparta/SocialPoint/ServerMessaging/AdminPanel/AdminPanelMessageCenter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SocialPoint.AdminPanel;
+
+namespace SocialPoint.ServerMessaging
+{
+    public class AdminPanelMessageCenter : IAdminPanelGUI, IAdminPanelConfigurer
+    {
+        IMessageCenter _mesageCenter;
+
+        public AdminPanelMessageCenter(IMessageCenter messageCenter)
+        {
+            _mesageCenter = messageCenter;
+        }
+
+        #region IAdminPanelGUI implementation
+
+        public void OnCreateGUI(AdminPanelLayout layout)
+        {
+            layout.CreateLabel("Messages");
+            layout.CreateButton("Get Messages", () => _mesageCenter.RequestMessages());
+        }
+
+        #endregion
+
+        #region IAdminPanelConfigurer implementation
+
+        public void OnConfigure(SocialPoint.AdminPanel.AdminPanel adminPanel)
+        {
+            adminPanel.RegisterGUI("System", new AdminPanelNestedGUI("Message Center", this));
+        }
+
+        #endregion
+    }
+}
+
