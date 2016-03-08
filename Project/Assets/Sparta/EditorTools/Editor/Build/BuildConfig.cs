8c7c0f44e1c266d57dbac0242dabf0d1bca1f045
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-03-08 12:11:09 +0100

    WIP Build sets and selection menu

---
+++ Project/Assets/Sparta/EditorTools/Editor/Build/BuildConfig.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SpartaTools.Editor.Build
+{
+    public class BuildConfig : EditorWindow
+    {
+        enum Mode
+        {
+            Debug,
+            Release
+        }
+
+        struct ConfigSet
+        {
+            public string CommonFlags;
+            public string AndroidFlags;
+            public string IosFlags;
+
+            public bool UseKeytore;
+            public string KeystorePath;
+            public string KeystoreFilePassword;
+            public string KeystoreAlias;
+            public string KeystorePassword;
+
+        }
+
+        static Mode CurrentMode = Mode.Debug;
+
+        const string DebugSymbols = "ADMIN_PANELTEST"; // TODO Read from...?
+
+        static readonly BuildTargetGroup[] AvailableTargets = { BuildTargetGroup.Android, BuildTargetGroup.iOS };
+
+        ConfigSet CurrentConfig;
+
+        #region Editor options
+
+        [MenuItem("Sparta/Build/Debug", false, 1)]
+        public static void SetDebugConfig()
+        {
+            CurrentMode = Mode.Debug;
+            SetFlags(DebugSymbols);
+        }
+
+        [MenuItem("Sparta/Build/Debug", true)]
+        static bool ValidateSetDebugConfig()
+        {
+            return CurrentMode != Mode.Debug;
+        }
+
+        [MenuItem("Sparta/Build/Release", false, 2)]
+        public static void SetReleaseConfig()
+        {
+            CurrentMode = Mode.Release;
+        }
+
+        [MenuItem("Sparta/Build/Release", true)]
+        static bool ValidateSetReleaseConfig()
+        {
+            return CurrentMode != Mode.Release;
+        }
+
+        [MenuItem("Sparta/Build/Build settings...", false, 3)]
+        public static void ShowBuildSettings()
+        {
+            EditorWindow.GetWindow(typeof(BuildConfig), false, "Sparta Build Settings", true);
+        }
+
+        #endregion
+
+        static void SetFlags(string defineSymbols)
+        {
+            foreach(var target in AvailableTargets)
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(target, defineSymbols);    
+            }
+        }
+
+        void OnGUI()
+        {
+            CurrentConfig.CommonFlags = EditorGUILayout.TextField("Common Flags", CurrentConfig.CommonFlags);
+
+            EditorGUILayout.LabelField("IOS");
+            CurrentConfig.IosFlags = EditorGUILayout.TextField("Flags", CurrentConfig.IosFlags);
+
+            EditorGUILayout.LabelField("Android");
+            CurrentConfig.AndroidFlags = EditorGUILayout.TextField("Flags", CurrentConfig.AndroidFlags);
+            CurrentConfig.UseKeytore = EditorGUILayout.Toggle("Use release keystore", CurrentConfig.UseKeytore);
+            CurrentConfig.KeystorePath = EditorGUILayout.TextField("Keystore file", CurrentConfig.KeystorePath);
+            CurrentConfig.KeystoreFilePassword = EditorGUILayout.TextField("Keystore password", CurrentConfig.KeystoreFilePassword);
+            CurrentConfig.KeystoreAlias = EditorGUILayout.TextField("Alias", CurrentConfig.KeystoreAlias);
+            CurrentConfig.KeystorePassword = EditorGUILayout.TextField("Password", CurrentConfig.KeystorePassword);
+        }
+    }
+}
---
+++ Project/Assets/Sparta/EditorTools/Editor/Build/BuildSet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+namespace SpartaTools.Editor.Build
+{
+    public class BuildSet : ScriptableObject
+    {
+        public string CommonFlags;
+        public string AndroidFlags;
+        public string IosFlags;
+
+        public bool UseKeytore;
+        public string KeystorePath;
+        public string KeystoreFilePassword;
+        public string KeystoreAlias;
+        public string KeystorePassword;
+
+
+        [MenuItem( "Sparta/Create/Instance" )]
+        public static void CreateInstance() {
+            CreateAsset(typeof(BuildSet));
+        }
+
+        private static void CreateAsset(System.Type type) {
+            var asset = ScriptableObject.CreateInstance( type );
+            string path = AssetDatabase.GetAssetPath( Selection.activeObject );
+            if( path == "" )  {
+                path = "Assets";
+            } else if( Path.GetExtension( path ) != "" ) {
+                path = path.Replace( Path.GetFileName( AssetDatabase.GetAssetPath( Selection.activeObject ) ), "" );
+            }
+            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath( "Assets/BuildSet/" + type.ToString() + ".asset" );
+            AssetDatabase.CreateAsset( asset, assetPathAndName );
+            AssetDatabase.SaveAssets();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+        }
+    }
+}
