dc8d280b2e5c52add9afad4ce06255dd39fa3917
sp-miguel-janer <miguel.janer@socialpoint.es>
2016-03-10 18:30:56 +0100

    added appEvents dependency in order to restart the messages after a game restart reverted pendingDeletion management, it's needed to avoid parse repeated messages that were set to delete

--- Project/Assets/Scripts/Services/MessageCenter.cs
+++ Project/Assets/Scripts/Services/MessageCenter.cs
@@ -1,8 +1,9 @@
 ﻿using SocialPoint.ServerSync;
+using SocialPoint.AppEvents;
 
 class MessageCenter : SocialPoint.ServerMessaging.MessageCenter
 {
-    public MessageCenter(ICommandQueue commandQueue, CommandReceiver commandReceiver) : base(commandQueue, commandReceiver)
+    public MessageCenter(ICommandQueue commandQueue, CommandReceiver commandReceiver, IAppEvents appEvents) : base(commandQueue, commandReceiver, appEvents)
     {
         
     }
