5e3a18965c872ead9441a2678943f872bde0c841
Andres Barrera <andres.barrera@socialpoint.es>
2016-03-21 12:57:48 +0100

    Some guidelines improvements

--- Project/Assets/Scripts/GUI/CrossPromotion/CrossPromoCellController.cs
+++ Project/Assets/Scripts/GUI/CrossPromotion/CrossPromoCellController.cs
@@ -12,6 +12,8 @@ public class CrossPromoCellController : BaseCrossPromoCellController
     protected Image _iconImage;
     [SerializeField]
     protected Image _buttonImage;
+    [SerializeField]
+    protected RectTransform _buttonContainer;
 
     //Guideline measures
     protected static float _defaultBannerWidth = 800f;
@@ -20,7 +22,7 @@ public class CrossPromoCellController : BaseCrossPromoCellController
     protected static float _iconSize = 128f;
     protected static float _iconMargin = 10f;
     protected static float _buttonHeight = 64f;
-    protected static float _buttonLeftMarginToCenter = 584f;
+    protected static float _buttonLeftMarginToCenterPercent = 0.73f;
     protected static float _buttonBottomMarginToCenterPercent = 0.2f;
 
     public override void InitCell(SocialPoint.CrossPromotion.CrossPromotionManager crossPromoManager, BaseCrossPromoPopupController popupController, int bannerId, int position)
@@ -40,18 +42,56 @@ public class CrossPromoCellController : BaseCrossPromoCellController
         cellLayout.preferredHeight = cellLayout.minHeight = height;
 
         SetIconSizeAndPos(width);
-        //TODO: New Game Size, Button Size
+        SetNewGameFlagSizeAndPos(width);
+        SetButtonSizeAndPos(width, height);
     }
 
     protected void SetIconSizeAndPos(float width)
     {
-        float newMargin = width * _iconMargin / _defaultBannerWidth;
-        float newSize = width * _iconSize / _defaultBannerWidth;
+        float scale = width / _defaultBannerWidth;
+        float newMargin = _iconMargin * scale;
+        float newSize = _iconSize * scale;
         RectTransform iconRectTransform = _iconImage.GetComponent<RectTransform>();
         iconRectTransform.anchorMin = new Vector2(0.0f, 1.0f);
         iconRectTransform.anchorMax = new Vector2(0.0f, 1.0f);
         iconRectTransform.offsetMin = new Vector2(newMargin, -(newMargin + newSize));
         iconRectTransform.offsetMax = new Vector2(newMargin + newSize, -newMargin);
+    }
+
+    protected void SetNewGameFlagSizeAndPos(float width)
+    {
+        //Use icon measures for bottom aligment
+        float scale = width / _defaultBannerWidth;
+        float newMargin = _iconMargin * scale;
+        float newSize = _iconSize * scale;
+        float bottomOffset = newMargin + newSize;
+        //Use preset offsets in prefab to calculate new scaled offsets
+        RectTransform newGameRectTransform = _newGameFlag.GetComponent<RectTransform>();
+        float scalePercent = Mathf.Abs(bottomOffset / newGameRectTransform.offsetMin.y);
+        float topOffset = Mathf.Abs(newGameRectTransform.offsetMax.y * scalePercent);
+        float rightOffset = Mathf.Abs(newGameRectTransform.offsetMax.x * scalePercent);
+        float leftOffset = Mathf.Abs(newGameRectTransform.offsetMin.x * scalePercent);
+        newGameRectTransform.anchorMin = new Vector2(1.0f, 1.0f);
+        newGameRectTransform.anchorMax = new Vector2(1.0f, 1.0f);
+        newGameRectTransform.offsetMin = new Vector2(-leftOffset, -bottomOffset);
+        newGameRectTransform.offsetMax = new Vector2(rightOffset, topOffset);
+    }
+
+    protected void SetButtonSizeAndPos(float width, float height)
+    {
+        //Current button image is 128x128, but visual sprite is only 80x72
+        float imageVerticalSize = 128;
+        float imageVisualVerticalSize = 72;
+
+        float scale = width / _defaultBannerWidth;
+        float newButtonHeight = _buttonHeight * scale;//Visual image must be this size
+        float requiredSize = imageVerticalSize * newButtonHeight / imageVisualVerticalSize;
+        float verticalOffset = requiredSize * 0.5f;
+        float horizontalOffset = verticalOffset * 2.5f;
+        _buttonContainer.anchorMin = new Vector2(_buttonLeftMarginToCenterPercent, _buttonBottomMarginToCenterPercent);
+        _buttonContainer.anchorMax = new Vector2(_buttonLeftMarginToCenterPercent, _buttonBottomMarginToCenterPercent);
+        _buttonContainer.offsetMin = new Vector2(-horizontalOffset, -verticalOffset);
+        _buttonContainer.offsetMax = new Vector2(horizontalOffset, verticalOffset);
     }
 
     protected override void CheckVisibility()
