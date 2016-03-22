70ff8ae8ad5922dcdb52ab59d1d290d63b23e6c8
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-03-22 11:40:55 +0100

    Added defined symbols

--- Project/Assets/Sparta/EditorTools/Editor/Build/SpartaCompiler.cs
+++ Project/Assets/Sparta/EditorTools/Editor/Build/SpartaCompiler.cs
@@ -13,7 +13,7 @@ namespace SpartaTools.Editor.Build
         [MenuItem("Sparta/Build/Sparta libraries", false, 001)]
         public static void CompileSparta()
         {
-            Compile(Path.Combine(Application.dataPath, "../Sources/Sparta/SocialPoint"));
+            Compile(Path.Combine(Application.dataPath, "../Sources/Sparta/SocialPoint"), true);
         }
 
         static string InstallationPath
@@ -46,7 +46,7 @@ namespace SpartaTools.Editor.Build
             }
         }
 
-        static string GetBuildCommand(string dllName, List<string> files, List<string> dependencies)
+        static string GetBuildCommand(string dllName, List<string> files, List<string> dependencies, List<string> defines)
         {
             var filesList = new StringBuilder();
             foreach(var f in files)
@@ -60,16 +60,23 @@ namespace SpartaTools.Editor.Build
                 depList.Append("/reference:\"").Append(d).Append("\" ");
             }
 
-            var command = string.Format("/t:\"library\" /out:\"{0}\" {1} {2}", Path.Combine(Application.dataPath, Path.Combine(BinariesFolderPath, dllName)), filesList, depList);
+            var defList = new StringBuilder();
+            foreach(var d in defines)
+            {
+                defList.Append("/define:\"").Append(d).Append("\" ");
+            }
+
+            var command = string.Format("/t:\"library\" /out:\"{0}\" {1} {2} {3}", Path.Combine(Application.dataPath, Path.Combine(BinariesFolderPath, dllName)), filesList, depList, defList);
             Debug.Log(command);
             return command;
         }
 
-        static void Compile(string modulePath)
+        static void Compile(string modulePath, bool editorAssembly)
         {
             var path = Application.dataPath;
             var filesToCompile = new List<string>();
 
+            // Assembly files
             string[] allFiles = Directory.GetFiles(modulePath, "*.cs", SearchOption.AllDirectories);
             foreach(var f in allFiles)
             {   
@@ -79,24 +86,41 @@ namespace SpartaTools.Editor.Build
                 }
             }
 
+            // Binary ependencies
             var dependencies = new List<string>();
             var ManagedLibrariesPath = Path.Combine(InstallationPath, UnityManagedLibrariesPath);
             var ExtensionPath = Path.Combine(InstallationPath, UnityExtensionsPath);
             dependencies.Add(Path.Combine(ManagedLibrariesPath, "UnityEngine.dll"));
             dependencies.Add(Path.Combine(ExtensionPath, "GUISystem/UnityEngine.UI.dll"));
+            if(editorAssembly)
+            {
+                dependencies.Add(Path.Combine(ExtensionPath, "UnityEditor.dll"));
+            }
 
             string[] internalDependencies = Directory.GetFiles(modulePath, "*.dll", SearchOption.AllDirectories);
             foreach(var d in internalDependencies)
             {   
-                if(!d.Contains("/Editor/"))
+                if(!d.Contains("/Editor/") || editorAssembly)
                 {
                     dependencies.Add(d);
                 }
             }
 
+            // Defines
+            var defines = new List<string>();
+            defines.Add("UNITY_5_3");
+
+            if(editorAssembly)
+            {
+                defines.Add("UNITY_EDITOR");
+            }
+            
+            //defines.Add("UNITY_ANDROID");
+            defines.Add("UNITY_IOS");
+
             try
             {
-                int code = NativeConsole.RunProcess(Compiler, GetBuildCommand("Sparta.dll", filesToCompile, dependencies), path, output => {
+                int code = NativeConsole.RunProcess(Compiler, GetBuildCommand("Sparta.dll", filesToCompile, dependencies, defines), path, output => {
                     Debug.Log(output);
                 });
 
