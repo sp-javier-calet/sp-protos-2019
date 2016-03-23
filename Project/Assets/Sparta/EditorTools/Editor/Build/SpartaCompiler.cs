448f4d1e31cb78e973ed68c94bd0d42fb8b261d1
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-03-23 11:52:01 +0100

    Removed compilation by path. Fixed mono libs path.

--- Project/Assets/Sparta/EditorTools/Editor/Build/SpartaCompiler.cs
+++ Project/Assets/Sparta/EditorTools/Editor/Build/SpartaCompiler.cs
@@ -11,13 +11,6 @@ namespace SpartaTools.Editor.Build
 {
     public class SpartaCompiler : EditorWindow
     {
-        [MenuItem("Sparta/Build/Sparta libraries", false, 001)]
-        public static void CompileSparta()
-        {
-            //Compile(Path.Combine(Application.dataPath, "../Sources/Sparta/SocialPoint"), true);
-            Compile(Path.Combine(Application.dataPath, "Sparta/SocialPoint"), true);
-        }
-
         [MenuItem("Sparta/Build/Sparta Modules", false, 001)]
         public static void CompileModule()
         {
@@ -34,6 +27,7 @@ namespace SpartaTools.Editor.Build
         }
 
         const string UnityExtensionsPath = "Unity.app/Contents/UnityExtensions/Unity/";
+        const string MonoFrameworkLibrariesPath = "Unity.app/Contents/Frameworks/Mono/lib/mono/2.0/";
         const string UnityManagedLibrariesPath = "Unity.app/Contents/Frameworks/Managed/";
         const string UnityMonoPath = "Unity.app/Contents/Frameworks/Mono/bin/gmcs";
         //const string BinariesFolderPath = "Sparta/Binaries";
@@ -66,6 +60,10 @@ namespace SpartaTools.Editor.Build
             var depList = new StringBuilder();
             foreach(var d in dependencies)
             {
+                if(!File.Exists(d))
+                {
+                    throw new Exception("Dependency file '" + d + "' not found");
+                }
                 depList.Append("/reference:\"").Append(d).Append("\" ");
             }
 
@@ -75,75 +73,9 @@ namespace SpartaTools.Editor.Build
                 defList.Append("/define:\"").Append(d).Append("\" ");
             }
 
-            var command = string.Format("/t:\"library\" /out:\"{0}\" {1} {2} {3}", dllPath, filesList, depList, defList);
+            var command = string.Format("/target:\"library\" /out:\"{0}\" {1} {2} {3}", dllPath, filesList, depList, defList);
             Debug.Log(command);
             return command;
-        }
-
-        static void Compile(string modulePath, bool editorAssembly)
-        {
-            var path = Application.dataPath;
-            var filesToCompile = new List<string>();
-
-            // Assembly files
-            string[] allFiles = Directory.GetFiles(modulePath, "*.cs", SearchOption.AllDirectories);
-            foreach(var f in allFiles)
-            {   
-                if(!f.Contains("/Editor/"))
-                {
-                    filesToCompile.Add(f);
-                }
-            }
-
-            // Binary ependencies
-            var dependencies = new List<string>();
-            var ManagedLibrariesPath = Path.Combine(InstallationPath, UnityManagedLibrariesPath);
-            var ExtensionPath = Path.Combine(InstallationPath, UnityExtensionsPath);
-            dependencies.Add(Path.Combine(ManagedLibrariesPath, "UnityEngine.dll"));
-            dependencies.Add(Path.Combine(ExtensionPath, "GUISystem/UnityEngine.UI.dll"));
-            if(editorAssembly)
-            {
-                dependencies.Add(Path.Combine(ManagedLibrariesPath, "UnityEditor.dll"));
-            }
-
-            string[] internalDependencies = Directory.GetFiles(modulePath, "*.dll", SearchOption.AllDirectories);
-            foreach(var d in internalDependencies)
-            {   
-                if(!d.Contains("/Editor/") || editorAssembly)
-                {
-                    dependencies.Add(d);
-                }
-            }
-
-            // Defines
-            var defines = new List<string>();
-            defines.Add("UNITY_5_3");
-
-            if(editorAssembly)
-            {
-                defines.Add("UNITY_EDITOR");
-            }
-            
-            //defines.Add("UNITY_ANDROID");
-            defines.Add("UNITY_IOS");
-
-            try
-            {
-                int code = NativeConsole.RunProcess(Compiler, GetBuildCommand("Sparta.dll", filesToCompile, dependencies, defines), path, output => {
-                    Debug.Log(output);
-                });
-
-                if(code != 0)
-                {
-                    Debug.LogError("Error while compiling library");
-                }
-            }
-            catch(System.ComponentModel.Win32Exception e)
-            {
-                var v = e.Message;
-
-                Debug.LogError(e);
-            }
         }
 
         static void Compile(Module module, BuildTarget target, bool editorAssembly)
@@ -165,8 +97,11 @@ namespace SpartaTools.Editor.Build
             // Binary ependencies
             var dependencies = new List<string>();
             var ManagedLibrariesPath = Path.Combine(InstallationPath, UnityManagedLibrariesPath);
+            var MonoLibrariesPath = Path.Combine(InstallationPath, MonoFrameworkLibrariesPath);
             var ExtensionPath = Path.Combine(InstallationPath, UnityExtensionsPath);
             dependencies.Add(Path.Combine(ManagedLibrariesPath, "UnityEngine.dll"));
+            dependencies.Add(Path.Combine(MonoLibrariesPath, "System.Xml.Linq.dll"));
+            dependencies.Add(Path.Combine(MonoLibrariesPath, "System.Xml.dll"));
             dependencies.Add(Path.Combine(ExtensionPath, "GUISystem/UnityEngine.UI.dll"));
             if(editorAssembly)
             {
@@ -306,12 +241,20 @@ namespace SpartaTools.Editor.Build
                 if(GUILayout.Button("Compile " + module.Name + " for Android"))
                 {
                     Compile(module, BuildTarget.Android, false);
+                }
+
+                if(GUILayout.Button("Compile " + module.Name + " for Android - Editor"))
+                {
                     Compile(module, BuildTarget.Android, true);
                 }
 
                 if(GUILayout.Button("Compile " + module.Name + " for iOS"))
                 {
                     Compile(module, BuildTarget.iOS, false);
+                }
+
+                if(GUILayout.Button("Compile " + module.Name + " for iOS - Editor"))
+                {
                     Compile(module, BuildTarget.iOS, true);
                 }
             }
