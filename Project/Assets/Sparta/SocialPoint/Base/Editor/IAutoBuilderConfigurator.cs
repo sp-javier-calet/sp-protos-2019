0bcaff690e051db84e2675c601a45f69560ba8ab
Marcos Herraiz <marcos.herraiz@socialpoint.es>
2016-03-24 10:27:16 +0100

    policy

--- Project/Assets/Sparta/SocialPoint/Base/Editor/IAutoBuilderConfigurator.cs
+++ Project/Assets/Sparta/SocialPoint/Base/Editor/IAutoBuilderConfigurator.cs
@@ -1,53 +1,52 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using UnityEditor;
 using System;
 
-public class AutoBuilderConfiguration
+namespace SocialPoint.Base
 {
-    public string[] Levels { get; set; }
-
-    public string LocationPathName { get; set; }
-
-    public BuildTarget Target { get; set; }
-
-    public BuildOptions Options { get; set; }
-
-    public string AndroidKeyStoreName { get; set; }
-
-    public string AndroidKeyStorePass { get; set; }
-
-    public string AndroidKeyAliasName { get; set; }
-
-    public string AndroidKeyAliasPass { get; set; }
-
-    public string BundleIdentifier { get; set; }
-}
-
-public class AutoBuilderConfiguratorFactory : Attribute
-{
-}
-
-public interface IAutoBuilderConfigurator
-{
-    AutoBuilderConfiguration Configure(AutoBuilderConfiguration baseConfiguration);
-
-    void OnBuildCompleted(AutoBuilderConfiguration configuration, string result);
-}
-
-public static class IAutoBuilderConfiguratorExtensions
-{
-    public static void BuildWithConfigure(this IAutoBuilderConfigurator configurator, AutoBuilderConfiguration baseConfiguration)
+    public class AutoBuilderConfiguration
     {
-        baseConfiguration = configurator.Configure(baseConfiguration);
-        configurator.Build(baseConfiguration);
+        public string[] Levels { get; set; }
+
+        public string LocationPathName { get; set; }
+
+        public BuildTarget Target { get; set; }
+
+        public BuildOptions Options { get; set; }
+
+        public string AndroidKeyStoreName { get; set; }
+
+        public string AndroidKeyStorePass { get; set; }
+
+        public string AndroidKeyAliasName { get; set; }
+
+        public string AndroidKeyAliasPass { get; set; }
+
+        public string BundleIdentifier { get; set; }
     }
 
-    public static void Build(this IAutoBuilderConfigurator configurator, AutoBuilderConfiguration baseConfiguration)
+    public class AutoBuilderConfiguratorFactory : Attribute
     {
-        string result = BuildPipeline.BuildPlayer(baseConfiguration.Levels, baseConfiguration.LocationPathName, baseConfiguration.Target, baseConfiguration.Options);
-        configurator.OnBuildCompleted(baseConfiguration, result);
+    }
+
+    public interface IAutoBuilderConfigurator
+    {
+        AutoBuilderConfiguration Configure(AutoBuilderConfiguration baseConfiguration);
+
+        void OnBuildCompleted(AutoBuilderConfiguration configuration, string result);
+    }
+
+    public static class IAutoBuilderConfiguratorExtensions
+    {
+        public static void BuildWithConfigure(this IAutoBuilderConfigurator configurator, AutoBuilderConfiguration baseConfiguration)
+        {
+            baseConfiguration = configurator.Configure(baseConfiguration);
+            configurator.Build(baseConfiguration);
+        }
+
+        public static void Build(this IAutoBuilderConfigurator configurator, AutoBuilderConfiguration baseConfiguration)
+        {
+            string result = BuildPipeline.BuildPlayer(baseConfiguration.Levels, baseConfiguration.LocationPathName, baseConfiguration.Target, baseConfiguration.Options);
+            configurator.OnBuildCompleted(baseConfiguration, result);
+        }
     }
 }
