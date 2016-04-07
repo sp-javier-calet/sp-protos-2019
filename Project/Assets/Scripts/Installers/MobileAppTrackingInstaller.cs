a65a93da0b1726641ac61cbadfc9659134180c8c
sp-miguel-janer <miguel.janer@socialpoint.es>
2016-04-07 16:38:29 +0200

    added services and installer for basegame

---
+++ Project/Assets/Scripts/Installers/MobileAppTrackingInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Zenject;
+using SocialPoint.Marketing;
+
+public class MobileAppTrackingInstaller : MonoInstaller
+{
+    [Serializable]
+    public class SettingsData
+    {
+        public bool ActiveOnIOS;
+        public string AdvertiserID;
+        public string ConversionKey;
+        public bool ActiveOnAndroid;
+    }
+
+    public SettingsData Settings = new SettingsData();
+
+    public override void InstallBindings()
+    {
+        #if UNITY_ANDROID
+        if(!Settings.ActiveOnAndroid) return;
+        #elif UNITY_IOS
+        if(!Settings.ActiveOnIOS) return;
+        #endif
+        Container.Bind<IMarketingTracker>().ToSingleMethod<SocialPointMobileAppTracking>(CreateMobileAppTracking);
+        Container.Bind<IDisposable>().ToSingleMethod<SocialPointMobileAppTracking>(CreateMobileAppTracking);
+    }
+
+    SocialPointMobileAppTracking CreateMobileAppTracking(InjectContext ctx)
+    {
+        var tracker = new SocialPointMobileAppTracking();
+        tracker.AdvertiserID = Settings.AdvertiserID;
+        tracker.ConversionKey = Settings.ConversionKey;
+        tracker.Init();
+        return tracker;
+    }
+}
+
+
