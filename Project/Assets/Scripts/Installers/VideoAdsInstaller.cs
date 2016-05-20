d7eade7cf3baadd05f10eb2506fc45b2f14c8505
sp-miguel-janer <miguel.janer@socialpoint.es>
2016-05-20 11:30:06 +0200

    added zenject installers for base game modified android manifest changed order of post process to fix error

---
+++ Project/Assets/Scripts/Installers/VideoAdsInstaller.cs
@@ -0,0 +1,37 @@
+﻿using SocialPoint.AdminPanel;
+using SocialPoint.VideoAds;
+using System;
+using Zenject;
+
+public class VideoAdsInstaller : MonoInstaller
+{
+    [Serializable]
+    public class SettingsData
+    {
+        public string AppID;
+        public string UserID;
+    }
+
+    public SettingsData iOSSettings;
+    public SettingsData AndroidSettings;
+
+    public override void InstallBindings()
+    {
+        SettingsData settings;
+        #if UNITY_IOS
+        settings = iOSSettings;
+        #elif UNITY_ANDROID
+        settings = AndroidSettings;
+        #endif
+
+        Container.BindInstance("videoads_appid", settings.AppID);
+        Container.BindInstance("videoads_userid", settings.UserID);
+        Container.Bind<VideoAds>().ToSingle();
+        Container.Bind<IVideoAdsManager>().ToLookup<VideoAds>();
+        Container.Bind<IDisposable>().ToLookup<IVideoAdsManager>();
+        Container.Bind<IAdminPanelConfigurer>().ToSingle<AdminPanelVideoAds>();
+    }
+    
+}
+
+
---
+++ Project/Assets/Scripts/Services/VideoAds.cs
@@ -0,0 +1,26 @@
+﻿using SocialPoint.VideoAds;
+using System;
+using Zenject;
+
+public class VideoAds : SocialPointVideoAdsManager
+{
+    [InjectOptional("videoads_appid")]
+    string userLevelUntilPrompt
+    {
+        set
+        {
+            AppId = value;
+        }
+    }
+
+    [InjectOptional("videoads_userid")]
+    string maxPromptsPerDay
+    {
+        set
+        {
+            UserId = value;
+        }
+    }
+}
+
+
