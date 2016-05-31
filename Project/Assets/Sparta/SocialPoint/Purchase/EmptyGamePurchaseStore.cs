4d612bb1ea0d4fcc48589eacbd94f994918d64c2
sp-xavier-delafuente <xavier.fuente@socialpoint.es>
2016-05-31 12:24:31 +0200

    Separate interfaces from implementation to have a better model-view separation

---
+++ Project/Assets/Sparta/SocialPoint/Purchase/EmptyGamePurchaseStore.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using SocialPoint.Attributes;
+
+namespace SocialPoint.Purchase
+{
+    //TODO: Verify behaviour for desired empty store
+    public class EmptyGamePurchaseStore : IGamePurchaseStore
+    {
+        Product[] _productList = new Product[0];
+        bool _productsLoaded = false;
+        PurchaseCompletedDelegate _purchaseCompleted;
+
+        public event ProductsUpdatedDelegate ProductsUpdated;
+        public event PurchaseUpdatedDelegate PurchaseUpdated;
+
+        public void Setup(AttrDic settings)
+        {
+            //Empty
+        }
+
+        public Product[] ProductList { get { return _productList; } }
+
+        public bool HasProductsLoaded { get { return _productsLoaded; } }
+
+        public void LoadProducts(string[] productIds)
+        {
+            if(ProductsUpdated != null)
+            {
+                ProductsUpdated(LoadProductsState.Success);
+            }
+            _productsLoaded = true;
+        }
+
+        public void SetProductMockList(IEnumerable<Product> productMockList)
+        {
+            //TODO: Allow mock products for this class?
+        }
+
+        public bool Purchase(string productId)
+        {
+            if(PurchaseUpdated != null)
+            {
+                PurchaseUpdated(PurchaseState.PurchaseFailed, productId);
+            }
+            if(_purchaseCompleted != null)
+            {
+                _purchaseCompleted(new Receipt(), PurchaseResponseType.Complete);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Registers the purchase completed delegate.
+        /// May throw an exception if another delegate is already registered.
+        /// </summary>
+        /// <param name="pDelegate">Delegate to register.</param>
+        public void RegisterPurchaseCompletedDelegate(PurchaseCompletedDelegate pDelegate)
+        {
+            if(_purchaseCompleted != null && _purchaseCompleted != pDelegate)
+            {
+                throw new Exception("Only one delegate allowed!");
+            }
+            _purchaseCompleted = pDelegate;
+        }
+
+        /// <summary>
+        /// Check if the current registered delegate matches with the param and unregister it if true
+        /// </summary>
+        /// <param name="pDelegate">Delegate to unregister.</param>
+        public void UnregisterPurchaseCompletedDelegate(PurchaseCompletedDelegate pDelegate)
+        {
+            if(_purchaseCompleted == pDelegate)
+            {
+                _purchaseCompleted = null;
+            }
+        }
+
+        public void ForceFinishPendingTransactions()
+        {
+        }
+    }
+}
---
+++ Project/Assets/Sparta/SocialPoint/Purchase/IGamePurchaseStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using SocialPoint.Attributes;
+
+namespace SocialPoint.Purchase
+{
+    public class PurchaseGameInfo
+    {
+        public string OfferName;
+        public string ResourceName;
+        public int ResourceAmount;
+        public AttrDic AdditionalData;
+    }
+
+    public delegate PurchaseGameInfo PurchaseCompletedDelegate(Receipt receipt, PurchaseResponseType response);
+
+    public interface IGamePurchaseStore
+    {
+        event ProductsUpdatedDelegate ProductsUpdated;
+        event PurchaseUpdatedDelegate PurchaseUpdated;
+
+        //Change desired settings. Use with PlatformPurchaseSettings
+        void Setup(AttrDic settings);
+
+        Product[] ProductList { get; }
+
+        bool HasProductsLoaded { get; }
+
+        void LoadProducts(string[] productIds);
+
+        void SetProductMockList(IEnumerable<Product> productMockList);
+
+        bool Purchase(string productId);
+
+        void RegisterPurchaseCompletedDelegate(PurchaseCompletedDelegate pDelegate);
+
+        void UnregisterPurchaseCompletedDelegate(PurchaseCompletedDelegate pDelegate);
+
+        void ForceFinishPendingTransactions();
+    }
+
+}
