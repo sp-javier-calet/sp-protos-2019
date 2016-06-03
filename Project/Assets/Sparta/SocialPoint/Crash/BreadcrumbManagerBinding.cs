fffaa4d36c467100dfba7dac85b9b065d7054f8e
Andres Barrera <andres.barrera@socialpoint.es>
2016-06-03 10:01:31 +0200

    Ifdefs for Unity Editor

--- Project/Assets/Sparta/SocialPoint/Crash/BreadcrumbManagerBinding.cs
+++ Project/Assets/Sparta/SocialPoint/Crash/BreadcrumbManagerBinding.cs
@@ -16,6 +16,7 @@ namespace SocialPoint.Crash
 
         /* Native plugin interface */
 
+        #if !UNITY_EDITOR
         [DllImport(PluginModuleName)]
         private static extern UIntPtr SPUnityBreadcrumbManager_Get();
 
@@ -33,38 +34,53 @@ namespace SocialPoint.Crash
 
         [DllImport(PluginModuleName)]
         private static extern void SPUnityBreadcrumbManager_Clear();
+        #endif
 
 
         /* Game interface */
 
         public static UIntPtr GetNativeObjectPointer()
         {
+            #if !UNITY_EDITOR
             return SPUnityBreadcrumbManager_Get();
+            #else
+            return default(UIntPtr);
+            #endif
         }
 
         public static void SetMaxLogs(int maxLogs)
         {
+            #if !UNITY_EDITOR
             SPUnityBreadcrumbManager_SetMaxLogs(maxLogs);
+            #endif
         }
 
         public static void SetDumpFilePath(string directory, string file)
         {
+            #if !UNITY_EDITOR
             SPUnityBreadcrumbManager_SetDumpFilePath(directory, file);
+            #endif
         }
 
         public static void Log(string info)
         {
+            #if !UNITY_EDITOR
             SPUnityBreadcrumbManager_Log(info);
+            #endif
         }
 
         public static void DumpToFile()
         {
+            #if !UNITY_EDITOR
             SPUnityBreadcrumbManager_DumpToFile();
+            #endif
         }
 
         public static void Clear(int maxLogs)
         {
+            #if !UNITY_EDITOR
             SPUnityBreadcrumbManager_Clear();
+            #endif
         }
     }
 }
