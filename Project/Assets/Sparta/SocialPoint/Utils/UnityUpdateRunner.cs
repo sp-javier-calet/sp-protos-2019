80c9cff6d01dc4caaa0faa0615cc0299bb7c2902
Ivan Hernández <ivan.hernandez@socialpoint.es>
2016-06-15 17:55:23 +0200

    disposes not required (using statement does the dispose).

--- Project/Assets/Sparta/SocialPoint/Utils/UnityUpdateRunner.cs
+++ Project/Assets/Sparta/SocialPoint/Utils/UnityUpdateRunner.cs
@@ -269,16 +269,13 @@ namespace SocialPoint.Utils
                     {
                         cbk(null, new Error(www.error));
                     }
-                    www.Dispose();
                     yield break;
                 }
                 if(cbk == null)
                 {
-                    www.Dispose();
                     yield break;
                 }
                 var bundle = www.assetBundle;
-                www.Dispose();
                 AssetBundleRequest req;
                 if(string.IsNullOrEmpty(def.Name))
                 {
