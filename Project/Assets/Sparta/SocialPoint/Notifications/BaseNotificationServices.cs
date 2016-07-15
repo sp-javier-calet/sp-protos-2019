f8cf5040698d5ee511585450c1e2e40309621227
Andres Barrera <andres.barrera@socialpoint.es>
2016-07-15 13:41:41 +0200

    Added params needed for backend to validate with new system

--- Project/Assets/Sparta/SocialPoint/Notifications/BaseNotificationServices.cs
+++ Project/Assets/Sparta/SocialPoint/Notifications/BaseNotificationServices.cs
@@ -72,15 +72,14 @@ namespace SocialPoint.Notifications
                     return;
                 }
 
-                //*** TEST
-                /*_commandQueue.Add(new PushEnabledCommand(pushTokenToSend, UserAllowsNofitication), (data, err) => {
+                _commandQueue.Add(new PushEnabledCommand(pushTokenToSend, UserAllowsNofitication), (data, err) => {
                     if(Error.IsNullOrEmpty(err))
                     {
                         PlayerPrefs.SetString(kPushTokenKey, _pushToken);
                         PlayerPrefs.SetInt(kPlayerAllowsNotificationKey, UserAllowsNofitication ? 1 : 0);
                         PlayerPrefs.Save();
                     }
-                });*/
+                });
             }
         }
 
