ac0ae186e5b908e6f426249bb3192abf5297c864
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-07-18 17:14:27 +0200

    Moved ProjectEditor to a separated class.

--- Project/Assets/Sparta/EditorTools/Editor/Build/XcodeEditor/XcodeProject.cs
+++ Project/Assets/Sparta/EditorTools/Editor/Build/XcodeEditor/XcodeProject.cs
@@ -6,20 +6,6 @@ using UnityEditor.iOS.Xcode;
 namespace SpartaTools.Editor.Build.XcodeEditor
 {
     /// <summary>
-    /// Public interface for the Editor class
-    /// There is only a implentation, but we need to hide some details 
-    /// and public accessors to other classes.
-    /// </summary>
-    public abstract class XCodeProjectEditor
-    {
-        public abstract void AddFile(string relativePath);
-
-        public abstract void AddFramework(string path, bool weak = false);
-
-        public abstract void Commit();
-    }
-
-    /// <summary>
     /// Xcode project wrapper class.
     /// </summary>
     public class XcodeProject
---
+++ Project/Assets/Sparta/EditorTools/Editor/Build/XcodeEditor/XcodeProjectEditor.cs
@@ -0,0 +1,16 @@
+﻿namespace SpartaTools.Editor.Build.XcodeEditor
+{
+    /// <summary>
+    /// Public interface for the Editor class
+    /// There is only a implentation, but we need to hide some details 
+    /// and public accessors to other classes.
+    /// </summary>
+    public abstract class XCodeProjectEditor
+    {
+        public abstract void AddFile(string relativePath);
+
+        public abstract void AddFramework(string path, bool weak = false);
+
+        public abstract void Commit();
+    }
+}
