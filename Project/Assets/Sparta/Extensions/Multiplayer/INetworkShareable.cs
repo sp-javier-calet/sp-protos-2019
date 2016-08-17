f540d4b70efafecb69acb9a63f2a199a50745a3b
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-08-17 16:43:19 +0200

    added INetworkShareable interface to simplify serialization of action and events

---
+++ Project/Assets/Sparta/Extensions/Multiplayer/INetworkShareable.cs
@@ -0,0 +1,29 @@
+﻿using SocialPoint.IO;
+
+namespace SocialPoint.Multiplayer
+{
+    public interface INetworkShareable
+    {
+        void Deserialize(IReader reader);
+        void Serialize(IWriter writer);
+    }
+
+    public class NetworkShareableParser<T> : SimpleParser<T> where T: INetworkShareable, new()
+    {
+        public override T Parse(IReader reader)
+        {
+            var obj = new T();
+            obj.Deserialize(reader);
+            return obj;
+        }
+    }
+
+    public class NetworkShareableSerializer<T> : SimpleSerializer<T> where T: INetworkShareable
+    {
+        public override void Serialize(T newObj, IWriter writer)
+        {
+            newObj.Serialize(writer);
+        }
+    }
+    
+}
