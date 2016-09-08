c96a1204c2f88d770b24568c80f483373a9df8ec
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-09-08 09:56:23 +0200

    working lockstep example

---
+++ Project/Assets/Examples/Lockstep/GameLockstepClientBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using SocialPoint.Lockstep;
+using SocialPoint.Dependency;
+using SocialPoint.Utils;
+using FixMath.NET;
+
+public class GameLockstepClientBehaviour : MonoBehaviour, IPointerClickHandler
+{
+    ClientLockstepController _lockstep;
+    LockstepModel _model;
+
+    [SerializeField]
+    Slider _manaSlider;
+
+    [SerializeField]
+    GameObject _unitPrefab;
+
+	void Start()
+    {
+        _lockstep = ServiceLocator.Instance.Resolve<ClientLockstepController>();
+        _lockstep.PendingCommandAdded += AutoConfirmCommand;
+        _lockstep.NeedsTurnConfirmation = false;
+        _model = ServiceLocator.Instance.Resolve<LockstepModel>();
+        _model.OnInstantiate += OnInstantiate;
+        _lockstep.Start(TimeUtils.TimestampMilliseconds);
+	}
+
+    void OnDestroy()
+    {
+        _lockstep.PendingCommandAdded -= AutoConfirmCommand;
+        _model.OnInstantiate -= OnInstantiate;
+    }
+
+    void AutoConfirmCommand(ILockstepCommand c)
+    {
+        _lockstep.AddConfirmedCommand(c);
+    }
+
+    void OnInstantiate(Fix64 x, Fix64 y, Fix64 z)
+    {
+        SocialPoint.ObjectPool.ObjectPool.Spawn(_unitPrefab, transform, 
+            new Vector3((float)x, (float)y, (float)z), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        _manaSlider.value = _model.ManaView;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var p = eventData.pointerPressRaycast.worldPosition;
+        _lockstep.AddPendingCommand(new ClickCommand(
+            (Fix64)p.x, (Fix64)p.y, (Fix64)p.z,
+            _lockstep.ExecutionTurn, _model));
+    }
+}
---
+++ Project/Assets/Examples/Lockstep/LockstepModel.cs
@@ -0,0 +1,58 @@
+﻿using SocialPoint.Utils;
+using System;
+using FixMath.NET;
+
+public class LockstepModel : ISimulateable
+{
+    long _mana = 0;
+    long _lastTimestamp;
+    int _nextObjectId = 0;
+
+    const long ManaSpeed = 2;
+    const long MaxMana = 10000;
+
+    const long UnitCost = 7000;
+
+    public event Action<Fix64, Fix64, Fix64> OnInstantiate;
+
+    public float ManaView
+    {
+        get
+        {
+            return ((float)_mana)/(float)MaxMana ;
+        }
+    }
+
+    public bool OnClick(Fix64 x, Fix64 y, Fix64 z)
+    {
+        if(_mana > UnitCost)
+        {
+            if(OnInstantiate != null)
+            {
+                OnInstantiate(x, y, z);
+            }
+            _nextObjectId++;
+            _mana -= UnitCost;
+        }
+        return true;
+    }
+        
+    public void Simulate(long timestamp)
+    {
+        var dt = timestamp - _lastTimestamp;
+        _mana += dt*ManaSpeed;
+        if(_mana > MaxMana)
+        {
+            _mana = MaxMana;
+        }
+        _lastTimestamp = timestamp;
+    }        
+
+    public long KeyTimestamp
+    {
+        get
+        {
+            return 0;
+        }
+    }
+}
