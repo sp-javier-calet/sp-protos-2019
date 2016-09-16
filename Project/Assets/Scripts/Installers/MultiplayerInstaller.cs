a2488d70d0cd4be2ec80f60ab390a3b0f1dfb294
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-09-16 08:53:55 +0200

    separated network from multiplayer

--- Project/Assets/Scripts/Installers/MultiplayerInstaller.cs
+++ Project/Assets/Scripts/Installers/MultiplayerInstaller.cs
@@ -7,21 +7,9 @@ using SocialPoint.AdminPanel;
 
 public class MultiplayerInstaller : Installer
 {
-
-    public enum MultiplayerTech
-    {
-        Local,
-        Unet,
-        Photon
-    }
-
     [Serializable]
     public class SettingsData
     {
-        public MultiplayerTech Tech = MultiplayerTech.Local;
-        public string ServerAddress = UnetNetworkClient.DefaultServerAddr;
-        public int ServerPort = UnetNetworkServer.DefaultPort;
-        public PhotonNetworkConfig PhotonConfig;
         public string MultiplayerParentTag = "MultiplayerParent";
     }
 
@@ -29,42 +17,10 @@ public class MultiplayerInstaller : Installer
 
     public override void InstallBindings()
     {
-        if(Settings.Tech == MultiplayerTech.Local)
-        {
-            Container.Rebind<LocalNetworkServer>().ToMethod<LocalNetworkServer>(CreateLocalServer, SetupServer);
-            Container.Rebind<INetworkServer>().ToLookup<LocalNetworkServer>();
-            Container.Rebind<LocalNetworkClient>().ToMethod<LocalNetworkClient>(CreateLocalClient, SetupClient);
-            Container.Rebind<INetworkClient>().ToLookup<LocalNetworkClient>();
-        }
-        else if(Settings.Tech == MultiplayerTech.Unet)
-        {
-            Container.Rebind<UnetNetworkServer>().ToMethod<UnetNetworkServer>(CreateUnetServer, SetupServer);
-            Container.Bind<IDisposable>().ToLookup<UnetNetworkServer>();
-            Container.Rebind<INetworkServer>().ToLookup<UnetNetworkServer>();
-            Container.Rebind<UnetNetworkClient>().ToMethod<UnetNetworkClient>(CreateUnetClient, SetupClient);
-            Container.Bind<IDisposable>().ToLookup<UnetNetworkClient>();
-            Container.Rebind<INetworkClient>().ToLookup<UnetNetworkClient>();
-        }
-        else if(Settings.Tech == MultiplayerTech.Photon)
-        {
-            Container.RebindUnityComponent<PhotonNetworkServer>().WithSetup<PhotonNetworkServer>(SetupPhotonServer);
-            Container.Rebind<INetworkServer>().ToLookup<PhotonNetworkServer>();
-            Container.RebindUnityComponent<PhotonNetworkClient>().WithSetup<PhotonNetworkServer>(SetupPhotonClient);
-            Container.Rebind<INetworkClient>().ToLookup<PhotonNetworkClient>();
-        }
-
         Container.Rebind<NetworkServerSceneController>()
             .ToMethod<NetworkServerSceneController>(CreateServerSceneController, SetupServerSceneController);        
         Container.Rebind<NetworkClientSceneController>()
             .ToMethod<NetworkClientSceneController>(CreateClientSceneController, SetupClientSceneController);
-        
-        Container.Bind<IAdminPanelConfigurer>().ToMethod<AdminPanelMultiplayer>(CreateAdminPanel);
-    }
-
-    AdminPanelMultiplayer CreateAdminPanel()
-    {
-        return new AdminPanelMultiplayer(
-            Container.Resolve<IUpdateScheduler>(), Container);
     }
 
     NetworkServerSceneController CreateServerSceneController()
@@ -79,48 +35,6 @@ public class MultiplayerInstaller : Installer
         return new UnityNetworkClientSceneController(
             Container.Resolve<INetworkClient>(),
             Settings.MultiplayerParentTag);
-    }
-
-    LocalNetworkClient CreateLocalClient()
-    {
-        return new LocalNetworkClient(
-            Container.Resolve<LocalNetworkServer>());
-    }
-
-    LocalNetworkServer CreateLocalServer()
-    {
-        return new LocalNetworkServer();
-    }
-
-    UnetNetworkClient CreateUnetClient()
-    {
-        return new UnetNetworkClient(
-            Settings.ServerAddress, Settings.ServerPort);
-    }
-
-    UnetNetworkServer CreateUnetServer()
-    {
-        return new UnetNetworkServer(
-            Container.Resolve<IUpdateScheduler>(),
-            Settings.ServerPort);
-    }
-
-    void SetupServer(INetworkServer server)
-    {
-        var dlgs = Container.ResolveList<INetworkServerDelegate>();
-        for(var i = 0; i < dlgs.Count; i++)
-        {
-            server.AddDelegate(dlgs[i]);
-        }
-    }
-
-    void SetupClient(INetworkClient client)
-    {
-        var dlgs = Container.ResolveList<INetworkClientDelegate>();
-        for(var i = 0; i < dlgs.Count; i++)
-        {
-            client.AddDelegate(dlgs[i]);
-        }
     }
 
     void SetupServerSceneController(NetworkServerSceneController ctrl)
@@ -139,17 +53,5 @@ public class MultiplayerInstaller : Installer
         {
             ctrl.AddBehaviour(behaviours[i]);
         }
-    }
-
-    void SetupPhotonServer(PhotonNetworkServer server)
-    {
-        server.Init(Settings.PhotonConfig);
-        SetupServer(server);
-    }
-
-    void SetupPhotonClient(PhotonNetworkClient client)
-    {
-        client.Init(Settings.PhotonConfig);
-        SetupClient(client);
     }
 }
