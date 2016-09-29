ab19a4424ee7a474b352f30280d6d7090cab298e
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-09-29 18:02:44 +0200

    fixed randomutils to work without unity

--- Project/Assets/Sparta/SocialPoint/Utils/RandomUtils.cs
+++ Project/Assets/Sparta/SocialPoint/Utils/RandomUtils.cs
@@ -1,8 +1,10 @@
 #if (UNITY_ANDROID || UNITY_IOS || UNITY_TVOS) && !UNITY_EDITOR
 #define UNITY_DEVICE
+#define UNITY
 #endif
 #if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX || UNITY_DEVICE
 #define NATIVE_RANDOM
+#define UNITY
 #endif
 
 using System;
@@ -13,6 +15,11 @@ namespace SocialPoint.Utils
     public static class RandomUtils
     {
         static bool _init = false;
+
+        #if !UNITY
+        static Random _random;
+        #endif
+
 
         #if NATIVE_RANDOM
 
@@ -48,7 +55,11 @@ namespace SocialPoint.Utils
             {
                 return;
             }
+        #if UNITY
             UnityEngine.Random.seed = GetRandomSeed();
+        #else
+            _random = new Random(Guid.NewGuid().GetHashCode());
+        #endif
             _init = true;
         }
 
@@ -67,12 +78,6 @@ namespace SocialPoint.Utils
             ts = ts << 31;
             rn = rn & 0x7FFFFFFF;
             return ts + rn;
-        }
-
-        public static uint GenerateUint()
-        {
-            Init();
-            return (uint)UnityEngine.Random.Range(int.MinValue, int.MaxValue);
         }
 
         [System.Obsolete("Use GenerateSecurityToken instead", true)]
@@ -113,6 +118,16 @@ namespace SocialPoint.Utils
             return s.ToLower();
         }
 
+        public static uint GenerateUint()
+        {
+            Init();
+        #if UNITY
+            return (uint)UnityEngine.Random.Range(int.MinValue, int.MaxValue);
+        #else
+            return (uint)_random.Next(Int32.MinValue, Int32.MaxValue);
+        #endif
+        }
+
         /// <summary>
         /// Return a random int value between the param range [min, max)
         /// </summary>
@@ -122,7 +137,12 @@ namespace SocialPoint.Utils
         {
             Init();
             DebugUtils.Assert(minInclusive < maxExclusive, "Max needs to be more that min.");
+            #if UNITY
             return UnityEngine.Random.Range(minInclusive, maxExclusive);
+            #else
+            return _random.Next(minInclusive, maxExclusive);
+            #endif
+
         }
 
         /// <summary>
@@ -134,7 +154,11 @@ namespace SocialPoint.Utils
         {
             Init();
             DebugUtils.Assert(minInclusive < maxInclusive, "Max needs to be more that min.");
+            #if UNITY
             return UnityEngine.Random.Range(minInclusive, maxInclusive);
+            #else
+            return (float)(float.MaxValue * 2.0 * (_random.NextDouble()-0.5));
+            #endif
         }
     }
 }
