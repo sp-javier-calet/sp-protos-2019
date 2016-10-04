c183244019735bdeaef4e32ac0399c961f59bf7d
Andres Barrera <andres.barrera@socialpoint.es>
2016-10-04 18:19:06 +0200

    DLLs and test class

---
+++ Project/Assets/Sparta/Extensions/Multiplayer/Pathfinding/PathfindingTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SharpNav;
+using SharpNav.Collections;
+using SharpNav.Crowds;
+using SharpNav.Geometry;
+using SharpNav.IO;
+using SharpNav.Pathfinding;
+
+public class PathfindingTest
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        /*
+        //prepare the geometry from your mesh data
+        var tris = TriangleEnumerable.FromIndexedVector3( ... );
+
+        //use the default generation settings
+        var settings = NavMeshGenerationSettings.Default;
+        settings.AgentHeight = 1.7f;
+        settings.AgentRadius = 0.6f;
+
+        //generate the mesh
+        var navMesh = NavMesh.Generate(tris, settings);
+        //*/
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+}
