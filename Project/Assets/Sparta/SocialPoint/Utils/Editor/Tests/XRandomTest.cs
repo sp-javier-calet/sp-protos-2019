54c0cbe9a756d058b9b8343ccbc4b7d7a9c6c352
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-10-18 16:30:03 +0200

    C# implementation for PRNG.

---
+++ Project/Assets/Sparta/SocialPoint/Utils/Editor/Tests/XRandomTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SocialPoint.Utils
+{
+    [TestFixture]
+    [Category("SocialPoint.Utils")]
+    public class XRandomTest
+    {
+        const int SeedA = 183783364;
+        const int SeedB = 870230629;
+
+        XRandom RNGSeedA1;
+        XRandom RNGSeedA2;
+        XRandom RNGSeedA3;
+        XRandom RNGSeedB1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            RNGSeedA1 = new XRandom(SeedA);
+            RNGSeedA2 = new XRandom(SeedA);
+            RNGSeedA3 = new XRandom(SeedA);
+            RNGSeedB1 = new XRandom(SeedB);
+        }
+
+        [Test]
+        [Repeat(1000)]
+        public void Deterministic()
+        {
+            var v1 = RNGSeedA1.Range(0, 100000);
+            var v2 = RNGSeedA2.Range(0, 100000);
+            Assert.That(v1 == v2);
+
+            var f1 = RNGSeedA1.Range(0, 100.0f);
+            var f2 = RNGSeedA2.Range(0, 100.0f);
+            Assert.That(f1 == f2);
+        }
+
+        [Test]
+        [Repeat(1000)]
+        public void SeededRandom()
+        {
+            var v1 = RNGSeedA3.Range(0, 100000);
+            var v2 = RNGSeedB1.Range(0, 100000);
+
+            Assert.That(v1 != v2);
+        }
+    }
+}
---
+++ Project/Assets/Sparta/SocialPoint/Utils/XRandom.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace SocialPoint.Utils
+{
+
+    public sealed class XRandom
+    {
+        LinearCongruentialEngine _lce;
+
+        double Value
+        {
+            get
+            {
+                return (double) _lce.Next / (double)LinearCongruentialEngine.Max;
+            }
+        }
+
+        public XRandom() : this(1)
+        {
+        }
+
+        public XRandom(uint seed)
+        {
+            _lce = new LinearCongruentialEngine(seed);
+        }
+
+        public int Range(int min, int max)
+        {
+            return (int)(Value * (max - min + 1) % (max - min + 1) + min);
+        }
+
+        public float Range(float min, float max)
+        {
+            return (float)(Value * (max - min) + min);
+        }
+
+        class LinearCongruentialEngine
+        {
+            const uint _a = 48271;
+            const uint _c = 0;
+            const uint _m = 2147483647;
+
+            uint _previous;
+
+            public LinearCongruentialEngine(uint seed)
+            {
+                _previous = seed == 0 ? 1 : seed;
+            }
+
+            public uint Next
+            {
+                get
+                {
+                    _previous = (uint)((_a * (UInt64)_previous + _c)  % _m);
+                    return _previous;
+                }
+            }
+
+            public static uint Min 
+            {
+                get
+                {
+                    return 0;
+                }
+            }
+
+            public static uint Max
+            {
+                get
+                {
+                    return _m - 1;
+                }
+            }
+        }
+    }
+}
