cfcd792ff82228682585a95473004f2dddeb7ba3
ihernandez-windows <ivan.hernandez@socialpoint.es>
2016-10-19 14:17:14 +0200

    Cleaning of GameMsgType in old class

--- Project/Assets/Examples/Multiplayer/GameMultiplayerClientBehaviour.cs
+++ Project/Assets/Examples/Multiplayer/GameMultiplayerClientBehaviour.cs
@@ -7,13 +7,6 @@ using SocialPoint.IO;
 using SocialPoint.Pooling;
 using Jitter.LinearMath;
 
-public static class GameMsgType
-{
-    public const byte ClickAction = SceneMsgType.Highest + 1;
-    public const byte ExplosionEvent = SceneMsgType.Highest + 2;
-    public const byte MovementAction = SceneMsgType.Highest + 3;
-}
-
 public class GameMultiplayerClientBehaviour : MonoBehaviour, INetworkClientSceneReceiver, IPointerClickHandler
 {
     INetworkClient _client;
