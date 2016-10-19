ca57a85dc1ff792b1c6b9702fbe8dad6d3065908
ihernandez-windows <ivan.hernandez@socialpoint.es>
2016-10-19 14:14:35 +0200

    Separate file for GameMsgType to reference it in server project (previously inside file with Unity references)

---
+++ Project/Assets/Examples/Multiplayer/GameMsgType.cs
@@ -0,0 +1,8 @@
+﻿using SocialPoint.Multiplayer;
+
+public static class GameMsgType
+{
+    public const byte ClickAction = SceneMsgType.Highest + 1;
+    public const byte ExplosionEvent = SceneMsgType.Highest + 2;
+    public const byte MovementAction = SceneMsgType.Highest + 3;
+}
