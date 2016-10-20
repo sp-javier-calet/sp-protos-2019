4da03b80c66b5a9aeebf4b2562fc12f10cfda247
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-10-20 10:23:02 +0200

    Added Installer and empty Chat classes.

---
+++ Project/Assets/Scripts/Installers/SocialFrameworkInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SocialPoint.Social;
+using SocialPoint.Dependency;
+
+public class SocialFrameworkInstaller : Installer
+{
+    [Serializable]
+    public class SettingsData
+    {
+    }
+
+    public override void InstallBindings()
+    {       
+        Container.Bind<ConnectionManager>().ToMethod<ConnectionManager>(CreateConnectionManager);
+    }
+
+    ConnectionManager CreateConnectionManager()
+    {
+        return new ConnectionManager();
+    }
+}
---
+++ Project/Assets/Sparta/SocialPoint/Social/SocialFramework/ChatManager.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialPoint.Social
+{
+    public class ChatManager
+    {
+
+    }
+}
