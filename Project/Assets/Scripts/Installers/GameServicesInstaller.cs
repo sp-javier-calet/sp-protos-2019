92ce6b29f45d39007a923b91dabeb0283b73ebdf
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-10-27 17:55:49 +0200

    Fixed localization.

--- Project/Assets/Scripts/Installers/GameServicesInstaller.cs
+++ Project/Assets/Scripts/Installers/GameServicesInstaller.cs
@@ -4,18 +4,38 @@ using SocialPoint.CrossPromotion;
 using SocialPoint.Dependency;
 using SocialPoint.ServerSync;
 using SocialPoint.ServerEvents;
+using SocialPoint.ScriptEvents;
+using SocialPoint.Locale;
 using SocialPoint.Notifications;
 using SocialPoint.Purchase;
+using SocialPoint.Network;
+using SocialPoint.Hardware;
 using SocialPoint.Utils;
 
 public class GameServicesInstaller : Installer
 {
-    public enum NetworkTech
+    public enum EnvironmentID
     {
-        Local,
-        Unet,
-        Photon
+        dev,
+        loc,
+        prod
     }
+
+    [Serializable]
+    public class SettingsData
+    {
+        public EnvironmentID EnvironmentId = EnvironmentID.prod;
+        public string ProjectId = GameLocalizationManager.LocationData.DefaultProjectId;
+        public string SecretKeyDev = GameLocalizationManager.LocationData.DefaultSecretKey;
+        public string SecretKeyLoc = GameLocalizationManager.LocationData.DefaultSecretKey;
+        public string SecretKeyProd = GameLocalizationManager.LocationData.DefaultSecretKey;
+        public string BundleDir = GameLocalizationManager.DefaultBundleDir;
+        public string[] SupportedLanguages = GameLocalizationManager.DefaultSupportedLanguages;
+        public float Timeout = GameLocalizationManager.DefaultTimeout;
+    }
+
+    public SettingsData Settings = new SettingsData();
+
 
     public override void InstallBindings()
     {
@@ -28,6 +48,10 @@ public class GameServicesInstaller : Installer
         Container.Rebind<GameNotificationManager>().ToMethod<GameNotificationManager>(CreateNotificationManager);
         Container.Rebind<NotificationManager>().ToLookup<GameNotificationManager>();
         Container.Bind<IDisposable>().ToMethod<GameNotificationManager>(CreateNotificationManager);
+
+        // Localization
+        Container.Rebind<ILocalizationManager>().ToMethod<GameLocalizationManager>(CreateLocalizationManager, SetupLocalizationManager);
+        Container.Bind<IDisposable>().ToLookup<ILocalizationManager>();
 
         // Purchase store
         Container.Bind<IStoreProductSource>().ToGetter<ConfigModel>((Config) => Config.Store);
@@ -54,5 +78,38 @@ public class GameServicesInstaller : Installer
             Container.Resolve<IAppEvents>(),
             Container.Resolve<ICommandQueue>()
         );
+    }
+
+    GameLocalizationManager CreateLocalizationManager()
+    {
+        return new GameLocalizationManager(
+            Container.Resolve<IHttpClient>(),
+            Container.Resolve<IAppInfo>(),
+            Container.Resolve<Localization>(),
+            Container.Resolve<LocalizeAttributeConfiguration>(),
+            Container.Resolve<IEventDispatcher>());
+    }
+
+    void SetupLocalizationManager(GameLocalizationManager mng)
+    {
+        string secretKey;
+        if(Settings.EnvironmentId == EnvironmentID.dev)
+        {
+            secretKey = Settings.SecretKeyDev;
+        }
+        else if(Settings.EnvironmentId == EnvironmentID.loc)
+        {
+            secretKey = Settings.SecretKeyLoc;
+        }
+        else
+        {
+            secretKey = Settings.SecretKeyProd;
+        }
+        mng.Location.ProjectId = Settings.ProjectId;
+        mng.Location.EnvironmentId = Settings.EnvironmentId.ToString();
+        mng.Location.SecretKey = secretKey;
+        mng.Timeout = Settings.Timeout;
+        mng.BundleDir = Settings.BundleDir;
+        mng.AppEvents = Container.Resolve<IAppEvents>();
     }
 }
--- Project/Assets/Sparta/SocialPoint/Locale/Installers/LocaleInstaller.cs
+++ Project/Assets/Sparta/SocialPoint/Locale/Installers/LocaleInstaller.cs
@@ -2,32 +2,13 @@ using System;
 using SocialPoint.Locale;
 using SocialPoint.AdminPanel;
 using SocialPoint.Dependency;
-using SocialPoint.Network;
-using SocialPoint.Hardware;
 using SocialPoint.Utils;
-using SocialPoint.AppEvents;
-using SocialPoint.ScriptEvents;
 
 public class LocaleInstaller : Installer
 {
-    public enum EnvironmentID
-    {
-        dev,
-        loc,
-        prod
-    }
-
     [Serializable]
     public class SettingsData
     {
-       // public string ProjectId = GameLocalizationManager.LocationData.DefaultProjectId;
-        public EnvironmentID EnvironmentId = EnvironmentID.prod;
-        //public string SecretKeyDev = GameLocalizationManager.LocationData.DefaultSecretKey;
-        //public string SecretKeyLoc = GameLocalizationManager.LocationData.DefaultSecretKey;
-        //public string SecretKeyProd = GameLocalizationManager.LocationData.DefaultSecretKey;
-        //public string BundleDir = GameLocalizationManager.DefaultBundleDir;
-        //public string[] SupportedLanguages = GameLocalizationManager.DefaultSupportedLanguages;
-        //public float Timeout = GameLocalizationManager.DefaultTimeout;
         public bool EditorDebug = true;
     }
 
@@ -35,10 +16,7 @@ public class LocaleInstaller : Installer
 
     public override void InstallBindings()
     {
-        // TODO Game code
         Container.Rebind<Localization>().ToMethod<Localization>(CreateLocalization);
-        //Container.Rebind<ILocalizationManager>().ToMethod<GameLocalizationManager>(CreateLocalizationManager, SetupLocalizationManager);
-        //Container.Bind<IDisposable>().ToLookup<ILocalizationManager>();
          
         Container.Rebind<LocalizeAttributeConfiguration>().ToMethod<LocalizeAttributeConfiguration>(CreateLocalizeAttributeConfiguration);
 
@@ -57,40 +35,6 @@ public class LocaleInstaller : Installer
         return new AdminPanelLocale(
             Container.Resolve<ILocalizationManager>());
     }
-
-   /* GameLocalizationManager CreateLocalizationManager()
-    {
-        return new GameLocalizationManager(
-            Container.Resolve<IHttpClient>(),
-            Container.Resolve<IAppInfo>(),
-            Container.Resolve<Localization>(),
-            Container.Resolve<LocalizeAttributeConfiguration>(),
-            Container.Resolve<IEventDispatcher>());
-    }
-
-    void SetupLocalizationManager(GameLocalizationManager mng)
-    {
-        string secretKey;
-        if(Settings.EnvironmentId == EnvironmentID.dev)
-        {
-            secretKey = Settings.SecretKeyDev;
-        }
-        else if(Settings.EnvironmentId == EnvironmentID.loc)
-        {
-            secretKey = Settings.SecretKeyLoc;
-        }
-        else
-        {
-            secretKey = Settings.SecretKeyProd;
-        }
-        mng.Location.ProjectId = Settings.ProjectId;
-        mng.Location.EnvironmentId = Settings.EnvironmentId.ToString();
-        mng.Location.SecretKey = secretKey;
-        mng.Timeout = Settings.Timeout;
-        mng.BundleDir = Settings.BundleDir;
-        mng.AppEvents = Container.Resolve<IAppEvents>();
-    }
-    */
 
     Localization CreateLocalization()
     {
