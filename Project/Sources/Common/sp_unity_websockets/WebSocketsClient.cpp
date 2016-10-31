ec2f1175d04b9b100ad180cbb87eb5b868dc7b91
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-10-31 15:05:09 +0100

    Added base code.

---
+++ Project/Sources/Common/sp_unity_websockets/WebSocketsClient.cpp
@@ -0,0 +1,24 @@
+//
+//  WebSocketsClient.cpp
+//  sp_unity_plugins
+//
+//  Created by Manuel Álvarez de Toledo on 31/10/16.
+//
+//
+
+#include "WebSocketsClient.hpp"
+
+extern "C"
+{
+    #include "libwebsockets.h"
+}
+
+WebSocketsClient::WebSocketsClient()
+{
+
+}
+
+WebSocketsClient::~WebSocketsClient()
+{
+    
+}
