28410a7e806f6f89bf3ae5375b755f9749184853
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-11-10 12:46:32 +0100

    restructured lockstep example, added support for matchmaking and server logic

--- Project/Assets/Examples/Lockstep/ClickCommand.cs
+++ Project/Assets/Examples/Lockstep/ClickCommand.cs
@@ -2,57 +2,60 @@
 using SocialPoint.IO;
 using FixMath.NET;
 
-public class ClickCommand : ILockstepCommand
+namespace Examples.Lockstep
 {
-    public Fix64 X{ get; private set; }
-
-    public Fix64 Y{ get; private set; }
-
-    public Fix64 Z{ get; private set; }
-
-    public ClickCommand()
+    public class ClickCommand : ILockstepCommand
     {
+        public Fix64 X { get; private set; }
+
+        public Fix64 Y { get; private set; }
+
+        public Fix64 Z { get; private set; }
+
+        public ClickCommand()
+        {
+        }
+
+        public ClickCommand(Fix64 x, Fix64 y, Fix64 z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public object Clone()
+        {
+            return new ClickCommand(X, Y, Z);
+        }
+
+        public void Deserialize(IReader reader)
+        {
+            X = Fix64.FromRaw(reader.ReadInt64());
+            Y = Fix64.FromRaw(reader.ReadInt64());
+            Z = Fix64.FromRaw(reader.ReadInt64());
+        }
+
+        public void Serialize(IWriter writer)
+        {
+            writer.Write((long)X.RawValue);
+            writer.Write((long)Y.RawValue);
+            writer.Write((long)Z.RawValue);
+        }
+
     }
 
-    public ClickCommand(Fix64 x, Fix64 y, Fix64 z)
+    public class ClickCommandLogic : ILockstepCommandLogic<ClickCommand>
     {
-        X = x;
-        Y = y;
-        Z = z;
-    }
+        Model _model;
 
-    public object Clone()
-    {
-        return new ClickCommand(X, Y, Z);
-    }
+        public ClickCommandLogic(Model model)
+        {
+            _model = model;
+        }
 
-    public void Deserialize(IReader reader)
-    {
-        X = Fix64.FromRaw(reader.ReadInt64());
-        Y = Fix64.FromRaw(reader.ReadInt64());
-        Z = Fix64.FromRaw(reader.ReadInt64());
-    }
-
-    public void Serialize(IWriter writer)
-    {
-        writer.Write((long)X.RawValue);
-        writer.Write((long)Y.RawValue);
-        writer.Write((long)Z.RawValue);
-    }
-
-}
-
-public class ClickCommandLogic : ILockstepCommandLogic<ClickCommand>
-{
-    LockstepModel _model;
-
-    public ClickCommandLogic(LockstepModel model)
-    {
-        _model = model;
-    }
-
-    public void Apply(ClickCommand cmd, byte playerNum)
-    {
-        _model.OnClick(cmd.X, cmd.Y, cmd.Z);
+        public void Apply(ClickCommand cmd, byte playerNum)
+        {
+            _model.OnClick(cmd.X, cmd.Y, cmd.Z, playerNum);
+        }
     }
 }
---
+++ Project/Assets/Examples/Lockstep/ServerBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using SocialPoint.Base;
+using SocialPoint.Attributes;
+using SocialPoint.Lockstep;
+using SocialPoint.Lockstep.Network;
+
+namespace Examples.Lockstep
+{
+    public class ServerBehaviour : IServerLockstepNetworkDelegate
+    {
+        ServerLockstepNetworkController _server;
+        Model _model;
+        ClientLockstepController _client;
+
+        public ServerBehaviour(ServerLockstepNetworkController server)
+        {
+            _server = server;
+            _model = new Model();
+            _client = new ClientLockstepController();
+
+            _client.Simulate += Simulate;
+
+            var cmdFactory = new LockstepCommandFactory();
+            CommandType.Setup(_model, cmdFactory, _client);
+            server.RegisterLocalClient(_client, cmdFactory);
+        }
+
+        void Simulate(int dt)
+        {
+            _model.Simulate(dt);
+        }
+
+        public void OnCommandFailed(Error err, byte playerNum)
+        {
+            // TODO: mark player as a cheater
+            _server.PlayerResults[playerNum] = new AttrInt(-1);
+            _server.Fail("Command failed: " + err);
+        }
+
+        public void OnError(Error err)
+        {
+            _server.Fail("Lockstep server failed: " + err);
+        }
+
+        public void OnFinish(Dictionary<byte, Attr> playerResults)
+        {
+            // TODO: only replay if playerResults do not match up
+            // for example: if all players say they won
+
+            // replays the whole game to see who really won
+            _model.Reset();
+            _server.ReplayLocalClient();
+            _client.Start();
+            _client.Update(_server.UpdateTime);
+            _client.Stop();
+
+            playerResults.Clear();
+            var itr = _model.Results.GetEnumerator();
+            while(itr.MoveNext())
+            {
+                playerResults.Add(itr.Current.Key, itr.Current.Value);
+            }
+            itr.Dispose();
+        }
+
+        public void OnStart(Attr data)
+        {
+            // TODO: game specific model setup
+        }
+    }
+}
