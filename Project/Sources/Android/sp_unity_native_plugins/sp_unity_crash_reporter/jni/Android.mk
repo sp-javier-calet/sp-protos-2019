04ddd1b3eafaa24ba921c7034fc18eadf11344dc
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-11-14 11:57:34 +0100

    Updated make files.

--- Project/Sources/Android/sp_unity_native_plugins/sp_unity_crash_reporter/jni/Android.mk
+++ Project/Sources/Android/sp_unity_native_plugins/sp_unity_crash_reporter/jni/Android.mk
@@ -1,16 +1,18 @@
 LOCAL_PATH := $(call my-dir)
-BASE_PATH  := $(LOCAL_PATH)/..
-COMMON_SRC_PATH  := $(BASE_PATH)/../../../Common/sp_unity_crash_reporter
+BASE_PATH := $(LOCAL_PATH)/..
+PLUGINS_PATH := $(BASE_PATH)/..
+COMMON_SRC_PATH := $(BASE_PATH)/../../../Common
+
+CR_SRC_PATH := $(COMMON_SRC_PATH)/sp_unity_crash_reporter
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := sp_unity_crash_reporter
 LOCAL_MODULE_FILENAME := libsp_unity_crash_reporter
 
-LOCAL_ARM_MODE  := arm
 LOCAL_CFLAGS    := -Werror
 LOCAL_LDLIBS    := -llog
-LOCAL_SRC_FILES := $(COMMON_SRC_PATH)/SPUnityBreadcrumbManager.cpp \
-                   $(COMMON_SRC_PATH)/SPUnityBreadcrumbManagerFacade.cpp \
+LOCAL_SRC_FILES := $(CR_SRC_PATH)/SPUnityBreadcrumbManager.cpp \
+                   $(CR_SRC_PATH)/SPUnityBreadcrumbManagerFacade.cpp \
                    $(BASE_PATH)/src/SPUnityCrashReporter.cpp \
                    $(BASE_PATH)/src/SPUnityCrashRepoterFacade.cpp
                    
@@ -18,17 +20,16 @@ LOCAL_SRC_FILES := $(COMMON_SRC_PATH)/SPUnityBreadcrumbManager.cpp \
 LOCAL_STATIC_LIBRARIES := breakpad_client
 LOCAL_SHARED_LIBRARIES := sp_unity_utils
 
-LOCAL_C_INCLUDES := $(COMMON_SRC_PATH)
+LOCAL_C_INCLUDES := $(CR_SRC_PATH)
 
 include $(BUILD_SHARED_LIBRARY)
 
 # Import modules
-$(call import-add-path,$(BASE_PATH)/../)
+$(call import-add-path,$(PLUGINS_PATH))
 
 ifeq ($(TARGET_ARCH),x86)
 else
-$(call import-add-path,$(BASE_PATH)/lib)
-$(call import-module,google-breakpad/android/google_breakpad)
+$(call import-module,lib/google-breakpad/android/google_breakpad)
 endif
 
 $(call import-module,sp_unity_utils/jni)
