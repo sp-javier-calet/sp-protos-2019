ff2b8891a69e65c88c0a5fa4c1ec155ab57ffd13
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-11-14 12:16:11 +0100

    Unified toolchain and android version for plugins mk files

--- Project/Sources/Android/sp_unity_native_plugins/sp_unity_curl/jni/Application.mk
+++ Project/Sources/Android/sp_unity_native_plugins/sp_unity_curl/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI          := armeabi-v7a x86
-APP_PLATFORM     := android-9
+APP_PLATFORM     := android-14
 APP_STL          := gnustl_static
 APP_OPTIM        := release
 APP_CPPFLAGS     := -std=c++11
