6da37239e0fb0d91e56bfbb5f1c5fa6136fe2671
Mario Quesada <marioquesada@Marios-iMac.local>
2016-11-16 12:33:35 +0100

    removed disconnect time

--- Project/Assets/Sparta/Extensions/Photon/Network/PhotonNetworkBase.cs
+++ Project/Assets/Sparta/Extensions/Photon/Network/PhotonNetworkBase.cs
@@ -113,7 +113,6 @@ namespace SocialPoint.Network
                 else
                 {
                     PhotonNetwork.networkingPeer.MaximumTransferUnit = _config.CustomInternalNetworkConfig.MaximumTransferUnit;
-                    PhotonNetwork.BackgroundTimeout = 0f;
                 }
             }
         }
