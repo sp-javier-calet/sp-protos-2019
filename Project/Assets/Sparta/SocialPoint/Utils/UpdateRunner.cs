085921d8bd086dd9acab0b7d6a99d99e7c6c6ea2
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2016-11-21 13:02:08 +0100

    Improved aggregated exceptions management. Fixes to allow multiple Remove calls and Remove/Add patterns with IUpdateable.

--- Project/Assets/Sparta/SocialPoint/Utils/UpdateRunner.cs
+++ Project/Assets/Sparta/SocialPoint/Utils/UpdateRunner.cs
@@ -115,6 +115,7 @@ namespace SocialPoint.Utils
             DebugUtils.Assert(elm != null);
             if(elm != null)
             {
+                _elementsToRemove.Remove(elm);
                 _elements.Add(elm);
             }
         }
@@ -139,7 +140,7 @@ namespace SocialPoint.Utils
 
         public void Remove(IUpdateable elm)
         {
-            if(elm != null)
+            if(elm != null && !_elementsToRemove.Contains(elm))
             {
                 _elementsToRemove.Add(elm);
             }
@@ -241,13 +242,38 @@ namespace SocialPoint.Utils
             var exceptionsCount = _exceptions.Count;
             if(exceptionsCount > 0)
             {
+                throw new AggregateException(_exceptions);
+            }
+        }
+
+        sealed class AggregateException : Exception
+        {
+            static string CreateMessage(IEnumerable<Exception> exceptions)
+            {
                 var sb = new StringBuilder();
-                for(int i = 0; i < exceptionsCount; i++)
+                sb.AppendLine("Multiple Exceptions thrown:");
+                var count = 1;
+                var itr = exceptions.GetEnumerator();
+                while(itr.MoveNext())
                 {
-                    var ex = _exceptions[i];
-                    sb.Append(ex.Message);
+                    var ex = itr.Current;
+                    sb.Append(count++)
+                        .Append(". ")
+                        .Append(ex.GetType().Name)
+                        .Append(": ")
+                        .Append(ex.Message)
+                        .AppendLine(ex.StackTrace);
+                    sb.AppendLine();
                 }
-                throw new Exception(sb.ToString());
+                itr.Dispose();
+                return sb.ToString();
+            }
+
+            public List<Exception> Exceptions { get; private set; }
+
+            public AggregateException(IEnumerable<Exception> exceptions) : base(CreateMessage(exceptions))
+            {
+                Exceptions = new List<Exception>(exceptions);
             }
         }
 
