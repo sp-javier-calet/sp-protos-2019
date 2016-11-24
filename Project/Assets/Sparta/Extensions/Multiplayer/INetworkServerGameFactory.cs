3b8821cc399513c07bf9b5176405573906be9d81
Miguel Ibero <miguel.ibero@socialpoint.es>
2016-11-24 09:08:33 +0100

    fixed copied multiplayer file

--- Project/Assets/Sparta/Extensions/Multiplayer/INetworkServerGameFactory.cs
+++ Project/Assets/Sparta/Extensions/Multiplayer/INetworkServerGameFactory.cs
@@ -1,50 +1,10 @@
-﻿using SocialPoint.IO;
-using SocialPoint.Network;
-using System;
+﻿using SocialPoint.Network;
+using System.Collections.Generic;
 
 namespace SocialPoint.Multiplayer
 {
-    public static class SceneMsgType
+    public interface INetworkServerGameFactory
     {
-        public const byte UpdateSceneEvent = 1;
-        public const byte InstantiateObjectEvent = 2;
-        public const byte DestroyObjectEvent = 3;
-        public const byte Highest = 3;
-    }
-
-    public class InstantiateNetworkGameObjectEvent : INetworkShareable
-    {
-        public int ObjectId;
-        public string PrefabName;
-        public Transform Transform;
-
-        public void Deserialize(IReader reader)
-        {
-            ObjectId = reader.ReadInt32();
-            PrefabName = reader.ReadString();
-            Transform = TransformParser.Instance.Parse(reader);
-        }
-
-        public void Serialize(IWriter writer)
-        {
-            writer.Write(ObjectId);
-            writer.Write(PrefabName);
-            TransformSerializer.Instance.Serialize(Transform, writer);
-        }
-    }
-
-    public class DestroyNetworkGameObjectEvent : INetworkShareable
-    {
-        public int ObjectId;
-
-        public void Deserialize(IReader reader)
-        {
-            ObjectId = reader.ReadInt32();
-        }
-
-        public void Serialize(IWriter writer)
-        {
-            writer.Write(ObjectId);
-        }
+        object Create(INetworkServer server, NetworkServerSceneController ctrl, Dictionary<string, string> config);
     }
 }
