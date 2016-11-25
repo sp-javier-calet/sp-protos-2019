8cb559d1610cd71531955699cb67383c3b7b00ac
Andreu Pi Suari <andreu.pi@socialpoint.es>
2016-11-25 13:06:52 +0100

    Fixes

--- Project/Assets/Sparta/Extensions/GrayboxLibrary/ModelClickPoping.cs
+++ Project/Assets/Sparta/Extensions/GrayboxLibrary/ModelClickPoping.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ModelClickPoping : MonoBehaviour {
+public class ModelClickPoping : MonoBehaviour
+{
 
     private Material _mat;
     private float _currentOverbrigth;
@@ -10,7 +11,7 @@ public class ModelClickPoping : MonoBehaviour {
     private float _originalOverbright;
 
     private Vector3 _currentSize;
-    public Vector3 _popSize = new Vector3(1.1f,2.6f,1.1f);
+    public Vector3 _popSize = new Vector3(1.1f, 2.6f, 1.1f);
     public float SizeSpeed = 14f;
     private Vector3 _originalSize;
 
@@ -19,20 +20,24 @@ public class ModelClickPoping : MonoBehaviour {
     private float _startTime;
     private Vector3 _targetSize;
 
-    void Awake ()
+    private int _shaderProperty;
+
+    void Awake()
     {
         Renderer rend = gameObject.GetComponentInChildren<Renderer>();
         _mat = rend.material;
+        _shaderProperty = Shader.PropertyToID("_Overbright");
         BoxCollider collider = gameObject.AddComponent<BoxCollider>();
         collider.size = rend.bounds.size;
-        _originalOverbright = _mat.GetFloat("_Overbright");
+        _originalOverbright = _mat.GetFloat(_shaderProperty);
         _originalSize = transform.localScale;
         _bigSize = new Vector3(_originalSize.x * _popSize.x, _originalSize.y * _popSize.y, _originalSize.z * _popSize.z);
         _sizeDiference = Vector3.Distance(_originalSize, _bigSize);
         _targetSize = _originalSize;
         _currentSize = _originalSize;
+
     }
-	
+
     void OnMouseDown()
     {
         _currentOverbrigth = _originalOverbright * _popOverbright;
@@ -41,30 +46,22 @@ public class ModelClickPoping : MonoBehaviour {
         _sizeDiference = Vector3.Distance(_originalSize, _bigSize);
         _targetSize = _bigSize;
     }
-	
-	void Update ()
+
+    void Update()
     {
-	    if(_currentOverbrigth > _originalOverbright)
+        if(_currentOverbrigth > _originalOverbright)
         {
-            _mat.SetFloat("_Overbright", _currentOverbrigth);
+            _mat.SetFloat(_shaderProperty, _currentOverbrigth);
             _currentOverbrigth = Mathf.Max(_currentOverbrigth - _ovebrightSpeed, _originalOverbright);
         }
 
-        if (Vector3.Distance(_currentSize, _targetSize) != 0 && _targetSize == _bigSize)
+        if(Vector3.Distance(_currentSize, _targetSize) != 0)
         {
             transform.localScale = _currentSize;
 
             float frame = (Time.time - _startTime) * SizeSpeed;
             float fraction = frame / _sizeDiference;
-            _currentSize = Vector3.Lerp(_originalSize, _bigSize, fraction);
-        }
-        else if(Vector3.Distance(_currentSize, _targetSize) != 0 && _targetSize == _originalSize)
-        {
-            transform.localScale = _currentSize;
-
-            float frame = (Time.time - _startTime) * SizeSpeed;
-            float fraction = frame / _sizeDiference;
-            _currentSize = Vector3.Lerp(_bigSize, _originalSize, fraction);
+            _currentSize = Vector3.Lerp(_originalSize, _targetSize, fraction);
         }
         else if(_targetSize == _bigSize)
         {
