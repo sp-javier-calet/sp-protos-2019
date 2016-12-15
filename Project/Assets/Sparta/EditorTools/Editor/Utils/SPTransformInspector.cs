8d460c155eecff57fb8a127be739974407c19413
Antoni Domingo <antoni.domingo@socialpoint.es>
2016-12-15 16:19:24 +0100

    Added #if !NGUI. Formatting

--- Project/Assets/Sparta/EditorTools/Editor/Utils/SPTransformInspector.cs
+++ Project/Assets/Sparta/EditorTools/Editor/Utils/SPTransformInspector.cs
@@ -1,8 +1,9 @@
+#if !NGUI
 using UnityEngine;
 using UnityEditor;
 
 [CanEditMultipleObjects]
-[CustomEditor(typeof(Transform), true)]
+[CustomEditor (typeof(Transform), true)]
 public class SPTransformInspector : Editor
 {
 	static public SPTransformInspector instance;
@@ -15,55 +16,61 @@ public class SPTransformInspector : Editor
 	{
 		instance = this;
 
-		mPos = serializedObject.FindProperty("m_LocalPosition");
-		mRot = serializedObject.FindProperty("m_LocalRotation");
-		mScale = serializedObject.FindProperty("m_LocalScale");
+		mPos = serializedObject.FindProperty ("m_LocalPosition");
+		mRot = serializedObject.FindProperty ("m_LocalRotation");
+		mScale = serializedObject.FindProperty ("m_LocalScale");
 	}
 
-	void OnDestroy () { instance = null; }
+	void OnDestroy ()
+	{
+		instance = null;
+	}
 
 
 	public override void OnInspectorGUI ()
 	{
 		EditorGUIUtility.labelWidth = 15f;
-		serializedObject.Update();
+		serializedObject.Update ();
 	
-		DrawPosition();
-		DrawRotation();
-		DrawScale();
+		DrawPosition ();
+		DrawRotation ();
+		DrawScale ();
 
-		serializedObject.ApplyModifiedProperties();
+		serializedObject.ApplyModifiedProperties ();
 
 	}
 
 	void DrawPosition ()
 	{
-		GUILayout.BeginHorizontal();
-		bool reset = GUILayout.Button("P", GUILayout.Width(20f));
-		EditorGUILayout.PropertyField(mPos.FindPropertyRelative("x"));
-		EditorGUILayout.PropertyField(mPos.FindPropertyRelative("y"));
-		EditorGUILayout.PropertyField(mPos.FindPropertyRelative("z"));
-		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal ();
+		bool reset = GUILayout.Button ("P", GUILayout.Width (20f));
+		EditorGUILayout.PropertyField (mPos.FindPropertyRelative ("x"));
+		EditorGUILayout.PropertyField (mPos.FindPropertyRelative ("y"));
+		EditorGUILayout.PropertyField (mPos.FindPropertyRelative ("z"));
+		GUILayout.EndHorizontal ();
 
-		if (reset) mPos.vector3Value = Vector3.zero;
+		if (reset)
+			mPos.vector3Value = Vector3.zero;
 	}
 
 	void DrawScale ()
 	{
-		GUILayout.BeginHorizontal();
+		GUILayout.BeginHorizontal ();
 		{
-			bool reset = GUILayout.Button("S", GUILayout.Width(20f));
+			bool reset = GUILayout.Button ("S", GUILayout.Width (20f));
 
-			EditorGUILayout.PropertyField(mScale.FindPropertyRelative("x"));
-			EditorGUILayout.PropertyField(mScale.FindPropertyRelative("y"));
-			EditorGUILayout.PropertyField(mScale.FindPropertyRelative("z"));
+			EditorGUILayout.PropertyField (mScale.FindPropertyRelative ("x"));
+			EditorGUILayout.PropertyField (mScale.FindPropertyRelative ("y"));
+			EditorGUILayout.PropertyField (mScale.FindPropertyRelative ("z"));
 
-			if (reset) mScale.vector3Value = Vector3.one;
+			if (reset)
+				mScale.vector3Value = Vector3.one;
 		}
-		GUILayout.EndHorizontal();
+		GUILayout.EndHorizontal ();
 	}
 
-#region 
+	#region
+
 	enum Axes : int
 	{
 		None = 0,
@@ -79,9 +86,12 @@ public class SPTransformInspector : Editor
 
 		Axes axes = Axes.None;
 
-		if (Differs(next.x, original.x)) axes |= Axes.X;
-		if (Differs(next.y, original.y)) axes |= Axes.Y;
-		if (Differs(next.z, original.z)) axes |= Axes.Z;
+		if (Differs (next.x, original.x))
+			axes |= Axes.X;
+		if (Differs (next.y, original.y))
+			axes |= Axes.Y;
+		if (Differs (next.z, original.z))
+			axes |= Axes.Z;
 
 		return axes;
 	}
@@ -90,14 +100,13 @@ public class SPTransformInspector : Editor
 	{
 		Axes axes = Axes.None;
 
-		if (property.hasMultipleDifferentValues)
-		{
+		if (property.hasMultipleDifferentValues) {
 			Vector3 original = property.quaternionValue.eulerAngles;
 
-			foreach (Object obj in serializedObject.targetObjects)
-			{
-				axes |= CheckDifference(obj as Transform, original);
-				if (axes == Axes.All) break;
+			foreach (Object obj in serializedObject.targetObjects) {
+				axes |= CheckDifference (obj as Transform, original);
+				if (axes == Axes.All)
+					break;
 			}
 		}
 		return axes;
@@ -108,89 +117,89 @@ public class SPTransformInspector : Editor
 		float newValue = value;
 		GUI.changed = false;
 
-		if (!hidden)
-		{
-			if (greyedOut)
-			{
-				GUI.color = new Color(0.7f, 0.7f, 0.7f);
-				newValue = EditorGUILayout.FloatField(name, newValue, opt);
+		if (!hidden) {
+			if (greyedOut) {
+				GUI.color = new Color (0.7f, 0.7f, 0.7f);
+				newValue = EditorGUILayout.FloatField (name, newValue, opt);
 				GUI.color = Color.white;
+			} else {
+				newValue = EditorGUILayout.FloatField (name, newValue, opt);
 			}
-			else
-			{
-				newValue = EditorGUILayout.FloatField(name, newValue, opt);
-			}
-		}
-		else if (greyedOut)
-		{
-			GUI.color = new Color(0.7f, 0.7f, 0.7f);
-			float.TryParse(EditorGUILayout.TextField(name, "--", opt), out newValue);
+		} else if (greyedOut) {
+			GUI.color = new Color (0.7f, 0.7f, 0.7f);
+			float.TryParse (EditorGUILayout.TextField (name, "--", opt), out newValue);
 			GUI.color = Color.white;
-		}
-		else
-		{
-			float.TryParse(EditorGUILayout.TextField(name, "--", opt), out newValue);
+		} else {
+			float.TryParse (EditorGUILayout.TextField (name, "--", opt), out newValue);
 		}
 
-		if (GUI.changed && Differs(newValue, value))
-		{
+		if (GUI.changed && Differs (newValue, value)) {
 			value = newValue;
 			return true;
 		}
 		return false;
 	}
 
-	static bool Differs (float a, float b) { return Mathf.Abs(a - b) > 0.0001f; }
+	static bool Differs (float a, float b)
+	{
+		return Mathf.Abs (a - b) > 0.0001f;
+	}
 
 	void DrawRotation ()
 	{
-		GUILayout.BeginHorizontal();
+		GUILayout.BeginHorizontal ();
 		{
-			bool reset = GUILayout.Button("R", GUILayout.Width(20f));
+			bool reset = GUILayout.Button ("R", GUILayout.Width (20f));
 
 			Vector3 visible = (serializedObject.targetObject as Transform).localEulerAngles;
 
-			visible.x = WrapAngle(visible.x);
-			visible.y = WrapAngle(visible.y);
-			visible.z = WrapAngle(visible.z);
+			visible.x = WrapAngle (visible.x);
+			visible.y = WrapAngle (visible.y);
+			visible.z = WrapAngle (visible.z);
 
-			Axes changed = CheckDifference(mRot);
+			Axes changed = CheckDifference (mRot);
 			Axes altered = Axes.None;
 
-			GUILayoutOption opt = GUILayout.MinWidth(30f);
+			GUILayoutOption opt = GUILayout.MinWidth (30f);
 
-			if (FloatField("X", ref visible.x, (changed & Axes.X) != 0, false, opt)) altered |= Axes.X;
-			if (FloatField("Y", ref visible.y, (changed & Axes.Y) != 0, false, opt)) altered |= Axes.Y;
-			if (FloatField("Z", ref visible.z, (changed & Axes.Z) != 0, false, opt)) altered |= Axes.Z;
+			if (FloatField ("X", ref visible.x, (changed & Axes.X) != 0, false, opt))
+				altered |= Axes.X;
+			if (FloatField ("Y", ref visible.y, (changed & Axes.Y) != 0, false, opt))
+				altered |= Axes.Y;
+			if (FloatField ("Z", ref visible.z, (changed & Axes.Z) != 0, false, opt))
+				altered |= Axes.Z;
 
-			if (reset)
-			{
+			if (reset) {
 				mRot.quaternionValue = Quaternion.identity;
-			}
-			else if (altered != Axes.None)
-			{
-				foreach (Object obj in serializedObject.targetObjects)
-				{
+			} else if (altered != Axes.None) {
+				foreach (Object obj in serializedObject.targetObjects) {
 					Transform t = obj as Transform;
 					Vector3 v = t.localEulerAngles;
 
-					if ((altered & Axes.X) != 0) v.x = visible.x;
-					if ((altered & Axes.Y) != 0) v.y = visible.y;
-					if ((altered & Axes.Z) != 0) v.z = visible.z;
+					if ((altered & Axes.X) != 0)
+						v.x = visible.x;
+					if ((altered & Axes.Y) != 0)
+						v.y = visible.y;
+					if ((altered & Axes.Z) != 0)
+						v.z = visible.z;
 
 					t.localEulerAngles = v;
 				}
 			}
 		}
-		GUILayout.EndHorizontal();
+		GUILayout.EndHorizontal ();
 	}
 
 
 	static public float WrapAngle (float angle)
 	{
-		while (angle > 180f) angle -= 360f;
-		while (angle < -180f) angle += 360f;
+		while (angle > 180f)
+			angle -= 360f;
+		while (angle < -180f)
+			angle += 360f;
 		return angle;
 	}
-#endregion
+
+	#endregion
 }
+#endif
