7427a1128767a8d0428eb03196d4c08f99964a96
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2017-01-10 19:26:29 +0100

    Added compiler settings file and window. Unified Compiler and Proxy settings windows. Added code to manage rsp files.

---
+++ Project/Assets/Sparta/EditorTools/Editor/View/SettingsWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using SpartaTools.Editor.Build;
+
+namespace SpartaTools.Editor.View
+{
+    public sealed class SettingsWindow : ComposedWindow
+    {
+        [MenuItem("Sparta/Settings...", false, 300)]
+        public static void OpenProxySettings()
+        {
+            EditorWindow.GetWindow(typeof(SettingsWindow), false, "Settings", true);
+        }
+
+        public SettingsWindow()
+        {
+            Views = new ISubWindow[] { new CompilerSettingsWindow(), new ProxySettingsWindow() };
+        }
+
+        protected void OnFocus()
+        {
+            Sparta.SetIcon(this, "Settings", "Sparta Editor Settings");
+        }
+
+    }
+}
