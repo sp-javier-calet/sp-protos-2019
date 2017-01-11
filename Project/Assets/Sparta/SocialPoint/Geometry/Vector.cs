889566f05639b2622eb0672bf6b39393eee16e56
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2017-01-11 12:31:06 +0100

    Revert "Some conversions and added Quaternion class."
    This reverts commit 6e14b7c6c8194f61e744b0029bb2be88c66388ff.

--- Project/Assets/Sparta/SocialPoint/Geometry/Vector.cs
+++ Project/Assets/Sparta/SocialPoint/Geometry/Vector.cs
@@ -33,41 +33,4 @@ namespace SocialPoint.Geometry
             return new UnityEngine.Vector3(v._x, v._y, v._z);
         }
     }
-
-
-    // TODO Move to file
-    public partial struct Quat
-    {
-        float _x;
-        float _y;
-        float _z;
-        float _w;
-
-        public Quat(float x, float y, float z, float w)
-        {
-            _x = x;
-            _y = y;
-            _z = z;
-            _w = w;
-        }
-    }
-
-    // UnityVector adapter
-    public partial struct Quat
-    {
-        public static Quat Convert(UnityEngine.Quaternion q)
-        {
-            return new Quat(q.x, q.y, q.z, q.w);
-        }
-
-        public static implicit operator Quat(UnityEngine.Quaternion q)
-        {
-            return new Quat(q.x, q.y, q.z, q.w);
-        }
-
-        public static implicit operator UnityEngine.Quaternion(Quat q)
-        {
-            return new UnityEngine.Quaternion(q._x, q._y, q._z, q._w);
-        }
-    }
 }
