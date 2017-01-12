314a55f38c2267d90455b62cc8d4ab6d90384232
Manuel Álvarez de Toledo <manuel.alvarez@socialpoint.es>
2017-01-12 12:59:02 +0100

    Created EventSystemsInstaller to manage EventSystem creation. Removed EventSystem objects from Base scenes. Create EventSystem prefab with map configuration.

--- Project/Assets/Sparta/SocialPoint/AdminPanel/Installers/AdminPanelInstaller.cs
+++ Project/Assets/Sparta/SocialPoint/AdminPanel/Installers/AdminPanelInstaller.cs
@@ -3,12 +3,16 @@ using SocialPoint.AdminPanel;
 using SocialPoint.Utils;
 using SocialPoint.Profiling;
 using SocialPoint.Attributes;
+using SocialPoint.Base;
+using UnityEngine.EventSystems;
 
-public class AdminPanelInstaller : ServiceInstaller
+public class AdminPanelInstaller : ServiceInstaller, IInitializable
 {
     public override void InstallBindings()
     {
 #if (ADMIN_PANEL && !NO_ADMIN_PANEL) || UNITY_EDITOR
+        Container.Bind<IInitializable>().ToInstance(this);
+
         Container.Rebind<AdminPanel>().ToMethod<AdminPanel>(CreateAdminPanel);
         Container.Bind<IAdminPanelConfigurer>().ToSingle<AdminPanelSceneSelector>();
         Container.Bind<IAdminPanelConfigurer>().ToSingle<AdminPanelLog>();
@@ -23,5 +27,12 @@ public class AdminPanelInstaller : ServiceInstaller
     {
         return new AdminPanel(
             Container.ResolveList<IAdminPanelConfigurer>());
+    }
+
+    public void Initialize()
+    {
+        // Force resolution of EventSystem as a requirement
+        var eventSystem = Container.Resolve<EventSystem>();
+        DebugUtils.Assert(eventSystem != null, "An EventSystem is required for AdminPanel UI");
     }
 }
