e0904a0f218f4c9f457ca60edcbec3e4ec48a01b
Fernando Serra <fernando.serra@socialpoint.es>
2017-01-17 10:02:23 +0100

    * Divided AdminPanelSocialFramework in different files

---
+++ Project/Assets/Sparta/Extensions/Social/SocialFramework/AdminPanel/AdminPanelSocialFrameworkPlayers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SocialPoint.AdminPanel;
+
+namespace SocialPoint.Social
+{
+    public class AdminPanelPlayers : IAdminPanelGUI
+    {
+        public void OnCreateGUI(AdminPanelLayout layout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
