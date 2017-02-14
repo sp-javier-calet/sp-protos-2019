ed7f4db7d4904e217ffef4f38cafac4b71721ec5
Ivan Hernández <ivan.hernandez@socialpoint.es>
2017-02-14 12:23:56 +0100

    parsing bundle data on game loading controller when login finishes

--- Project/Assets/Scripts/GUI/GameLoadingController.cs
+++ Project/Assets/Scripts/GUI/GameLoadingController.cs
@@ -1,5 +1,6 @@
 using SocialPoint.AdminPanel;
 using SocialPoint.AppEvents;
+using SocialPoint.AssetBundlesClient;
 using SocialPoint.Attributes;
 using SocialPoint.Crash;
 using SocialPoint.Dependency;
@@ -28,6 +29,8 @@ public class GameLoadingController : SocialPoint.GameLoading.GameLoadingControll
     const float ExpectedLoadModelDuration = 1.0f;
     const float ExpectedLoadSceneDuration = 2.0f;
 
+    AssetBundleManager _assetBundleManager;
+
     protected override void OnLoad()
     {
         Login = Services.Instance.Resolve<ILogin>();
@@ -37,6 +40,8 @@ public class GameLoadingController : SocialPoint.GameLoading.GameLoadingControll
         ErrorHandler = Services.Instance.Resolve<IGameErrorHandler>();
         _coroutineRunner = Services.Instance.Resolve<ICoroutineRunner>();
         _gameLoader = Services.Instance.Resolve<IGameLoader>();
+
+        _assetBundleManager = Services.Instance.Resolve<AssetBundleManager>();
 
         #if ADMIN_PANEL
         _adminPanel = Services.Instance.Resolve<AdminPanel>();
@@ -90,8 +95,31 @@ public class GameLoadingController : SocialPoint.GameLoading.GameLoadingControll
     {
         var data = reader.ParseElement();
         _gameLoader.Load(data);
+
+        ParseBundleData(data);
+
         _loadModelOperation.Finish("game model loaded");
         return true;
+    }
+
+    void ParseBundleData(Attr data)
+    {
+        if(_assetBundleManager != null)
+        {
+            const string configKey = "config";
+            const string bundleDataKey = "bundle_data";
+
+            var dataDic = data.AssertDic;
+            if(dataDic.ContainsKey(configKey))
+            {
+                var configData = dataDic.Get(configKey).AssertDic;
+                if(configData.ContainsKey(bundleDataKey))
+                {
+                    var bundleData = configData.Get(bundleDataKey).AssertDic;
+                    _assetBundleManager.Init(bundleData.Get(bundleDataKey).AssertList);
+                }
+            }
+        }
     }
 
     void OnConfirmLinkEvent(ILink link, LinkConfirmType type, Attr data, ConfirmBackLinkDelegate cbk)
