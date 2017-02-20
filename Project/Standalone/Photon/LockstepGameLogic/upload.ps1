5a2d55fb9234631e1ad87deb803f05deab9c4a27
Miguel Ibero <miguel.ibero@socialpoint.es>
2017-02-20 11:43:07 +0100

    reordered stuff

--- Project/Standalone/Photon/LockstepGameLogic/upload.ps1
+++ Project/Standalone/Photon/LockstepGameLogic/upload.ps1
@@ -1,18 +1,19 @@
 
-Import-Module .\sas-client\Photon.PrivateCloud.Plugin.Client.SAS.psm1
+Import-Module ..\Server\sas-client\Photon.PrivateCloud.Plugin.Client.SAS.psm1
 
 $vsPath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE"
-$basePath = ".\src-server\Plugins"
-$solutionName = "Lockstep"
+$basePath = "."
+$solutionName = "LockstepGameLogic"
 $solutionPath = "$basePath\$solutionName.sln"
-$buildPath = "$basePath\$solutionName\bin"
+$buildPath = "$basePath\bin"
 $customer = "Socialpoint"
 $key = "66QFur7XJcyRsCXxuk7yrtwDHw8Xsne9"
-$plugin = "PhotonHive.LockstepPlugin"
+$plugin = "LockstepPlugin"
 
 Write-Host "building server solution..."
 $p = Start-Process -FilePath $vsPath\devenv.exe -ArgumentList $solutionPath,"/Rebuild Release" -PassThru
 $null = $p.WaitForExit(-1)
+Write-Host ""
 Write-Host "server solution ready!"
 Write-Host "creating plugin package..."
 Compress-Archive -Path $buildPath -DestinationPath archive.zip -Force
