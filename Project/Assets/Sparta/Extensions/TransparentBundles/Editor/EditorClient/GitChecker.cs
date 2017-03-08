d99465804998979d5292a0c84dd2f12b49f1c14c
Juan Legaz <juan.legaz@socialpoint.es>
2017-03-08 11:44:59 +0100

    Branch compare WIP

--- Project/Assets/Sparta/Extensions/TransparentBundles/Editor/EditorClient/GitChecker.cs
+++ Project/Assets/Sparta/Extensions/TransparentBundles/Editor/EditorClient/GitChecker.cs
@@ -7,14 +7,44 @@ namespace SocialPoint.TransparentBundles
 {
     public static class GitChecker
     {
+        const string _gitModifiedToken = " M";
+        const string _gitUntrackedToken = "??";
 
-        public static bool CheckFilePending(ref List<string> infractions, params string[] assetPaths)
+        public static List<string> CheckInBranchUpdated(string branchName)
         {
-            var query = new Repository(Application.dataPath).CreateQuery("status").WithArg("--porcelain");
+            var infractions = new List<string>();
+            var repo = new Repository(Application.dataPath);
+
+            var query = repo.CreateQuery("rev-parse").WithOption("abbrev-ref HEAD");
+
+            var currentBranch = query.Exec().TrimEnd('\n');
+
+            if(currentBranch != branchName)
+            {
+                infractions.Add(string.Format("The current branch is {0}. The configured branch for Transparent Bundles is {1}", currentBranch, branchName));
+            }
+
+            var queryLocal = repo.CreateQuery("log").WithArg("-1").WithOption("oneline").WithArg(currentBranch);
+            var queryRemote = repo.CreateQuery("log").WithArg("-1").WithOption("oneline").WithArg("origin/" + currentBranch);
+            var lastLocalCommit = queryLocal.Exec().TrimEnd('\n');
+            var lastFetchedRemoteCommit = queryRemote.Exec().TrimEnd('\n');
+
+
+            if(lastLocalCommit != lastFetchedRemoteCommit)
+            {
+                infractions.Add(string.Format("The current commit is {0}. The remote commit is {1}. Please update your changes with pull/push.", lastLocalCommit, lastFetchedRemoteCommit));
+            }
+
+            return infractions;
+        }
+
+        public static List<string> CheckFilePending(params string[] assetPaths)
+        {
+            var infractions = new List<string>();
+            var query = new Repository(Application.dataPath).CreateQuery("status").WithOption("porcelain");
 
             var gitStatus = query.Exec();
 
-            bool anyInfraction = false;
 
             for(int i = 0; i < assetPaths.Length; i++)
             {
@@ -23,7 +53,6 @@ namespace SocialPoint.TransparentBundles
 
                 if(idx != -1)
                 {
-                    anyInfraction = true;
                     var shiftedIdx = 1;
                     var prevChar = gitStatus[idx - 1];
 
@@ -36,11 +65,11 @@ namespace SocialPoint.TransparentBundles
 
                     var line = gitStatus.Substring(idx, shiftedIdx + path.Length);
 
-                    if(line.StartsWith("M"))
+                    if(line.StartsWith(_gitModifiedToken))
                     {
-                        line = line.Remove(0).Insert(0, "This file is modified and pending to commit:");
+                        line = line.Remove(0, 2).Insert(0, "This file is modified and pending to commit:");
                     }
-                    else if(line.StartsWith("??"))
+                    else if(line.StartsWith(_gitUntrackedToken))
                     {
                         line = line.Remove(0, 2).Insert(0, "This file is new and pending to commit:");
                     }
@@ -49,7 +78,7 @@ namespace SocialPoint.TransparentBundles
                 }
             }
 
-            return anyInfraction;
+            return infractions;
         }
     }
 }
