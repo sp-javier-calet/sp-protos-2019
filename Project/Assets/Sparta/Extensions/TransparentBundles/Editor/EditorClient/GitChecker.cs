a32364a9a6cda1d357c6a70b773fc44d4aeb25fd
Juan Legaz <juan.legaz@socialpoint.es>
2017-03-08 12:20:50 +0100

    Branch and commit comparison

--- Project/Assets/Sparta/Extensions/TransparentBundles/Editor/EditorClient/GitChecker.cs
+++ Project/Assets/Sparta/Extensions/TransparentBundles/Editor/EditorClient/GitChecker.cs
@@ -21,15 +21,14 @@ namespace SocialPoint.TransparentBundles
 
             if(currentBranch != branchName)
             {
-                infractions.Add(string.Format("The current branch is {0}. The configured branch for Transparent Bundles is {1}", currentBranch, branchName));
+                infractions.Add(string.Format("The current branch is {0}. Was expecting {1}.", currentBranch, branchName));
             }
 
             var queryLocal = repo.CreateQuery("log").WithArg("-1").WithOption("oneline").WithArg(currentBranch);
-            var queryRemote = repo.CreateQuery("log").WithArg("-1").WithOption("oneline").WithArg("origin/" + currentBranch);
+            var queryRemote = repo.CreateQuery("log").WithArg("-1").WithOption("oneline").WithArg(currentBranch + "@{u}");
+
             var lastLocalCommit = queryLocal.Exec().TrimEnd('\n');
             var lastFetchedRemoteCommit = queryRemote.Exec().TrimEnd('\n');
-
-
             if(lastLocalCommit != lastFetchedRemoteCommit)
             {
                 infractions.Add(string.Format("The current commit is {0}. The remote commit is {1}. Please update your changes with pull/push.", lastLocalCommit, lastFetchedRemoteCommit));
