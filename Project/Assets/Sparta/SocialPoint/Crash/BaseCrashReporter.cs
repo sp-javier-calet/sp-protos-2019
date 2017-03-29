4e29b5137dbcc3b8f37163258062128eda2e1eb9
Ivan Hernández <ivan.hernandez@socialpoint.es>
2017-03-29 18:53:09 +0200

    crash reporter installer is now IInitializable in order to be enabled before login. Also, other installers minor fixes.

--- Project/Assets/Sparta/SocialPoint/Crash/BaseCrashReporter.cs
+++ Project/Assets/Sparta/SocialPoint/Crash/BaseCrashReporter.cs
@@ -660,11 +660,13 @@ namespace SocialPoint.Crash
 
         void SendCrashesAfterLogin(Action callback = null)
         {
+            DebugUtils.Assert(IsEnabled, "CrashReporter should be already enabled after login"); 
             SendCrashesWithSafeCallback(ReportSendType.AfterLogin, callback);
         }
 
         public void SendCrashesBeforeLogin(Action callback)
         {
+            DebugUtils.Assert(IsEnabled, "CrashReporter should be enabled before login"); 
             SendCrashesWithSafeCallback(ReportSendType.BeforeLogin, callback);
         }
 
@@ -1055,7 +1057,6 @@ namespace SocialPoint.Crash
 
         void OnGameWasLoaded()
         {
-            Enable();
             SendCrashesAfterLogin();
         }
 
