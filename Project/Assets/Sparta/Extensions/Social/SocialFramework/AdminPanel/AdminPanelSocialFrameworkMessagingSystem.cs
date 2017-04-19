194ace0f780215a501a4614ba042b1a625042ba3
Fernando Serra <fernando.serra@socialpoint.es>
2017-04-19 18:33:13 +0200

    * Added AdminPanel panel to send messages

--- Project/Assets/Sparta/Extensions/Social/SocialFramework/AdminPanel/AdminPanelSocialFrameworkMessagingSystem.cs
+++ Project/Assets/Sparta/Extensions/Social/SocialFramework/AdminPanel/AdminPanelSocialFrameworkMessagingSystem.cs
@@ -14,11 +14,13 @@ namespace SocialPoint.Social
     {
         readonly AdminPanelMessagesList _messagesListPanel;
         readonly AdminPanelMessageInfo _messageInfoPanel;
+        readonly AdminPanelSendMessage _messageSendPanel;
 
         public AdminPanelSocialFrameworkMessagingSystem(AdminPanelConsole console, MessagingSystemManager messagesManager)
         {
             _messageInfoPanel = new AdminPanelMessageInfo(messagesManager, console);
             _messagesListPanel = new AdminPanelMessagesList(messagesManager, console, _messageInfoPanel);
+            _messageSendPanel = new AdminPanelSendMessage(messagesManager, console);
         }
 
         public void OnCreateGUI(AdminPanelLayout layout)
@@ -26,6 +28,7 @@ namespace SocialPoint.Social
             layout.CreateLabel("Messaging System");
             layout.CreateMargin();
             layout.CreateOpenPanelButton("Messages List", _messagesListPanel);
+            layout.CreateOpenPanelButton("New Message", _messageSendPanel);
         }
 
         #region Base Panels
@@ -41,6 +44,17 @@ namespace SocialPoint.Social
                 _messagesManager = messagesManager;
                 _console = console;
                 _requestInProgress = false;
+            }
+
+            protected void CreateRequestInProgressGUI(AdminPanelLayout layout)
+            {
+                layout.CreateLabel("Refreshing...");
+            }
+
+            protected void CreateRequestErrorGUI(AdminPanelLayout layout)
+            {
+                layout.CreateLabel("Request failed:");
+                layout.CreateTextArea(_wampRequestError.ToString());
             }
         }
 
@@ -103,17 +117,6 @@ namespace SocialPoint.Social
                 }
             }
 
-            void CreateRequestInProgressGUI(AdminPanelLayout layout)
-            {
-                layout.CreateLabel("Refreshing...");
-            }
-
-            void CreateRequestErrorGUI(AdminPanelLayout layout)
-            {
-                layout.CreateLabel("Request failed:");
-                layout.CreateTextArea(_wampRequestError.ToString());
-            }
-
             void CreateMessageGUI(AdminPanelLayout layout)
             {
                 layout.CreateLabel("Id: " + Msg.Id);
@@ -138,7 +141,7 @@ namespace SocialPoint.Social
                     {
                         var hLayout = layout.CreateHorizontalLayout();
                         hLayout.CreateLabel(itr.Current);
-                        MessagingSystemManager.FinishCallback finishCallback = (Error error, AttrDic dic) => {
+                        MessagingSystemManager.FinishCallback finishCallback = (error, dic) => {
                             _requestInProgress = false;
                             _wampRequestError = error;
                             if(Error.IsNullOrEmpty(_wampRequestError))
@@ -164,8 +167,8 @@ namespace SocialPoint.Social
 
             void CreateAddPropertyGUI(AdminPanelLayout layout)
             {
-                layout.CreateTextInput("Insert new property", (property) => {
-                    MessagingSystemManager.FinishCallback finishCallback = (Error error, AttrDic dic) => {
+                layout.CreateTextInput("Insert new property", property => {
+                    MessagingSystemManager.FinishCallback finishCallback = (error, dic) => {
                         _requestInProgress = false;
                         _wampRequestError = error;
                         if(Error.IsNullOrEmpty(_wampRequestError))
@@ -188,7 +191,7 @@ namespace SocialPoint.Social
 
             void CreateDeleteMessageGUI(AdminPanelLayout layout)
             {
-                MessagingSystemManager.FinishCallback finishCbk = (Error error, AttrDic dic) => {
+                MessagingSystemManager.FinishCallback finishCbk = (error, dic) => {
                     _requestInProgress = false;
                     _wampRequestError = error;
                     if(Error.IsNullOrEmpty(_wampRequestError))
@@ -209,6 +212,195 @@ namespace SocialPoint.Social
                     _wampRequest = _messagesManager.DeleteMessage(Msg, finishCbk);
                     layout.Refresh();
                 });
+            }
+        }
+
+        class AdminPanelSendMessage : BaseMessagingSystemPanel
+        {
+            string _currentPayload;
+            readonly Dictionary<string, AdminPanelEditPayloadPanel> _payloadPanels;
+            string _currentDestination;
+            readonly Dictionary<string, AdminPanelEditDestinationPanel> _destinationPanels;
+
+            public AdminPanelSendMessage(MessagingSystemManager messagesManager, AdminPanelConsole console) : base(messagesManager, console)
+            {
+                _payloadPanels = new Dictionary<string, AdminPanelEditPayloadPanel>();
+                _payloadPanels.Add(MessagePayloadPlainText.Identifier, new AdminPanelMessagePayloadPlainText());
+                _currentPayload = _payloadPanels.Keys.First();
+
+                _destinationPanels = new Dictionary<string, AdminPanelEditDestinationPanel>();
+                _destinationPanels.Add("user", new AdminPanelUserDestination());
+                _currentDestination = _destinationPanels.Keys.First();
+            }
+
+            public override void OnCreateGUI(AdminPanelLayout layout)
+            {
+                if(_requestInProgress)
+                {
+                    CreateRequestInProgressGUI(layout);
+                }
+                else if(!Error.IsNullOrEmpty(_wampRequestError))
+                {
+                    CreateRequestErrorGUI(layout);
+                }
+                else
+                {
+                    layout.CreateLabel("Send Message");
+                    layout.CreateMargin();
+                    CreateDestinationGUI(layout);
+                    layout.CreateMargin();
+                    CreatePayloadGUI(layout);
+                    CreateSendGUI(layout);
+                }
+            }
+
+            void CreateDestinationGUI(AdminPanelLayout layout)
+            {
+                var fLayout = layout.CreateFoldoutLayout("Destination type:");
+                using(var itr = _destinationPanels.GetEnumerator())
+                {
+                    while(itr.MoveNext())
+                    {
+                        var currentStr = itr.Current.Key;
+                        bool status = currentStr == _currentDestination;
+                        fLayout.CreateToggleButton(currentStr, status, selected => {
+                            _currentDestination = currentStr;
+                            layout.Refresh();
+                        });
+                    }
+                }
+
+                layout.CreateTextArea(_destinationPanels[_currentDestination].GetData().ToString());
+                layout.CreateOpenPanelButton("Edit destination", _destinationPanels[_currentDestination]);
+            }
+
+            void CreatePayloadGUI(AdminPanelLayout layout)
+            {
+                var fLayout = layout.CreateFoldoutLayout("Payload type:");
+                using(var itr = _payloadPanels.GetEnumerator())
+                {
+                    while(itr.MoveNext())
+                    {
+                        var currentStr = itr.Current.Key;
+                        bool status = currentStr == _currentPayload;
+                        fLayout.CreateToggleButton(currentStr, status, selected => {
+                            _currentPayload = currentStr;
+                            layout.Refresh();
+                        });
+                    }
+                }
+
+                layout.CreateTextArea(_payloadPanels[_currentPayload].GetPayload().ToString());
+                layout.CreateOpenPanelButton("Edit payload", _payloadPanels[_currentPayload]);
+            }
+
+            void CreateSendGUI(AdminPanelLayout layout)
+            {
+                layout.CreateButton("Send", () => {
+                    var payload = _payloadPanels[_currentPayload].GetPayload();
+                    var destination = _destinationPanels[_currentDestination].GetData();
+                    MessagingSystemManager.FinishCallback finishCallback = (error, dic) => {
+                        _requestInProgress = false;
+                        _wampRequestError = error;
+                        if(Error.IsNullOrEmpty(_wampRequestError))
+                        {
+                            _console.Print(string.Format("Message with payload {0} sent to {1}", payload, destination));
+                        }
+                        else
+                        {
+                            _console.Print(string.Format("Error sending message {0} to {1}. Error: {2}", payload, destination, _wampRequestError));
+                        }
+                        layout.Refresh();
+                    };
+                    Cancel();
+                    _requestInProgress = true;
+                    _console.Print(string.Format("Sending message {0} to {1}", payload, destination));
+                    _messagesManager.SendMessage(destination.Type, destination.Data, payload, finishCallback);
+                });
+            }
+        }
+
+        public class DestinationData
+        {
+            public string Type{ get; set; }
+
+            public AttrDic Data{ get; set; }
+
+            public DestinationData()
+            {
+                Data = new AttrDic();
+            }
+
+            public override string ToString()
+            {
+                return string.Format("[DestinationData: Type={0}, Data={1}]", Type, Data);
+            }
+        }
+
+        public abstract class AdminPanelEditDestinationPanel : IAdminPanelGUI
+        {
+            public abstract DestinationData GetData();
+
+            public abstract void OnCreateGUI(AdminPanelLayout layout);
+        }
+
+        class AdminPanelUserDestination : AdminPanelEditDestinationPanel
+        {
+            public DestinationData Destination{ get; private set; }
+
+            public AdminPanelUserDestination()
+            {
+                Destination = new DestinationData();
+                Destination.Type = "user";
+                Destination.Data.SetValue("id", "");
+            }
+
+            public override DestinationData GetData()
+            {
+                return Destination;
+            }
+
+            public override void OnCreateGUI(AdminPanelLayout layout)
+            {
+                layout.CreateLabel("Edit user destination");
+                layout.CreateMargin();
+                var idInput = layout.CreateTextInput("UserId", id => Destination.Data.SetValue("id", id));
+                idInput.text = Destination.Data.GetValue("id").ToString();
+
+                layout.CreateButton("Ok", layout.ClosePanel);
+            }
+        }
+
+        public abstract class AdminPanelEditPayloadPanel : IAdminPanelGUI
+        {
+            public abstract IMessagePayload GetPayload();
+
+            public abstract void OnCreateGUI(AdminPanelLayout layout);
+        }
+
+        class AdminPanelMessagePayloadPlainText : AdminPanelEditPayloadPanel
+        {
+            string _title;
+            string _text;
+
+            public override IMessagePayload GetPayload()
+            {
+                return new MessagePayloadPlainText(_title, _text);
+            }
+
+            public override void OnCreateGUI(AdminPanelLayout layout)
+            {
+                layout.CreateLabel("Edit plain text payload");
+                layout.CreateMargin();
+                var titleInput = layout.CreateTextInput("Title", title => {
+                    _title = title;
+                });
+                titleInput.text = _title;
+                var textInput = layout.CreateTextInput("Text", text => {
+                    _text = text;
+                });
+                textInput.text = _text;
+                layout.CreateButton("Ok", layout.ClosePanel);
             }
         }
     }
