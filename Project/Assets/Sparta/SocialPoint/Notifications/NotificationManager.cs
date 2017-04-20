e336358ca5324344eef8edf4c48838c46357945c
Andres Barrera <andres.barrera@socialpoint.es>
2017-04-20 13:05:04 +0200

    Revert "I_AM_LOD. Change to avoid auto start for event tracker. Keep or discard?"
    This reverts commit c39a4eb557e8b97854c461c4712012f6b3f4abbd.

--- Project/Assets/Sparta/SocialPoint/Notifications/NotificationManager.cs
+++ Project/Assets/Sparta/SocialPoint/Notifications/NotificationManager.cs
@@ -173,9 +173,6 @@ namespace SocialPoint.Notifications
 
         void ScheduleNotifications()
         {
-            #if I_AM_LOD //Should be ported to Sparta?? should clear all? should clear only some that will be rescheduled (in _notifications)?
-            ClearNotifications();
-            #endif
             AddGameNotifications();
             for(int i = 0, _notificationsCount = _notifications.Count; i < _notificationsCount; i++)
             {
