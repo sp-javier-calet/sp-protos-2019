68749f015f66d81d55674638fe0ee8c00e00cad0
Andres Barrera <andres.barrera@socialpoint.es>
2017-04-20 12:57:43 +0200

    I_AM_LOD. Getting Ping from Photon in code that should not depend on it.

--- Project/Assets/Sparta/SocialPoint/Profiling/PerfInfo.cs
+++ Project/Assets/Sparta/SocialPoint/Profiling/PerfInfo.cs
@@ -273,6 +273,9 @@ namespace SocialPoint.Profiling
             builder.AppendFormat("fps: {0:F0}\n", Frame.FPS);
             builder.AppendFormat("tris: {0} ({1})\n", Frame.Tris, Frame.BatchedTris);
             builder.AppendFormat("draws: {0} ({1})\n", Frame.DrawCalls, Frame.BatchedDrawCalls);
+            #if I_AM_LOD //Should not depend on Photon
+            builder.AppendFormat("ping: {0}\n", PhotonNetwork.networkingPeer.RoundTripTime.ToString());
+            #endif
             return builder.ToString();
         }
 
