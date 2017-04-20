df398adf60311b6bdbd345509c77f55555b29c87
Andres Barrera <andres.barrera@socialpoint.es>
2017-04-20 11:38:24 +0200

    Performance settings add verification and reset

--- Project/Assets/Sparta/SocialPoint/PerformanceSettings/PerformanceSettingsManager.cs
+++ Project/Assets/Sparta/SocialPoint/PerformanceSettings/PerformanceSettingsManager.cs
@@ -94,7 +94,16 @@ namespace SocialPoint.PerformanceSettings
 
         void Init(AttrDic config)
         {
-            _data.Add(kDefaultSettings, new PerformanceSettingsData(config));
+            if(_data.ContainsKey(kDefaultSettings))
+            {
+                _data[kDefaultSettings] = new PerformanceSettingsData(config);
+            }
+            else
+            {
+                _data.Add(kDefaultSettings, new PerformanceSettingsData(config));
+            }
+            
+
 
             ApplyPerformanceSettings(kDefaultSettings);	
         }
@@ -105,7 +114,17 @@ namespace SocialPoint.PerformanceSettings
             while(itr.MoveNext())
             {
                 var pair = itr.Current;
-                _data.Add(pair.Key, new PerformanceSettingsData(pair.Value.AsDic));
+
+                if(_data.ContainsKey(pair.Key))
+                {
+                    _data[pair.Key] = new PerformanceSettingsData(pair.Value.AsDic);
+                }
+                else
+                {
+                    _data.Add(pair.Key, new PerformanceSettingsData(pair.Value.AsDic));
+                }
+
+
             }
             itr.Dispose();
 
@@ -201,7 +220,8 @@ namespace SocialPoint.PerformanceSettings
 
         void Reset()
         {
-            _data = null;
+            _data.Clear();
+            _login.NewGenericDataEvent -= ParsePerformanceSettings;
             _login = null;
         }
     }
