03360e9d0452e1b140ebc1180253a266a623967f
Andres Barrera <andres.barrera@socialpoint.es>
2017-05-15 12:58:01 +0200

    Basic free storage analyzer class

---
+++ Project/Assets/Sparta/SocialPoint/Hardware/StorageAnalyzer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using SocialPoint.Utils;
+
+namespace SocialPoint.Hardware
+{
+    public class StorageAnalyzer : IUpdateable, IDisposable
+    {
+        [Serializable]
+        public struct Config
+        {
+            public ulong FreeStorageWarning;
+            public float UpdateInterval;
+        }
+
+        public Action<ulong> OnLowStorageWarning;
+
+        Config _config;
+        IStorageInfo _storageInfo;
+        UpdateScheduler _scheduler;
+
+        public StorageAnalyzer(IStorageInfo storageInfo, UpdateScheduler scheduler, Config config)
+        {
+            _storageInfo = storageInfo;
+            _scheduler = scheduler;
+            _config = config;
+            Start();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public void Start()
+        {
+            Stop();
+            _scheduler.Add(this, _config.UpdateInterval);
+        }
+
+        public void Stop()
+        {
+            _scheduler.Remove(this);
+        }
+
+        public void Update()
+        {
+            CheckFreeStorageSpace();
+        }
+
+        void CheckFreeStorageSpace()
+        {
+            var freeStorage = _storageInfo.FreeStorage;
+            if(freeStorage < _config.FreeStorageWarning)
+            {
+                RaiseLowStorageWarning(freeStorage);
+            }
+        }
+
+        void RaiseLowStorageWarning(ulong freeStorage)
+        {
+            if(OnLowStorageWarning != null)
+            {
+                OnLowStorageWarning(freeStorage);
+            }
+        }
+    }
+}
