3b9752cf92e8a0db418c90caf5ec5d583a064bf5
sp-fran-carrascosa <francisco.carrascosa@socialpoint.es>
2017-07-13 18:24:01 +0200

    Memory obfuscation classes

---
+++ Project/Assets/Sparta/SocialPoint/Utils/Obfuscation/Obfuscated.cs
@@ -0,0 +1,51 @@
+﻿namespace SocialPoint.Utils.Obfuscation
+{
+    public abstract class Obfuscated<T>
+    {
+        private static ulong _mask;
+        private T _obfuscatedValue;
+
+        public T Value
+        {
+            get
+            {
+                return Obfuscate(_obfuscatedValue);
+            }
+        }
+
+        public static implicit operator T(Obfuscated<T> obfustated)
+        {
+            return obfustated.Value;
+        }
+        
+        public Obfuscated(T value = default(T))
+        {
+            _obfuscatedValue = default(T);
+
+            Init();
+            Set(value);
+        }
+
+        protected abstract T Obfuscate(T value);
+
+        protected void Set(T value)
+        {
+            _obfuscatedValue = Obfuscate(value);
+        }
+
+        private void Init()
+        {
+            if (0 == _mask)
+            {
+                uint highMask = RandomUtils.GenerateUint();
+                uint lowMask = RandomUtils.GenerateUint();
+                _mask = (ulong)(highMask << 32 | lowMask);
+            }
+        }
+
+        protected void DoObfuscate(ref ulong value)
+        {
+            value ^= _mask;
+        }
+    }
+}
---
+++ Project/Assets/Sparta/SocialPoint/Utils/Obfuscation/ObfuscatedUShort.cs
@@ -0,0 +1,102 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SocialPoint.Utils.Obfuscation
+{
+    public class ObfuscatedUShort : Obfuscated<ushort>
+    {
+        [StructLayout(LayoutKind.Explicit)]
+        public struct UnionMaskUShort
+        {
+            [FieldOffset(0)]
+            public ushort value;
+            [FieldOffset(0)]
+            public ulong mask;
+        }
+
+        public ObfuscatedUShort(ushort value = default(ushort))
+            : base(value)
+        {
+        }
+
+        protected override ushort Obfuscate(ushort value)
+        {
+            UnionMaskUShort reinterpret = default(UnionMaskUShort);
+            reinterpret.value = value;
+            DoObfuscate(ref reinterpret.mask);
+
+            return reinterpret.value;
+        }
+
+        public static ObfuscatedUShort operator +(ObfuscatedUShort obfuscated1, ObfuscatedUShort obfuscated2)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated1.Value + obfuscated2.Value));
+        }
+
+        public static ObfuscatedUShort operator +(ObfuscatedUShort obfuscated, ushort value)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated.Value + value));
+        }
+
+        public static ObfuscatedUShort operator +(ushort value, ObfuscatedUShort obfuscated)
+        {
+            return new ObfuscatedUShort((ushort)(value + obfuscated.Value));
+        }
+
+        public static ObfuscatedUShort operator -(ObfuscatedUShort obfuscated1, ObfuscatedUShort obfuscated2)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated1.Value + obfuscated2.Value));
+        }
+
+        public static ObfuscatedUShort operator -(ObfuscatedUShort obfuscated, ushort value)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated.Value - value));
+        }
+
+        public static ObfuscatedUShort operator -(ushort value, ObfuscatedUShort obfuscated)
+        {
+            return new ObfuscatedUShort((ushort)(value - obfuscated.Value));
+        }
+
+        public static ObfuscatedUShort operator *(ObfuscatedUShort obfuscated1, ObfuscatedUShort obfuscated2)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated1.Value * obfuscated2.Value));
+        }
+
+        public static ObfuscatedUShort operator *(ObfuscatedUShort obfuscated, ushort value)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated.Value * value));
+        }
+
+        public static ObfuscatedUShort operator *(ushort value, ObfuscatedUShort obfuscated)
+        {
+            return new ObfuscatedUShort((ushort)(value * obfuscated.Value));
+        }
+
+        public static ObfuscatedUShort operator /(ObfuscatedUShort obfuscated1, ObfuscatedUShort obfuscated2)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated1.Value / obfuscated2.Value));
+        }
+
+        public static ObfuscatedUShort operator /(ObfuscatedUShort obfuscated, ushort value)
+        {
+            return new ObfuscatedUShort((ushort)(obfuscated.Value / value));
+        }
+
+        public static ObfuscatedUShort operator /(ushort value, ObfuscatedUShort obfuscated)
+        {
+            return new ObfuscatedUShort((ushort)(value / obfuscated.Value));
+        }
+
+        public static ObfuscatedUShort operator ++(ObfuscatedUShort obfuscated)
+        {
+            obfuscated.Set((ushort)(obfuscated.Value + 1));
+            return obfuscated;
+        }
+
+        public static ObfuscatedUShort operator --(ObfuscatedUShort obfuscated)
+        {
+            obfuscated.Set((ushort)(obfuscated.Value - 1));
+            return obfuscated;
+        }
+    }
+}
