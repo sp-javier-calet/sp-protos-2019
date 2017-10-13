072d8007c0aa5e3ecbc394a6935ead87e4ef73c9
Sergi Gil <sergio.gil.calvo@socialpoint.es>
2017-10-13 10:19:46 +0200

    defining default animations for popups

--- Project/Assets/Scripts/GUI/ScreensController.cs
+++ Project/Assets/Scripts/GUI/ScreensController.cs
@@ -4,52 +4,14 @@ using UnityEngine;
 
 public class ScreensController : UIStackController
 {
-    const string kShowAnimName = "AnimIn";
-    const string kHideAnimName = "AnimOut";
-    const float kDefaultAnimationTime = 1.0f;
+    public const float DefaultAnimationTime = 1.0f;
+    float AnimationTime = DefaultAnimationTime;
 
     override protected void OnLoad()
     {
-        float animationTime = Services.Instance.Resolve("popup_animation_time", kDefaultAnimationTime);
-
-        if(Animation == null)
-        {
-            // Example ScaleAnimation
-            AppearAnimation = new ScaleAnimation(animationTime, Vector3.zero, Vector3.one, GoEaseType.QuadIn);
-            DisappearAnimation = new ScaleAnimation(animationTime, Vector3.one, Vector3.zero, GoEaseType.QuadIn);
-
-            // Example SlideAnimation
-//            AppearAnimation = new SlideAnimation(animationTime, SlideAnimation.PosType.Left, SlideAnimation.PosType.Center, GoEaseType.QuadIn);
-//            DisappearAnimation = new SlideAnimation(animationTime, SlideAnimation.PosType.Center, SlideAnimation.PosType.Down, GoEaseType.QuadIn);
-
-            // Example FadeAnimation
-//            AppearAnimation = new FadeAnimation(animationTime, 0f, 1f);
-//            DisappearAnimation = new FadeAnimation(animationTime, 1f, 0f);
-
-            // Example CombinedAnimation
-//            UIViewAnimation[] appearAnimations = new UIViewAnimation[2];
-//            appearAnimations[0] = new SlideAnimation(animationTime, SlideAnimation.PosType.Left, SlideAnimation.PosType.Center, GoEaseType.QuadIn);
-//            appearAnimations[1] = new FadeAnimation(animationTime, 0f, 1f);
-//
-//            UIViewAnimation[] disappearAnimations = new UIViewAnimation[2];
-//            disappearAnimations[0] = new ScaleAnimation(animationTime, Vector3.one, Vector3.zero, GoEaseType.QuadIn);
-//            disappearAnimations[1] = new FadeAnimation(animationTime, 1f, 0f);
-//
-//            AppearAnimation = new CombinedAnimation(appearAnimations);
-//            DisappearAnimation = new CombinedAnimation(disappearAnimations);
-
-            // Example UnityAnimation
-//            AppearAnimation = new UnityLegacyAnimation(kShowAnimName);
-//
-//            UIViewAnimation[] disappearAnimations = new UIViewAnimation[2];
-//            disappearAnimations[0] = new SlideAnimation(animationTime, SlideAnimation.PosType.Center, SlideAnimation.PosType.Top, GoEaseType.QuadIn);
-//            disappearAnimations[1] = new FadeAnimation(animationTime, 1f, 0f);
-//
-//            DisappearAnimation = new CombinedAnimation(disappearAnimations);
-
-//            DisappearAnimation =  new UnityLegacyAnimation(kHideAnimName);
-        }
-
+        AnimationTime = Services.Instance.Resolve("popup_animation_time", DefaultAnimationTime);
+        AppearAnimation = new FadeAnimation(AnimationTime, 0f, 1f);
+        DisappearAnimation = new FadeAnimation(AnimationTime, 1f, 0f);
         base.OnLoad();
     }
 
