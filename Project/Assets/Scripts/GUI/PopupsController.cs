aa90741c0ac03a6f7c7a8f709762caa6529233cd
Sergi Gil <sergio.gil.calvo@socialpoint.es>
2017-10-13 09:58:18 +0200

    define default animations for popups

--- Project/Assets/Scripts/GUI/PopupsController.cs
+++ Project/Assets/Scripts/GUI/PopupsController.cs
@@ -3,13 +3,14 @@ using SocialPoint.Dependency;
 
 public class PopupsController : UIStackController
 {
-//    public const float DefaultAnimationTime = 1.0f;
-//    float AnimationTime = DefaultAnimationTime;
+    public const float DefaultAnimationTime = 1.0f;
+    float AnimationTime = DefaultAnimationTime;
 
     override protected void OnLoad()
     {
-//        AnimationTime = Services.Instance.Resolve("popup_animation_time", DefaultAnimationTime);
-//        ShowAnimation = new FadeAnimation(AnimationTime);
-//        base.OnLoad();
+        AnimationTime = Services.Instance.Resolve("popup_animation_time", DefaultAnimationTime);
+        AppearAnimation = new FadeAnimation(AnimationTime, 0f, 1f);
+        DisappearAnimation = new FadeAnimation(AnimationTime, 1f, 0f);
+        base.OnLoad();
     }
 }
