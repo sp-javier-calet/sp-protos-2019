d98a348db0d36ee9bad0ab274ded18f9e6aba64e
Sergi Herrero <sergi.herrero@socialpoint.es>
2017-10-17 18:52:01 +0200

    Fix Test not parsing Bytes ok

--- Project/Assets/Sparta/SocialPoint/Login/Editor/Tests/ConfigLoginTests.cs
+++ Project/Assets/Sparta/SocialPoint/Login/Editor/Tests/ConfigLoginTests.cs
@@ -6,60 +6,60 @@ using SocialPoint.Utils;
 using SocialPoint.Base;
 using System;
 using SocialPoint.Attributes;
+using System.Text;
 
 namespace SocialPoint.Login
 {
-	[TestFixture]
-	[Category("SocialPoint.Login")]
+    [TestFixture]
+    [Category("SocialPoint.Login")]
     public sealed class ConfigLoginTests
-	{
+    {
         ConfigLogin ConfigLogin;
-	    IHttpClient HttpClient;
+        IHttpClient HttpClient;
 
         const string kConfigUrl = "http://backend.pro.configmanager.sp.laicosp.net/products/mt/envs/env1/download";
-        const string kConfigResponse = "{\"game\":{}, \"globals\":{\"globals\":{}}}";
-	    [SetUp]
-	    public void SetUp()
-	    {
-	        HttpClient = Substitute.For<IHttpClient>();
-            ConfigLogin = new ConfigLogin(HttpClient, kConfigUrl);
-	    }
-	    
+        const string kConfigResponse = "{\"game\":{},\"globals\":{\"globals\":[{}]},\"map\":{},\"store\":{},\"payment_products\":{},\"bundle_data\":{}}";
+        const string kConfig = "config";
 
-	    [Test]
-	    public void Login_calls_HttpClient_Send()
-	    {
+        [SetUp]
+        public void SetUp()
+        {
+            HttpClient = Substitute.For<IHttpClient>();
+            ConfigLogin = new ConfigLogin(HttpClient, kConfigUrl);
+        }
+
+
+        [Test]
+        public void Login_calls_HttpClient_Send()
+        {
             HttpClient.When(x => x.Send(Arg.Any<HttpRequest>(), Arg.Any<HttpResponseDelegate>()))
                 .Do(x => {
-                    // Set hardcoded body
-                    HttpResponse resp = new HttpResponse();
-                    resp.OriginalBody =  StringUtils.GetBytes(kConfigResponse);
-                    resp.StatusCode = 200;
-                    x.Arg<HttpResponseDelegate>().Invoke(resp);
-                });
+                HttpResponse resp = new HttpResponse();
+                resp.OriginalBody = Encoding.UTF8.GetBytes(kConfigResponse);
+                resp.StatusCode = 200;
+                x.Arg<HttpResponseDelegate>().Invoke(resp);
+            });
 
-            bool eventIsCalled = false;
+            bool isParsedOk = false;
             ConfigLogin.NewUserStreamEvent += (reader) => {
-                eventIsCalled = true;
 
-                // Check game_data
-
-                // Check game_data config
-
+                var data = reader.ParseElement();
+                if(data.AsDic.ContainsKey(kConfig))
+                {
+                    isParsedOk = true;
+                }
                 return true;
             };
 
-
             ConfigLogin.Login();
 
-            Assert.IsTrue(eventIsCalled);
+            Assert.IsTrue(isParsedOk);
+        }
 
-	    }
-
-	    [TearDown]
-	    public void TearDown()
-	    {
+        [TearDown]
+        public void TearDown()
+        {
 	        
-	    }
-	}
+        }
+    }
 }
