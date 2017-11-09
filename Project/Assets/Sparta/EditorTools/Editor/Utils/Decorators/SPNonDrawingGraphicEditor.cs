eded4f00845a710dbcec8887c3c5be7fd2fb1be8
Sergi Gil <sergio.gil.calvo@socialpoint.es>
2017-11-09 17:15:45 +0100

    New component NonDrawingGraphic: A concrete subclass of the Unity UI `Graphic` class that just skips drawing. Useful for providing a raycast target without actually drawing anything. Taken directly from unity UI extensions repository

---
+++ Project/Assets/Sparta/EditorTools/Editor/Utils/Decorators/SPNonDrawingGraphicEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+using SocialPoint.GUIControl;
+
+namespace UnityEngine.UI.Extensions
+{
+    [CanEditMultipleObjects, CustomEditor(typeof(NonDrawingGraphic), false)]
+    public class SPNonDrawingGraphicEditor : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(m_Script, new GUILayoutOption[0]);
+
+            RaycastControlsGUI();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Project/Assets/Sparta/SocialPoint/GUIControl/Components/NonDrawingGraphic/NonDrawingGraphic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SocialPoint.GUIControl
+{
+    [AddComponentMenu("UI/Extensions/NonDrawingGraphic")]
+    public class NonDrawingGraphic : MaskableGraphic
+    {
+        public override void SetMaterialDirty() { return; }
+        public override void SetVerticesDirty() { return; }
+
+        /// Probably not necessary since the chain of calls `Rebuild()`->`UpdateGeometry()`->`DoMeshGeneration()`->`OnPopulateMesh()` won't happen; so here really just as a fail-safe.
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+            return;
+        }
+    }
+}
