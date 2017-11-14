51cc3d9174a980e391736f0e01123d624e9630e5
Ivan Hernández <ivan.hernandez@socialpoint.es>
2017-11-14 11:56:35 +0100

    graybox reverted to the right state. Locale installer typo.

--- Project/Assets/Sparta/Extensions/GrayboxLibrary/Editor/View/GrayboxLibraryWindow.cs
+++ Project/Assets/Sparta/Extensions/GrayboxLibrary/Editor/View/GrayboxLibraryWindow.cs
@@ -20,7 +20,7 @@ namespace SocialPoint.GrayboxLibrary
         private static string[] _categories;
         public static string Filter = "";
         public static List<string> Filters = new List<string>();
-        private static float _timeFilterUpdated = Time.realtimeSinceStartup;
+        private static float _timeFilterUpdated;
         private static bool _filterUpdated = false;
         private static bool _displayFilterOptions = false;
         private static string _currentSelectedOption = "";
@@ -42,7 +42,7 @@ namespace SocialPoint.GrayboxLibrary
 
         public static GrayboxLibraryWindow Window;
 
-        private static float _timeKeyPressed = Time.realtimeSinceStartup;
+        private static float _timeKeyPressed;
         private const float _keyDelay = 0.2f;
         private static int _focusChangeDelay = 0;
 
@@ -53,7 +53,7 @@ namespace SocialPoint.GrayboxLibrary
 
         private static bool _secondGUIDraw = false;
         private static bool _wasInactive = false;
-        private static Scene _previousScene = SceneManager.GetActiveScene();
+        private static Scene _previousScene;
 
         private static GrayboxLibraryInspectorDummy _inspectorDummyA, _inspectorDummyB;
         private static int _currentInspectorDummy = 0;
@@ -110,49 +110,49 @@ namespace SocialPoint.GrayboxLibrary
             Window.titleContent.text = "Library";
         }
 
-        [MenuItem("Social Point/Graybox Library/Buildings")]
+        [MenuItem("Sparta/Graybox Library/Buildings", false, 500)]
         public static void LaunchBuldingsClient()
         {
             _currentCategory = (int)GrayboxAssetCategory.Buildings;
             LaunchClient();
         }
 
-        [MenuItem("Social Point/Graybox Library/Props")]
+        [MenuItem("Sparta/Graybox Library/Props", false, 500)]
         public static void LaunchPropsClient()
         {
             _currentCategory = (int)GrayboxAssetCategory.Props;
             LaunchClient();
         }
 
-        [MenuItem("Social Point/Graybox Library/Decos")]
+        [MenuItem("Sparta/Graybox Library/Decos", false, 500)]
         public static void LaunchDecosClient()
         {
             _currentCategory = (int)GrayboxAssetCategory.Decos;
             LaunchClient();
         }
 
-        [MenuItem("Social Point/Graybox Library/Fx")]
+        [MenuItem("Sparta/Graybox Library/Fx", false, 500)]
         public static void LaunchFxClient()
         {
             _currentCategory = (int)GrayboxAssetCategory.Fx;
             LaunchClient();
         }
 
-        [MenuItem("Social Point/Graybox Library/Characters")]
+        [MenuItem("Sparta/Graybox Library/Characters", false, 500)]
         public static void LaunchCharactersClient()
         {
             _currentCategory = (int)GrayboxAssetCategory.Characters;
             LaunchClient();
         }
 
-        [MenuItem("Social Point/Graybox Library/Vehicles")]
+        [MenuItem("Sparta/Graybox Library/Vehicles", false, 500)]
         public static void LaunchVehiclesClient()
         {
             _currentCategory = (int)GrayboxAssetCategory.Vehicles;
             LaunchClient();
         }
 
-        [MenuItem("Social Point/Graybox Library/UI")]
+        [MenuItem("Sparta/Graybox Library/UI", false, 500)]
         public static void LaunchUIClient()
         {
             _currentCategory = (int)GrayboxAssetCategory.UI;
