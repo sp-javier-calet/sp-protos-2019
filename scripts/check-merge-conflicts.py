f97d3bfb49c1008565e91d780463ab22b8e3efa4
Miguel Ibero <miguel.ibero@socialpoint.es>
2017-11-30 17:51:02 +0100

    fixed comments

--- scripts/check-merge-conflicts.py
+++ scripts/check-merge-conflicts.py
@@ -4,57 +4,51 @@ import sys
 import git
 import os.path
 import argparse
-import fnmatch
-import glob
 
-parser = argparse.ArgumentParser(description='Detect conflict markers in repo files.')
-parser.add_argument("--src", help="git origin ref to check for changed files", default=None)
-parser.add_argument("--dst", help="git destination ref to check for changed files", default='HEAD')
-parser.add_argument("--path", help="root path of the repository", default='.')
-parser.add_argument("--pattern", help="file pattern to match", default=None)
+def get_conflicted_files(git_src, git_dst, base_path, path_pattern):
+    def get_tree_paths(tree, paths):
+        for blob in tree.blobs:
+            paths.append(blob.path)
+        for subtree in tree.trees:
+            get_tree_paths(subtree, paths)
 
-args = parser.parse_args()
-base_path = args.path
-path_pattern = args.pattern
-
-def get_tree_paths(tree, paths):
-    for blob in tree.blobs:
-        paths.append(blob.path)
-    for subtree in tree.trees:
-        get_tree_paths(subtree, paths)
-
-def fropen(path):
-    if sys.hexversion >= 0x3000000:
-        return open(path,'r', encoding="ISO-8859-1")
+    repo = git.Repo(base_path)
+    if git_src is None:
+        paths = []
+        get_tree_paths(repo.tree(git_dst), paths)
+        print('found %d files in %s' % (len(paths), args.dst))
     else:
-        return open(path,'r')
+        span = '%s..%s' % (git_src, git_dst)
+        paths = repo.git.diff(span, name_only=True, diff_filter='AMCX').split()
+        print('found %d files changed from %s to %s' % (len(paths), args.src, args.dst))
+    conflicted = []
+    for rpath in paths:
+        path = os.path.join(base_path, rpath)
+        if os.path.isfile(path):
+            if path_pattern is None or fnmatch.fnmatch(path, path_pattern):
+                with open(path, 'rb') as file:
+                    found = False
+                    for line in file:
+                        if line.startswith(b'>>>>>>>') or line.startswith(b'<<<<<<<'):
+                            found = True
+                            break;
+                    if found:
+                        conflicted.append(rpath)
+                        break
+    return conflicted
 
-repo = git.Repo(base_path)
-if args.src is None:
-    paths = []
-    get_tree_paths(repo.tree(args.dst), paths)
-    print('found %d files changed in %s' % (len(paths), args.dst))
-else:
-    span = '%s..%s' % (args.src, args.dst)
-    paths = repo.git.diff(span, name_only=True).split()
-    print('found %d files changed from %s to %s' % (len(paths), args.src, args.dst))
-conflicted = []
-for rpath in paths:
-    path = os.path.join(base_path, rpath)
-    if os.path.isfile(path):
-        if path_pattern is None or fnmatch.fnmatch(path, path_pattern):
-            with fropen(path) as file:
-                found = False
-                for line in file:
-                    if line.startswith('>>>>>>>') or line.startswith('<<<<<<<'):
-                        found = True
-                        break;
-                if found:
-                    conflicted.append(rpath)
-                    break
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Detect conflict markers in repo files.')
+    parser.add_argument("--src", help="git origin ref to check for changed files", default=None)
+    parser.add_argument("--dst", help="git destination ref to check for changed files", default='HEAD')
+    parser.add_argument("--path", help="root path of the repository", default='.')
+    parser.add_argument("--pattern", help="file pattern to match", default=None)
 
-if len(conflicted) > 0:
-    print("You have merge markers in the following files:")
-    for path in conflicted:
-        print(path)
-    exit(1)
+    args = parser.parse_args()
+    conflicted = get_conflicted_files(args.src, args.dst, args.path, args.pattern)
+
+    if len(conflicted) > 0:
+        print("You have merge markers in the following files:")
+        for path in conflicted:
+            print(path)
+        exit(1)
