be328979046dafec5f055f54d05a27a69de9e3e7
Fernando Serra <fernando.serra@socialpoint.es>
2017-12-19 18:07:23 +0100

    * Initial implementation of ItemRequest and empty DonationsManager

---
+++ Project/Assets/Plugins/Sparta/Extensions/Social/SocialFramework/Donations/Editor/Tests/DonationItemRequestTests.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+using System;
+using SocialPoint.Attributes;
+
+namespace SocialPoint.Social
+{
+    public class DonationItemRequestTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        static ItemRequest CreateRequest(int amount)
+        {
+            return new ItemRequest(123, "fake_uuid", 456, amount, "fake_type", TimeSpan.Zero, null);
+        }
+
+        [Test]
+        public void InitialState()
+        {
+            const int requesterId = 123;
+            const string uuid = "fake_uuid";
+            const int itemId = 456;
+            const int amount = 789;
+            const string type = "fake_type";
+            var ts = TimeSpan.FromSeconds(987);
+            var metadata = new AttrDic();
+
+            var request = new ItemRequest(requesterId, uuid, itemId, amount, type, ts, metadata);
+
+            Assert.AreEqual(requesterId, request.RequesterId);
+            Assert.AreEqual(uuid, request.RequestUuid);
+            Assert.AreEqual(itemId, request.ItemId);
+            Assert.AreEqual(amount, request.Amount);
+            Assert.AreEqual(type, request.DonationType);
+            Assert.AreEqual(ts, request.Timestamp);
+            Assert.AreEqual(metadata, request.Metadata);
+
+            Assert.AreEqual(0, request.TotalReceivedAmount);
+            Assert.AreEqual(0, request.TotalCollectedAmount);
+        }
+
+        [Test]
+        public void AddContribution()
+        {
+            const int contribution = 2;
+            const int userId = 123;
+
+            var request = CreateRequest(5);
+
+            Assert.AreEqual(0, request.TotalReceivedAmount);
+
+            request.AddContribution(userId, contribution);
+
+            Assert.AreEqual(contribution, request.TotalReceivedAmount);
+        }
+
+        [Test]
+        public void SetCollected()
+        {
+            const int collected = 1;
+            const int userId = 123;
+
+            var request = CreateRequest(3);
+
+            Assert.AreEqual(0, request.TotalCollectedAmount);
+
+            request.AddContribution(userId, collected);
+            request.SetCollected(userId, collected);
+
+            Assert.AreEqual(collected, request.TotalCollectedAmount);
+        }
+
+        [Test]
+        public void CollectSimple()
+        {
+            const int contributed = 1;
+            const int userId = 123;
+
+            var request = CreateRequest(3);
+            request.AddContribution(userId, contributed);
+
+            Assert.AreEqual(0, request.TotalCollectedAmount);
+
+            request.CollectContribution(userId);
+
+            Assert.AreEqual(contributed, request.TotalCollectedAmount);
+        }
+
+        [Test]
+        public void CollectWithInitialCollected()
+        {
+            const int contributed = 5;
+            const int collectedInitial = 3;
+            const int userId = 123;
+
+            var request = CreateRequest(10);
+            request.AddContribution(userId, contributed);
+            request.SetCollected(userId, collectedInitial);
+
+            Assert.AreEqual(contributed, request.TotalReceivedAmount);
+            Assert.AreEqual(collectedInitial, request.TotalCollectedAmount);
+
+            request.CollectContribution(userId);
+
+            Assert.AreEqual(contributed, request.TotalCollectedAmount);
+        }
+    }
+}
---
+++ Project/Assets/Plugins/Sparta/Extensions/Social/SocialFramework/Donations/ItemRequest.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using SocialPoint.Attributes;
+
+namespace SocialPoint.Social
+{
+    public sealed class ItemRequest
+    {
+
+        readonly long _requesterId;
+
+        public long RequesterId
+        {
+            get
+            {
+                return _requesterId;
+            }
+        }
+
+        readonly string _requestUuid;
+
+        public string RequestUuid
+        {
+            get
+            {
+                return _requestUuid;
+            }
+        }
+
+        readonly int _itemId;
+
+        public int ItemId
+        {
+            get
+            {
+                return _itemId;
+            }
+        }
+
+        readonly int _amount;
+
+        public int Amount
+        {
+            get
+            {
+                return _amount; 
+            }
+        }
+
+        public int TotalReceivedAmount { get; private set; }
+
+        public int TotalCollectedAmount{ get; private set; }
+
+        readonly string _donationType;
+
+        public string DonationType
+        {
+            get
+            {
+                return _donationType;
+            }
+        }
+
+        System.TimeSpan _timestamp;
+
+        public System.TimeSpan Timestamp
+        {
+            get
+            {
+                return _timestamp;
+            }
+        }
+
+        readonly AttrDic _metadata;
+
+        public AttrDic Metadata
+        {
+            get
+            {
+                return _metadata;
+            }
+        }
+
+        readonly Dictionary<long, int> _receivedMap;
+
+        public IEnumerator<KeyValuePair<long, int>> ReceivedMapEnumerator
+        {
+            get
+            {
+                return _receivedMap.GetEnumerator();
+            }
+        }
+
+        readonly Dictionary<long, int> _collectedMap;
+
+        public IEnumerator<KeyValuePair<long, int>> CollectedMapEnumerator
+        {
+            get
+            {
+                return _collectedMap.GetEnumerator();
+            }
+        }
+
+        public ItemRequest(long requesterId, string requestUuid, int itemId, int amount, string donationType, System.TimeSpan timestamp, AttrDic metadata)
+        {
+            _requesterId = requesterId;
+            _requestUuid = requestUuid;
+            _itemId = itemId;
+            _amount = amount;
+            _donationType = donationType;
+            _timestamp = timestamp;
+            _metadata = metadata;
+
+            TotalReceivedAmount = 0;
+            TotalCollectedAmount = 0;
+
+            _receivedMap = new Dictionary<long, int>();
+            _collectedMap = new Dictionary<long, int>();
+        }
+
+        public void SetCollected(long contributorId, int amount)
+        {
+            if(_collectedMap.ContainsKey(contributorId))
+            {
+                _collectedMap[contributorId] = amount;
+            }
+            else
+            {
+                _collectedMap.Add(contributorId, amount);
+            }
+            TotalCollectedAmount += amount;
+        }
+
+        public void AddContribution(long contributorId, int amount)
+        {
+            if(_receivedMap.ContainsKey(contributorId))
+            {
+                _receivedMap[contributorId] += amount;
+            }
+            else
+            {
+                _receivedMap.Add(contributorId, amount);
+            }
+            TotalReceivedAmount += amount;
+        }
+
+        public void CollectContribution(long contributorId)
+        {
+            int received;
+            int collected;
+            _receivedMap.TryGetValue(contributorId, out received);
+            _collectedMap.TryGetValue(contributorId, out collected);
+
+            int diff = received - collected;
+
+            if(_receivedMap.ContainsKey(contributorId))
+            {
+                _receivedMap[contributorId] += received;
+            }
+            else
+            {
+                _receivedMap.Add(contributorId, received);
+            }
+            TotalCollectedAmount += diff;
+        }
+
+        public int GetContributedBy(long contributorId)
+        {
+            int amount;
+            _receivedMap.TryGetValue(contributorId, out amount);
+            return amount;
+        }
+
+        public int GetCollectedBy(long collectorId)
+        {
+            int amount;
+            _collectedMap.TryGetValue(collectorId, out amount);
+            return amount;
+        }
+    }
+}
