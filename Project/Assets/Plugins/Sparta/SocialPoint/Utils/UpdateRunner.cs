e2dff464ca137f8f2832466d130ee1f14de54748
Miguel Ibero <miguel.ibero@socialpoint.es>
2017-12-20 16:35:23 +0100

    fixes for server to compile

--- Project/Assets/Plugins/Sparta/SocialPoint/Utils/UpdateRunner.cs
+++ Project/Assets/Plugins/Sparta/SocialPoint/Utils/UpdateRunner.cs
@@ -377,7 +377,7 @@ namespace SocialPoint.Utils
             UpdateHandlers();
 
             // Check new exceptions
-            AggregateException.Trigger(_exceptions);
+            SocialPoint.Utils.AggregateException.Trigger(_exceptions);
 
             // Sync for internal changes during update
             Synchronize();
