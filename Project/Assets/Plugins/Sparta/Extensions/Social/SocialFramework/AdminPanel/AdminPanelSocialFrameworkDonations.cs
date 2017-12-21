9d3644d08717be95a235f6229ff0e76d689fea1b
Fernando Serra <fernando.serra@socialpoint.es>
2017-12-21 13:50:54 +0100

    Revert "* Removed empty AdminPanel for doantions to be added to another PR"
    This reverts commit 7f1d3ff2593fa53c33a9ef4df4a3538010e9ae64.

---
+++ Project/Assets/Plugins/Sparta/Extensions/Social/SocialFramework/AdminPanel/AdminPanelSocialFrameworkDonations.cs
@@ -0,0 +1,15 @@
+﻿#if ADMIN_PANEL 
+
+using SocialPoint.AdminPanel;
+
+namespace SocialPoint.Social
+{
+    public class AdminPanelSocialFrameworkDonations : IAdminPanelGUI
+    {
+        public void OnCreateGUI(AdminPanelLayout layout)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+#endif
