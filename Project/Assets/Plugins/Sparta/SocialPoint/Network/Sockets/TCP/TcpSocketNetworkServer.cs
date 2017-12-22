5ee67850e2c2f022a5b483fe54ea4f182ff5532e
Sergi Herrero <sergi.herrero@socialpoint.es>
2017-12-22 15:23:28 +0100

    fix comments

--- Project/Assets/Plugins/Sparta/SocialPoint/Network/Sockets/TCP/TcpSocketNetworkServer.cs
+++ Project/Assets/Plugins/Sparta/SocialPoint/Network/Sockets/TCP/TcpSocketNetworkServer.cs
@@ -18,7 +18,6 @@ namespace SocialPoint.Network
             public TcpSocketMessageReader Reader;
         }
 
-
         public const string DefaultAddress = "127.0.0.1";
         public const int DefaultPort = 8888;
 
