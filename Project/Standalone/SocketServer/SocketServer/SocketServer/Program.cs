22d1ca1ee3abc8274ded3cacd59a59993c83c43d
Sergi Herrero <sergi.herrero@socialpoint.es>
2018-02-02 12:33:14 +0100

    Added Server Ping Pong standalone project

---
+++ Project/Standalone/SocketServer/SocketServer/SocketServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using SocialPoint.Utils;
+
+namespace SocialPoint.Sockets
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            UpdateScheduler updateScheduler = new UpdateScheduler();
+            if(args.Length == 0 || args.Length == 1)
+            {
+                Console.WriteLine("Please enter (TCP or UDP) and the port: ex -> TCP 8888");
+                return;
+            }
+            if(args[0] == "TCP" || args[0] == "UDP")
+            {
+                int port;
+                bool portOK = int.TryParse(args[1], out port);
+                if(portOK)
+                {
+                    SocketServer server;
+                    if(args[0] == "TCP")
+                    {
+                        Console.WriteLine("INSTANTIATE TCP SERVER");
+                        server = new SocketServer(SocketServer.Protocol.TCP, port, updateScheduler);
+                        server.Start();
+                    }
+                    else if(args[0] == "UDP")
+                    {
+                        Console.WriteLine("INSTANTIATE UDP SERVER");
+                        server = new SocketServer(SocketServer.Protocol.UDP, port, updateScheduler);
+                        server.Start();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please add a correct port: ex -> 8888");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter (TCP or UDP): ex -> TCP");
+            }
+
+            while(true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    return;
+                }
+
+                updateScheduler.Update(1.0f, 1.0f);
+            }
+        }
+    }
+}
---
+++ Project/Standalone/SocketServer/SocketServer/SocketServer/SocketServer.cs
@@ -0,0 +1,47 @@
+﻿#define SPARTA_LOG_VERBOSE
+
+using SocialPoint.Base;
+using SocialPoint.Network;
+using SocialPoint.Utils;
+
+namespace SocialPoint.Sockets
+{
+    public class SocketServer
+    {
+        public enum Protocol
+        {
+            TCP,
+            UDP
+        }
+
+        const string ServerAddress = "127.0.0.1";
+        const int PeerLimit = 100;
+        const string ConnectionKey = "TestConnectionKey";
+        const int UpdateTime = 10;
+
+        INetworkServer _netServer;
+        NetworkMatchDelegateFactory _matchDelegateFactory;
+        MultiMatchController _multiMatch;
+
+        public SocketServer(Protocol protocol, int port, IUpdateScheduler updateScheduler)
+        {
+            switch(protocol)
+            {
+            case Protocol.TCP:
+                _netServer = new TcpSocketNetworkServer(updateScheduler, ServerAddress, port);
+                break;
+            case Protocol.UDP:
+                _netServer = new UdpSocketNetworkServer(updateScheduler, PeerLimit, ConnectionKey, UpdateTime);
+                (_netServer as UdpSocketNetworkServer).Port = port;
+                break;
+            }
+            _matchDelegateFactory = new NetworkMatchDelegateFactory();
+            _multiMatch = new MultiMatchController(_netServer, _matchDelegateFactory, TypeMessages.ConnectMessageType);
+        }
+
+        public void Start()
+        {
+            _netServer.Start();
+        }
+    }
+}
