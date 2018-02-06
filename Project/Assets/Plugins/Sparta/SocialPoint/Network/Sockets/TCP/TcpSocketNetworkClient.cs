ff59af7296624251cd0cb3c65c419ad030cc5429
Sergi Herrero <sergi.herrero@socialpoint.es>
2018-02-06 12:33:29 +0100

    fixes

--- Project/Assets/Plugins/Sparta/SocialPoint/Network/Sockets/TCP/TcpSocketNetworkClient.cs
+++ Project/Assets/Plugins/Sparta/SocialPoint/Network/Sockets/TCP/TcpSocketNetworkClient.cs
@@ -11,19 +11,19 @@ namespace SocialPoint.Network
     {
         List<INetworkClientDelegate> _delegates = new List<INetworkClientDelegate>();
         INetworkMessageReceiver _receiver;
-        string _serverAddr;
-        int _serverPort;
         TcpClient _client;
         bool _connecting;
         bool _connected;
         IUpdateScheduler _scheduler;
         List<NetworkStream> _stream;
         TcpSocketMessageReader _socketMessageReader;
+        string _serverAddress;
+        int _serverPort;
 
         public TcpSocketNetworkClient(IUpdateScheduler scheduler, string serverAddr = TcpSocketNetworkServer.DefaultAddress, int serverPort = TcpSocketNetworkServer.DefaultPort)
         {
             _scheduler = scheduler;
-            _serverAddr = serverAddr;
+            _serverAddress = serverAddr;
             _serverPort = serverPort;
             _client = new TcpClient();
         }
@@ -32,7 +32,7 @@ namespace SocialPoint.Network
         {
             _connecting = true;
             _scheduler.Add(this);
-            _client.Connect(_serverAddr, _serverPort);
+            _client.Connect(_serverAddress, _serverPort);
             _stream = new List<NetworkStream>();
             _stream.Add(_client.GetStream());
         }
@@ -101,11 +101,11 @@ namespace SocialPoint.Network
 
         void ConnectClient()
         {
-            if(_connecting && _client.Connected)
+            if (_connecting && _client.Connected)
             {
                 _connecting = false;
                 _connected = true;
-                for(var i = 0; i < _delegates.Count; i++)
+                for (var i = 0; i < _delegates.Count; i++)
                 {
                     _delegates[i].OnClientConnected();
                 }
@@ -117,11 +117,11 @@ namespace SocialPoint.Network
 
         void OnServerMessageReceived(NetworkMessageData data, IReader reader)
         {
-            for(var i = 0; i < _delegates.Count; i++)
+            for (var i = 0; i < _delegates.Count; i++)
             {
                 _delegates[i].OnMessageReceived(data);
             }
-            if(_receiver != null)
+            if (_receiver != null)
             {
                 _receiver.OnMessageReceived(data, reader);
             }
@@ -129,7 +129,7 @@ namespace SocialPoint.Network
 
         void DisconnectClient()
         {
-            if(Connected && IsSocketDisconnected())
+            if (Connected && IsSocketDisconnected())
             {
                 OnDisconnected();
             }
@@ -137,10 +137,10 @@ namespace SocialPoint.Network
 
         bool IsSocketDisconnected()
         {
-            if(_client.Client.Poll(0, SelectMode.SelectRead))
+            if (_client.Client.Poll(0, SelectMode.SelectRead))
             {
                 byte[] buff = new byte[1];
-                if(_client.Client.Receive(buff, SocketFlags.Peek) == 0)
+                if (_client.Client.Receive(buff, SocketFlags.Peek) == 0)
                 {
                     return true;
                 }
@@ -150,7 +150,7 @@ namespace SocialPoint.Network
 
         void ReceiveServerMessages()
         {
-            while(_client.Connected && _client.Available > 0)
+            while (_client.Connected && _client.Available > 0)
             {
                 _socketMessageReader.Receive();
             }
@@ -159,7 +159,7 @@ namespace SocialPoint.Network
         void OnDisconnected()
         {
             _connected = false;
-            for(var i = 0; i < _delegates.Count; i++)
+            for (var i = 0; i < _delegates.Count; i++)
             {
                 _delegates[i].OnClientDisconnected();
             }
