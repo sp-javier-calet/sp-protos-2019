d5989c21eadcc10bcc83c382c084f1cb5b87b874
ihernandez-windows <ivan.hernandez@socialpoint.es>
2018-02-16 15:39:00 +0100

    Added Standalone lockstep game logic to base game

---
+++ Project/Standalone/Photon/LockstepGameLogic/upload.ps1
@@ -0,0 +1,23 @@
+
+Import-Module ..\Server\sas-client\Photon.PrivateCloud.Plugin.Client.SAS.psm1
+
+$vsPath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE"
+$basePath = "."
+$solutionName = "LockstepGameLogic"
+$solutionPath = "$basePath\$solutionName.sln"
+$buildPath = "$basePath\bin"
+$customer = "Socialpoint"
+$key = "66QFur7XJcyRsCXxuk7yrtwDHw8Xsne9"
+$plugin = "LockstepPlugin"
+
+Write-Host "building server solution..."
+$p = Start-Process -FilePath $vsPath\devenv.exe -ArgumentList $solutionPath,"/Rebuild Release" -PassThru
+$null = $p.WaitForExit(-1)
+Write-Host ""
+Write-Host "server solution ready!"
+Write-Host "creating plugin package..."
+Compress-Archive -Path $buildPath -DestinationPath archive.zip -Force
+Write-Host "package created!"
+Write-Host "uploading package..."
+Add-PhotonPlugin -Customer $customer -Plugin $plugin -File .\archive.zip -Key $key
+Write-Host "package uploaded!"
