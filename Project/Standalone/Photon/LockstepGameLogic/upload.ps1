0287c84bb343ab956e57241b0bd7950322687dfe
ihernandez-windows <ivan.hernandez@socialpoint.es>
2018-02-19 11:51:11 +0100

    Updated some project files and scripts with new paths

--- Project/Standalone/Photon/LockstepGameLogic/upload.ps1
+++ Project/Standalone/Photon/LockstepGameLogic/upload.ps1
@@ -1,7 +1,6 @@
+Import-Module ..\..\..\Assets\Plugins\Sparta\Hidden~\Standalone\Photon\Server\sas-client\Photon.PrivateCloud.Plugin.Client.SAS.psm1
 
-Import-Module ..\Server\sas-client\Photon.PrivateCloud.Plugin.Client.SAS.psm1
-
-$vsPath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE"
+$msbuildPath = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\"
 $basePath = "."
 $solutionName = "LockstepGameLogic"
 $solutionPath = "$basePath\$solutionName.sln"
@@ -11,12 +10,14 @@ $key = "66QFur7XJcyRsCXxuk7yrtwDHw8Xsne9"
 $plugin = "LockstepPlugin"
 
 Write-Host "building server solution..."
-$p = Start-Process -FilePath $vsPath\devenv.exe -ArgumentList $solutionPath,"/Rebuild Release" -PassThru
+$p = Start-Process -FilePath $msbuildPath\MSBuild.exe -ArgumentList $solutionPath,"/t:Clean /p:Configuration=Release" -PassThru -NoNewWindow
+$null = $p.WaitForExit(-1)
+$p = Start-Process -FilePath $msbuildPath\MSBuild.exe -ArgumentList $solutionPath,"/t:Rebuild /p:Configuration=Release" -PassThru -NoNewWindow
 $null = $p.WaitForExit(-1)
 Write-Host ""
 Write-Host "server solution ready!"
 Write-Host "creating plugin package..."
-Compress-Archive -Path $buildPath -DestinationPath archive.zip -Force
+..\..\..\Assets\Plugins\Sparta\Hidden~\Standalone\Photon\Server\sas-client\7z.exe a -tzip archive.zip "$buildPath" -xr!"*.meta"
 Write-Host "package created!"
 Write-Host "uploading package..."
 Add-PhotonPlugin -Customer $customer -Plugin $plugin -File .\archive.zip -Key $key
