3bf4730676a20ed45dab9109b5fada614de0c687
Ivan Hernández <ivan.hernandez@socialpoint.es>
2018-06-14 16:22:56 +0200

    Removed deprecated folders and files (some were used with ANT, before we were all in GRADLE). Removed bin and gen folders and some other files.

--- Project/Assets/Plugins/Android/main/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/bin/AndroidManifest.xml : \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/AndroidManifest.xml \
--- Project/Assets/Plugins/Android/main/gen/R.java.d
+++
@@ -1,8 +0,0 @@
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/gen/es/socialpoint/main/R.java \
- : /Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/res/drawable-hdpi/notify_icon_small.png \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/res/drawable-ldpi/notify_icon_small.png \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/res/drawable-mdpi/notify_icon_small.png \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/res/drawable-xhdpi/notify_icon_small.png \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/res/drawable-xxhdpi/notify_icon_small.png \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/res/drawable-xxxhdpi/notify_icon_small.png \
-/Users/mibero/Projects/sp-unity-BaseGame/Project/Assets/Plugins/Android/main/bin/AndroidManifest.xml \
