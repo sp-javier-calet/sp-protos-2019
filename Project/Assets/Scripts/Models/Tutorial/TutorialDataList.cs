42e8159b260d475e20594d7a2d6704422e69a54d
Ivan Hernandez <ivan.hernandez@socialpoint.es>
2019-01-14 12:38:46 +0100

    - BaseLoadingScene renamed to LoadingScene and removed the previous LoadingScene. - BaseTutorialScene renamed to LoadingScene and removed the previous TutorialScene. - TutorialDataList moved to Sparta. - GUI_TutorialSelector does not need to be in a Resources folder because is already referenced in the GUI_TutorialSelectorInstaller.asset. - TutorialAsset redone using Odin serialisation.

--- Project/Assets/Scripts/Models/Tutorial/TutorialDataList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using SocialPoint.Tutorial;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Tutorials", menuName = "Sparta/Tutorials")]
-public class TutorialDataList : ScriptableObject
-{
-    public List<TutorialData> Tutorials;
-}
