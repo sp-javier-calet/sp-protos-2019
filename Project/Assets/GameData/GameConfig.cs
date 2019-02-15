eddef9337c420bd4cd44501f40ab0b41fbe13aac
Ivan Hernandez <ivan.hernandez@socialpoint.es>
2019-02-15 13:10:58 +0100

    - Removed old External art folder. // - Added new Art folder structure. // - Removed old config with attr parsers. // - Added new ScriptableObjects config setup. // - Enabled again some services installers.

---
+++ Project/Assets/GameData/GameConfig.cs
@@ -0,0 +1,10 @@
+using SocialPoint.GameData;
+using UnityEngine;
+
+namespace GameLoading
+{
+    [CreateAssetMenu(menuName = "Sparta/Game/Config")]
+    public class GameConfig : BaseGameConfig
+    {
+    }
+}
