fe4031b32fb2fbab228f1b9e76d497f3c7fab5a7
sp-ferran-mestres <ferran.mestres@socialpoint.es>
2019-03-21 16:53:21 +0100

    Reset LFS hooks Concatenate scripts when needed

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -5,6 +5,29 @@ fi
 
 mkdir -p .git/hooks
 
+# lfs sets pre-push, post-commit, post-checkout and post-merge scripts
+# This resets these scripts
+
+# git lfs update [--manual | --force]
+
+# Updates the Git hooks used by Git LFS. Silently upgrades known hook contents.
+# If you have your own custom hooks you may need to use one of the extended
+# options below.
+
+# Options:
+
+# * --manual -m
+#     Print instructions for manually updating your hooks to include git-lfs
+#     functionality. Use this option if git lfs update fails because of existing
+#     hooks and you want to retain their functionality.
+
+# * --force -f
+#     Forcibly overwrite any existing hooks with git-lfs hooks. Use this option
+#     if git lfs update fails because of existing hooks but you don't care
+#     about their current contents.
+git lfs update --force 
+
+
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/scripts/hooks" && pwd )"
 GIT_HOOKS_DIR=$(cd ".git/hooks" && pwd)
 
@@ -14,7 +37,12 @@ echo "GIT_HOOKS_DIR: " $GIT_HOOKS_DIR
 SCRIPTS=(pre-commit post-checkout post-merge)
 
 for SCRIPT in "${SCRIPTS[@]}"; do
-    ln -snf "$SCRIPTS_DIR/$SCRIPT" "$GIT_HOOKS_DIR/$SCRIPT"
+	echo "SCRIPT: " $SCRIPT
+	if [ "$SCRIPT" == "pre-push" ] || [ "$SCRIPT" == "post-commit" ] || [ "$SCRIPT" == "post-checkout" ] || [ "$SCRIPT" == "post-merge" ]; then
+		cat "$SCRIPTS_DIR/$SCRIPT" >> "$GIT_HOOKS_DIR/$SCRIPT"
+	else
+    	ln -snf "$SCRIPTS_DIR/$SCRIPT" "$GIT_HOOKS_DIR/$SCRIPT"
+	fi
 done
 
 echo "Installed ${SCRIPTS[*]}"
