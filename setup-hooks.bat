233e98e81c3273922d26eb00a45364e04a96294b
sp-ferran-mestres <ferran.mestres@socialpoint.es>
2019-04-25 15:58:40 +0200

    Updated setup-hooks scripts to reflecta changes in Sparta. Removed leftover files. Updated Sparta submodule.

--- setup-hooks.bat
+++ setup-hooks.bat
@@ -5,9 +5,4 @@ echo off
 :: This is a shortcut to the setup-hooks script in Sparta. Check there for more info.
 ::---------------------------------------------------------
 
-pushd %~dp0
-set root_dir=%cd%
-
-set SPARTA_DIR=%root_dir%\Project\Assets\Plugins\Sparta
-
-call %SPARTA_DIR%\setup-hooks.bat
+call %~dp0\Project\Assets\Plugins\Sparta\Hidden~\setup-hooks.bat
--- setup-hooks.sh
+++ setup-hooks.sh
@@ -2,6 +2,6 @@
 # This is a shortcut to the setup-hooks script in Sparta. Check there for more info.
 #---------------------------------------------------------
 
-SPARTA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/Project/Assets/Plugins/Sparta/" && pwd )"
+SPARTA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/Project/Assets/Plugins/Sparta/Hidden~/" && pwd )"
 
 /bin/bash $SPARTA_DIR/setup-hooks.sh
