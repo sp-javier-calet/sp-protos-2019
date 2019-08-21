4787b3626b4813c9af02de229586f41eada7a03f
Javier Calet <caletbak@gmail.com>
2019-08-21 16:27:29 +0200

    wip

--- Project/Assets/LikeChildrenInThePark/Scripts/Characters/CP_PlayerController.cs
+++ Project/Assets/LikeChildrenInThePark/Scripts/Characters/CP_PlayerController.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class CP_PlayerController : MonoBehaviour
 {
+    enum PlayerState
+    {
+        E_NONE,
+        E_WALKING,
+        E_JUMPING,
+        E_JUMPING_FALL,
+        E_STOPPED
+    }
+
     const int kHoldingJumpMaxMillis = 150;
-    const float kJumpForce = 10.0f;
+    const float kMoveForce = 0.05f;
+    const float kMoveJumpingForce = 0.03f;
+    const float kJumpForce = 11.0f;
+    const float kFallingThreshold = 1f;
+    const float kMaxFallingThreshold = 20f;
 
     Vector3 _vectTemp = new Vector3();
 
     bool _holding = false;
     long _holdingStartTime = 0;
-    bool _jumping = false;
-    bool _jumpingGoingDown = false;
+    PlayerState _playerState = PlayerState.E_NONE;
 
     Rigidbody _rigidBody = null;
     Animation _animator = null;
     Camera _gameCamera = null;
+    CP_SceneManager _sceneManager = null;
+    RaycastHit _hit;
+    float _initialDistance = 0f;
 
     void Awake()
     {
@@ -30,7 +45,35 @@ public class CP_PlayerController : MonoBehaviour
 
         _gameCamera = GameObject.Find("GameCamera").GetComponent<Camera>();
 
+        transform.position = new Vector3(CP_SceneManager.kScenePieceSize * 1.5f, 1.0f, -1.2f);
+
+        var dist = 0f;
+        GetHitDistance(out dist);
+        _initialDistance = dist;
+
         Walk();
+    }
+
+    bool GetHitDistance(out float distance)
+    {
+        distance = 0f;
+
+        int layerMask = 1 << 9;
+        layerMask = ~layerMask;
+
+        Ray downRay = new Ray(transform.position, -Vector3.up);
+        if (Physics.Raycast(downRay, out _hit, 0.0001f, layerMask))
+        {
+            distance = _hit.distance;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void SetSceneManager(CP_SceneManager sceneManager)
+    {
+        _sceneManager = sceneManager;
     }
 
     void Walk()
@@ -39,6 +82,8 @@ public class CP_PlayerController : MonoBehaviour
         {
             _animator.Play("walk");
         }
+
+        _playerState = PlayerState.E_WALKING;
     }
 
     void Jump()
@@ -53,7 +98,7 @@ public class CP_PlayerController : MonoBehaviour
                 _animator["jump"].time = 0.06f;
             }
 
-            _jumping = true;
+            _playerState = PlayerState.E_JUMPING;
         }
     }
 
@@ -63,10 +108,29 @@ public class CP_PlayerController : MonoBehaviour
         {
             _animator.Play("idle");
         }
+
+        _playerState = PlayerState.E_STOPPED;
     }
 
-    void Update()
+    void MoveForward()
     {
+        if (_playerState == PlayerState.E_WALKING)
+        {
+            transform.position += (Vector3.right * kMoveForce);
+        }
+        else
+        {
+            transform.position += (Vector3.right * kMoveJumpingForce);  
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (_sceneManager != null)
+        {
+            _sceneManager.CheckMapGeneration();
+        }
+
         if(!_holding)
         {
             if(Input.GetMouseButtonDown(0))
@@ -82,11 +146,17 @@ public class CP_PlayerController : MonoBehaviour
 
             if (TimeUtils.TimestampMilliseconds <= _holdingStartTime + kHoldingJumpMaxMillis)
             {
-                Jump();
+                if (_playerState == PlayerState.E_WALKING)
+                {
+                    Jump();
+                } 
             }
             else
             {
-                Walk();
+                if (_playerState == PlayerState.E_STOPPED)
+                {
+                    Walk();
+                }
             }
         }
         else
@@ -95,33 +165,40 @@ public class CP_PlayerController : MonoBehaviour
             {
                 if(TimeUtils.TimestampMilliseconds > _holdingStartTime + kHoldingJumpMaxMillis)
                 {
-                    Stop();
+                    if (_playerState == PlayerState.E_WALKING)
+                    {
+                        Stop();
+                    }
                 }
             }
         }
 
-        if(_jumping)
+        if(_playerState == PlayerState.E_JUMPING)
         {
-            if(!_jumpingGoingDown)
+            if(_rigidBody != null)
             {
-                if(_rigidBody != null)
+                if(_rigidBody.velocity.y < 0.0f)
                 {
-                    if(_rigidBody.velocity.y < 0.0f)
-                    {
-                        _jumpingGoingDown = true;
-                    }
+                    _playerState = PlayerState.E_JUMPING_FALL;
                 }
             }
+        }
 
-            if(_jumpingGoingDown)
+        if (_playerState == PlayerState.E_WALKING ||
+            _playerState == PlayerState.E_JUMPING ||
+            _playerState == PlayerState.E_JUMPING_FALL)
+        {
+            MoveForward();
+        }
+
+        var dist = 0f;
+        if (GetHitDistance(out dist))
+        {
+            Debug.Log(_hit.collider.name);
+
+            if(_playerState == PlayerState.E_JUMPING_FALL)
             {
-                if(_rigidBody.velocity.y == 0.0f)
-                {
-                    _jumping = false;
-                    _jumpingGoingDown = false;
-
-                    Walk();
-                }
+                Walk();
             }
         }
 
--- Project/Assets/LikeChildrenInThePark/Scripts/Scene/CP_SceneManager.cs
+++ Project/Assets/LikeChildrenInThePark/Scripts/Scene/CP_SceneManager.cs
@@ -5,39 +5,152 @@ using UnityEngine;
 
 public class CP_SceneManager : MonoBehaviour
 {
+    const int kMaxSceneSize = 256;
+    public const int kScenePieceSize = 16;
+
     public GameObject SceneMainPiece = null;
     public List<GameObject> SceneBackgrounds = null;
-    public int ScenePiecesLength = 0;
+    public int ScenePiecesLength = -1;
+    public GameObject PlayerGO = null;
 
     GameObject _sceneObjectBase = null;
+    List<int> _sceneBackgrounds = new List<int>();
+    List<GameObject> _sceneBackgroundsGO = new List<GameObject>();
+    CP_PlayerController _player = null;
+
+    int _sceneMapLastGeneratedIndex = -1;
 
     void Awake()
     {
+        Application.targetFrameRate = 60;
+        Time.fixedDeltaTime =  Time.timeScale * 0.02f;
+
         _sceneObjectBase = new GameObject("SceneBase");
         _sceneObjectBase.transform.SetParent(transform, true);
 
-        GenerateMap();
+        if (ScenePiecesLength == -1 || ScenePiecesLength > kMaxSceneSize)
+        {
+            ScenePiecesLength = kMaxSceneSize;
+        }
+
+        GenerateMapData(ScenePiecesLength);
+
+        for (var i = 0; i < 3; ++i)
+        {
+            GenerateMap(i, i, true);
+        }
+        _sceneMapLastGeneratedIndex = 1;
+
+        GeneratePlayer();
     }
 
-    void GenerateMap()
+    void GenerateMapData(int length)
     {
-        for(var i = 0; i < ScenePiecesLength; ++i)
-        {
-            if(SceneMainPiece != null)
-            {
-                GameObject newSceneMainPiece = Instantiate(SceneMainPiece);
-                newSceneMainPiece.transform.SetParent(_sceneObjectBase.transform, true);
+        _sceneBackgrounds.Clear();
 
-                newSceneMainPiece.transform.position = new Vector3(i * 16.0f, 0.0f, 0.0f);
+        List<int> previousRandoms = new List<int>();
+
+        for(var i = 0; i < length; ++i)
+        {
+            var randomBackground = -1;
+            while(randomBackground == -1 || previousRandoms.Contains(randomBackground))
+            {
+                randomBackground = RandomUtils.Range(0, SceneBackgrounds.Count);
             }
 
-            var randomBackground = RandomUtils.Range(0, SceneBackgrounds.Count);
-            if(SceneBackgrounds[randomBackground] != null)
+            if (previousRandoms.Count == 2)
             {
-                GameObject newSceneBackground = Instantiate(SceneBackgrounds[randomBackground]);
-                newSceneBackground.transform.SetParent(_sceneObjectBase.transform, true);
+                previousRandoms.RemoveAt(0);
+            }
 
-                newSceneBackground.transform.position = new Vector3(i * 16.0f, 0.0f, 0.0f);
+            previousRandoms.Add(randomBackground);
+
+            _sceneBackgrounds.Add(randomBackground);
+        }
+    }
+
+    void GenerateMap(int mapPos, int mapIndex, bool goingRight)
+    {
+        Debug.Log("GenerateMap " + mapPos + " " + mapIndex + " " + goingRight);
+
+        if(SceneMainPiece != null)
+        {
+            GameObject newSceneMainPiece = Instantiate(SceneMainPiece);
+            newSceneMainPiece.transform.SetParent(_sceneObjectBase.transform, true);
+
+            newSceneMainPiece.transform.position = new Vector3((mapPos * kScenePieceSize) + (kScenePieceSize * 0.5f), 0.0f, 0.0f);
+
+            if(SceneBackgrounds[_sceneBackgrounds[mapIndex]] != null)
+            {
+                GameObject newSceneBackground = Instantiate(SceneBackgrounds[_sceneBackgrounds[mapIndex]]);
+                newSceneBackground.transform.SetParent(newSceneMainPiece.transform, false);
+            }
+
+            if (_sceneBackgroundsGO.Count == 3)
+            {
+                if (goingRight)
+                {
+                    Destroy(_sceneBackgroundsGO[0]);
+                    _sceneBackgroundsGO.RemoveAt(0);
+
+                    _sceneBackgroundsGO.Add(newSceneMainPiece);
+                }
+                else
+                {
+                    Destroy(_sceneBackgroundsGO[_sceneBackgroundsGO.Count-1]);
+                    _sceneBackgroundsGO.RemoveAt(_sceneBackgroundsGO.Count-1);
+
+                    _sceneBackgroundsGO.Insert(0, newSceneMainPiece);
+                }
+            }
+            else
+            {
+                _sceneBackgroundsGO.Add(newSceneMainPiece);
+            }
+        }
+    }
+
+    void GeneratePlayer()
+    {
+        if(PlayerGO != null)
+        {
+            GameObject playerGO = Instantiate(PlayerGO);
+            _player = playerGO.GetComponent<CP_PlayerController>();            
+
+            if (_player != null)
+            {
+                _player.SetSceneManager(this);
+            }
+        }
+    }
+
+    public void CheckMapGeneration()
+    {
+        if (_player != null)
+        {
+            var playerXPosition = (int) _player.transform.position.x;
+            var playerIndexPos = playerXPosition / kScenePieceSize;
+
+            if (playerIndexPos != _sceneMapLastGeneratedIndex)
+            {
+                var goingRight = (playerIndexPos > _sceneMapLastGeneratedIndex);
+                _sceneMapLastGeneratedIndex = playerIndexPos;
+                
+                if (goingRight) playerIndexPos++;
+                else playerIndexPos--;
+                    
+                var playerIndexPosMap = playerIndexPos;
+
+                if (playerIndexPos < 0)
+                {
+                    playerIndexPos += _sceneBackgrounds.Count;
+                }
+                else if (playerIndexPos >= _sceneBackgrounds.Count)
+                {
+                    playerIndexPos -= _sceneBackgrounds.Count;
+                }
+
+                GenerateMap(playerIndexPosMap, playerIndexPos, goingRight);
             }
         }
     }
