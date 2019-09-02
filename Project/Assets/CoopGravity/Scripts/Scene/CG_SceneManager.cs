e77bec58092fc0805b2f70c84d28c9ee09d55ac1
Javier Calet <javier.calet@socialpoint.es>
2019-09-02 17:58:42 +0200

    wip

---
+++ Project/Assets/CoopGravity/Scripts/Scene/CG_SceneManager.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class CG_SceneManager : MonoBehaviour
+{
+    public GameObject EnvironmentGO = null;
+
+    const int kNumStagesByColumn = 2;
+    const int kNumStagesByRow = 4;
+
+    void Awake()
+    {
+        if(EnvironmentGO != null)
+        {
+            var currentPosX = 0f;
+            var currentPosY = 0f;
+            for(var i = 0; i < kNumStagesByColumn; ++i)
+            {
+                currentPosX = 0f;
+
+                for(var j = 0; j < kNumStagesByRow; ++j)
+                {
+                    GameObject newStage = Instantiate(EnvironmentGO);
+                    newStage.transform.position = new Vector3(currentPosX, currentPosY, 0f);
+
+                    currentPosX += 36f;
+                }
+
+                currentPosY -= 28f;
+            }
+        }
+    }
+}
